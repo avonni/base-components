dee9ffaa688566d8ac6bea09bed2a336706c0bc4
Aliaksandr Antaniuk <aleksandrantonyuk89@gmail.com>
2021-02-09 23:50:21 +0300

    Added rating component. Added stories.

---
+++ src/modules/avonni/rating/__docs__/rating.stories.js
@@ -0,0 +1,80 @@
+import { Rating } from '../__examples__/rating';
+
+export default {
+    title: 'Example/Rating',
+    argTypes: {
+        label: {
+            control: {
+                type: 'text'
+            }
+        },
+        fieldLevelHelp: {
+            control: {
+                type: 'text'
+            }
+        },
+        iconName: {
+            control: {
+                type: 'text'
+            }
+        },
+        value: {
+            control: {
+                type: 'number'
+            }
+        },
+        min: {
+            control: {
+                type: 'number'
+            },
+            defaultValue: 1,
+            table: {
+                defaultValue: { summary: '1' }
+            }
+        },
+        max: {
+            control: {
+                type: 'number'
+            },
+            defaultValue: 5,
+            table: {
+                defaultValue: { summary: '5' }
+            }
+        },
+        selection: {
+            control: {
+                type: 'select',
+                options: ['continuous', 'single']
+            },
+            defaultValue: 'continuous',
+            table: {
+                defaultValue: { summary: 'continuous' }
+            }
+        },
+        disabled: {
+            control: {
+                type: 'boolean'
+            }
+        },
+        readOnly: {
+            control: {
+                type: 'boolean'
+            }
+        },
+        valueHidden: {
+            control: {
+                type: 'boolean'
+            }
+        }
+    }
+};
+
+const Template = (args) => Rating(args);
+
+export const Base = Template.bind({});
+Base.args = {
+    label: 'Label',
+    fieldLevelHelp: 'Help text',
+    iconName: 'utility:favorite',
+    value: 3
+};
