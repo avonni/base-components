57677b5e02a1e3b012acefd5f75ef2b909435f38
Aliaksandr Antaniuk <aleksandrantonyuk89@gmail.com>
2021-02-09 15:38:49 +0300

    Added inputPen component. Added stories.

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,3 @@
-const path = require('path');
 const LWCWebpackPlugin = require('lwc-webpack-plugin');
 
 module.exports = {
@@ -20,12 +19,6 @@ module.exports = {
         config.module.rules = config.module.rules.filter(
             (f) => f.test.toString() !== '/\\.css$/'
         );
-
-        config.module.rules.push({
-            test: /\.css$/,
-            include: [path.resolve(__dirname, 'not_exist_path')],
-            loader: 'style!css'
-        });
 
         return config;
     }
