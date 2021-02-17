bda268febce3efb38b1b258fcdf2f46a8aebee17
Renaud Archer-Fortin <71739112+renaudaf@users.noreply.github.com>
2021-02-17 10:27:19 -0500

    stories and example done

---
+++ src/modules/avonni/progressRing/__docs__/progressRing.stories.js
@@ -0,0 +1,72 @@
+import { ProgressRing } from '../__examples__/progressRing';
+
+export default {
+    title: 'Example/Progress Ring',
+    argTypes: {
+        direction: {
+            control: {
+                type: 'select',
+                options: ['fill', 'drain']
+            },
+            defaultValue: 'fill',
+            table: {
+                type: { summary: 'String' },
+                defaultValue: { summary: 'fill' }
+            }
+        },
+        size: {
+            control: {
+                type: 'select',
+                options: ['medium', 'large']
+            },
+            defaultValue: 'medium',
+            table: {
+                type: { summary: 'String' },
+                defaultValue: { summary: 'medium' }
+            }
+        },
+        value: {
+            control: {
+                type: 'number',
+                min: 0,
+                max: 100
+            },
+            defaultValue: 0,
+            table: {
+                type: { summary: 'Number', detail: 'From 0 to 100' },
+                defaultValue: { summary: '0' }
+            }
+        },
+        variant: {
+            control: {
+                type: 'select',
+                options: [
+                    'base',
+                    'active-step',
+                    'warning',
+                    'expired',
+                    'base-autocomplete'
+                ]
+            },
+            defaultValue: 'base',
+            table: {
+                type: { summary: 'String' },
+                defaultValue: { summary: 'base' }
+            }
+        },
+        hideIcon: {
+            control: {
+                type: 'boolean'
+            },
+            defaultValue: 0,
+            table: {
+                type: { summary: 'Boolean' },
+                defaultValue: { summary: 'false' }
+            }
+        }
+    }
+};
+
+const Template = (args) => ProgressRing(args);
+
+export const Base = Template.bind({});
