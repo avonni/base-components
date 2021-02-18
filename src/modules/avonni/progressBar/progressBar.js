b0f798debf627189f5725cb8d61dbc2c1932e213
Renaud Archer-Fortin <71739112+renaudaf@users.noreply.github.com>
2021-02-18 15:09:18 -0500

    constant declaration and getters and setters for variables

--- src/modules/avonni/progressBar/progressBar.js
+++ src/modules/avonni/progressBar/progressBar.js
@@ -1,4 +1,30 @@
 import { LightningElement, api } from 'lwc';
+import { normalizeBoolean, normalizeString } from 'avonni/utilsPrivate';
+import { classSet } from 'avonni/utils';
+
+const validSizes = ['x-small', 'small', 'medium', 'large'];
+const validPositions = [
+    'left',
+    'right',
+    'top-right',
+    'top-left',
+    'bottom-right',
+    'bottom-left',
+    'centered'
+];
+const validVariants = ['base', 'circular'];
+const validThemes = [
+    'base',
+    'success',
+    'inverse',
+    'alt-inverse',
+    'warning',
+    'info',
+    'error',
+    'offline'
+];
+const validThickness = ['x-small', 'small', 'medium', 'large'];
+const validOrientations = ['horizontal', 'vertical'];
 
 export default class ProgressBar extends LightningElement {
     @api label;
@@ -15,10 +41,90 @@ export default class ProgressBar extends LightningElement {
     _thickness = 'medium';
     _orientation = 'horizontal';
 
-    @api get value() {
+    @api
+    get size() {
+        return this._size;
+    }
+
+    set size(size) {
+        this._size = normalizeString(size, {
+            fallbackValue: 'medium',
+            validValues: validSizes
+        });
+    }
+
+    @api
+    get position() {
+        return this._position;
+    }
+
+    set position(position) {
+        this._position = normalizeString(position, {
+            fallbackValue: 'top-right',
+            validValues: validPositions
+        });
+    }
+
+    @api
+    get variant() {
+        return this._variant;
+    }
+
+    set variant(variant) {
+        this._variant = normalizeString(variant, {
+            fallbackValue: 'base',
+            validValues: validVariants
+        });
+    }
+
+    @api
+    get theme() {
+        return this._theme;
+    }
+
+    set theme(theme) {
+        this._theme = normalizeString(theme, {
+            fallbackValue: 'standard',
+            validValues: validThemes
+        });
+    }
+
+    @api
+    get thickness() {
+        return this._thickness;
+    }
+
+    set thickness(thickness) {
+        this._thickness = normalizeString(thickness, {
+            fallbackValue: 'medium',
+            validValues: validThickness
+        });
+    }
+
+    @api
+    get orientation() {
+        return this._orientation;
+    }
+
+    set orientation(orientation) {
+        this._orientation = normalizeString(orientation, {
+            fallbackValue: 'standard',
+            validValues: validOrientations
+        });
+    }
+    get computedOuterClass() {
+        return classSet('slds-progress-bar')
+            .add({
+                'slds-progress-bar_vertical': this._orientation === 'vertical'
+            })
+            .toString();
+    }
+
+    @api
+    get value() {
         return this._value;
     }
-    get styleValue() {
+    get progressBarValue() {
         return `width: ${this.value}%`;
     }
 
@@ -30,5 +136,14 @@ export default class ProgressBar extends LightningElement {
         } else {
             this._value = value;
         }
+    }
+
+    @api
+    get showValue() {
+        return this._showValue;
+    }
+
+    set showValue(value) {
+        this._showValue = normalizeBoolean(value);
     }
 }
