75ee6efdac8069580517d0b6bba41a079ace6f89
Renaud Archer-Fortin <71739112+renaudaf@users.noreply.github.com>
2021-02-24 10:02:32 -0500

    added description and vertical in stories

--- src/modules/avonni/progressBar/__docs__/progressBar.stories.js
+++ src/modules/avonni/progressBar/__docs__/progressBar.stories.js
@@ -17,6 +17,8 @@ export default {
                 options: ['x-small', 'small', 'medium', 'large', 'full']
             },
             defaultValue: 'full',
+            description:
+                'The size of the progress bar. Valid values are x-small, small, medium, large and full. The default value is medium.',
             table: {
                 type: { summary: 'String' },
                 defaultValue: { summary: 'full' }
@@ -29,6 +31,7 @@ export default {
                 max: 100
             },
             defaultValue: 0,
+            description: 'The percentage value of the progress bar.',
             table: {
                 type: { summary: 'Number', detail: 'From 0 to 100' },
                 defaultValue: { summary: '0' }
@@ -39,6 +42,7 @@ export default {
                 type: 'boolean'
             },
             defaultValue: false,
+            description: 'If true, display the value.',
             table: {
                 type: { summary: 'Boolean' },
                 defaultValue: { summary: 'false' }
@@ -57,6 +61,8 @@ export default {
                 ]
             },
             defaultValue: 'top-right',
+            description:
+                'Valid values include left, right, top-right, top-left, bottom-right, bottom-left and centered',
             table: {
                 type: { summary: 'String' },
                 defaultValue: { summary: 'top-right' }
@@ -66,6 +72,8 @@ export default {
             control: {
                 type: 'text'
             },
+            description:
+                'Text display next to the value. <br> Example: 25% Complete',
             table: {
                 type: { summary: 'String' }
             }
@@ -74,6 +82,8 @@ export default {
             control: {
                 type: 'object'
             },
+            description:
+                'Field: <ul><li>label: string</li> <li>value: number</li> <li>variant: string (default, darker, lightest, success, warning, error)</li> <li>borderStyle: string</li>',
             table: {
                 type: { summary: 'Object' }
             }
@@ -84,6 +94,8 @@ export default {
                 options: ['base', 'circular']
             },
             defaultValue: 'base',
+            description:
+                'The variant changes the appearance of the progress bar. Accepted variants include base or circular. This value defaults to base.',
             table: {
                 type: { summary: 'String' },
                 defaultValue: { summary: 'base' }
@@ -104,9 +116,11 @@ export default {
                 ]
             },
             defaultValue: 'base',
+            description:
+                'Valid values includes base, success, inverse, alt-inverse, warning, info, error and offline.',
             table: {
                 type: { summary: 'string' },
-                defaultValue: 'base'
+                defaultValue: { summary: 'base' }
             }
         },
         textured: {
@@ -114,9 +128,10 @@ export default {
                 type: 'boolean'
             },
             defaultValue: false,
+            description: 'If true display a texture background.',
             table: {
                 type: { summary: 'Boolean' },
-                defaultValue: 'false'
+                defaultValue: { summary: 'false' }
             }
         },
         thickness: {
@@ -125,9 +140,11 @@ export default {
                 options: ['x-small', 'small', 'medium', 'large']
             },
             defaultValue: 'medium',
+            description:
+                'Set progress bar thickness. Valid values include x-small, small, medium and large',
             table: {
                 type: { summary: 'String' },
-                defaultValue: 'medium'
+                defaultValue: { summary: 'medium' }
             }
         },
         orientation: {
@@ -136,6 +153,8 @@ export default {
                 options: ['horizontal', 'vertical']
             },
             defaultValue: 'horizontal',
+            description:
+                'Orientation of the progress bar to be used. Valid values include horizontal and vertical.',
             table: {
                 type: { summary: 'string' },
                 defaultValue: { summary: 'horizontal' }
@@ -159,5 +178,13 @@ export const Base = Template.bind({});
 Base.args = {
     label: 'Label',
     value: 45,
+    badges: badges
+};
+
+export const Vertical = Template.bind({});
+Vertical.args = {
+    label: 'Label',
+    value: 45,
+    orientation: 'vertical',
     badges: badges
 };
