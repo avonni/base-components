0ccd7deaaf19ea8b4e830a5c067317e064c07ef2
Renaud Archer-Fortin <71739112+renaudaf@users.noreply.github.com>
2021-03-04 13:46:31 -0500

    variant is working

--- src/modules/avonni/lwc/progressIndicator/progressIndicator.js
+++ src/modules/avonni/lwc/progressIndicator/progressIndicator.js
@@ -1,3 +1,46 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api } from 'lwc';
+import { normalizeString } from 'c/utilsPrivate';
+import { classSet } from 'c/utils';
 
-export default class ProgressIndicator extends LightningElement {}
+const TYPES = { valid: ['base', 'arrow'], default: 'base' };
+
+const VARIANTS = { valid: ['base', 'shaded'], default: 'base' };
+
+export default class ProgressIndicator extends LightningElement {
+    @api currentStep;
+
+    _variant = 'base';
+    _type = 'base';
+
+    @api
+    get variant() {
+        return this._variant;
+    }
+
+    set variant(variant) {
+        this._variant = normalizeString(variant, {
+            fallbackValue: VARIANTS.default,
+            validValues: VARIANTS.valid
+        });
+    }
+
+    @api
+    get type() {
+        return this._type;
+    }
+
+    set type(type) {
+        this._variant = normalizeString(type, {
+            fallbackValue: TYPES.default,
+            validValues: TYPES.valid
+        });
+    }
+
+    get computedOuterClass() {
+        return classSet('slds-progress')
+            .add({
+                'slds-progress_shade': this._variant === 'shaded'
+            })
+            .toString();
+    }
+}
