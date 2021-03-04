16dd6068919c7976631b2e263b27c15e19ea1f17
Renaud Archer-Fortin <71739112+renaudaf@users.noreply.github.com>
2021-03-04 13:13:55 -0500

    stories and example done

---
+++ src/modules/avonni/lwc/progressIndicator/__docs__/progressIndicator.stories.js
@@ -0,0 +1,95 @@
+import { ProgressIndicator } from '../__examples__/progressIndicator';
+
+export default {
+    title: 'Example/Progress Indicator',
+    argTypes: {
+        completedSteps: {
+            control: {
+                type: 'text'
+            },
+            description: 'All completed steps values.',
+            table: {
+                type: { summary: 'String[]' }
+            }
+        },
+        disabledSteps: {
+            control: {
+                type: 'text'
+            },
+            description: 'All disabled steps values.',
+            table: {
+                type: { summary: 'String[]' }
+            }
+        },
+        errorSteps: {
+            control: {
+                type: 'text'
+            },
+            description: 'All error steps values.',
+            table: {
+                type: { summary: 'String[]' }
+            }
+        },
+        warningSteps: {
+            control: {
+                type: 'text'
+            },
+            description: 'All warning steps values.',
+            table: {
+                type: { summary: 'String[]' }
+            }
+        },
+        currentSteps: {
+            control: {
+                type: 'text'
+            },
+            description:
+                'Set current-step to match the value attribute of one of progress-step components. If current-step is not provided, the value of the first progress-step component is used.',
+            table: {
+                type: { summary: 'String' }
+            }
+        },
+        type: {
+            control: {
+                type: 'select',
+                options: ['base', 'arrow']
+            },
+            description:
+                'Changes the visual pattern of the indicator. Valid values are base and arrow. The default is base.',
+            defaultValue: 'base',
+            table: {
+                defaultValue: { summary: 'base' },
+                type: { summary: 'String' }
+            }
+        },
+        variant: {
+            control: {
+                type: 'select',
+                options: ['base', 'shaded']
+            },
+            description:
+                'Changes the appearance of the progress indicator for the base type only. Valid values are base or shaded. The shaded variant adds a light gray border to the step indicators. The default is base.',
+            defaultValue: 'base',
+            table: {
+                defaultValue: { summary: 'base' },
+                type: { summary: 'string' }
+            }
+        },
+        steps: {
+            control: {
+                type: 'object'
+            },
+            table: {
+                type: { summary: 'object[]' }
+            }
+        }
+    }
+};
+
+const Template = (args) => ProgressIndicator(args);
+
+export const Base = Template.bind({});
+Base.args = {
+    type: 'base',
+    variant: 'base'
+};
---
+++ src/modules/avonni/lwc/progressIndicator/__examples__/progressIndicator.js
@@ -0,0 +1,28 @@
+import Component from 'avonni/progressIndicator';
+
+customElements.define(
+    'ac-avonni-progress-indicator',
+    Component.CustomElementConstructor
+);
+
+export const ProgressIndicator = ({
+    completedSteps,
+    disabledSteps,
+    errorSteps,
+    warningSteps,
+    currentStep,
+    type,
+    variant,
+    steps
+}) => {
+    const element = document.createElement('ac-avonni-progress-indicator');
+    element.completedSteps = completedSteps;
+    element.disabledSteps = disabledSteps;
+    element.errorSteps = errorSteps;
+    element.warningSteps = warningSteps;
+    element.currentStep = currentStep;
+    element.type = type;
+    element.variant = variant;
+    element.steps = steps;
+    return element;
+};
