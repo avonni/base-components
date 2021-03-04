fe28bc37e38521e74c9c780d5eb7fcbbeeac247f
Renaud Archer-Fortin <71739112+renaudaf@users.noreply.github.com>
2021-03-04 16:15:37 -0500

    base done for progress step

--- src/modules/avonni/lwc/progressStep/progressStep.js
+++ src/modules/avonni/lwc/progressStep/progressStep.js
@@ -1,5 +1,39 @@
 import { LightningElement, api } from 'lwc';
+import { classSet } from 'c/utils';
+
+// const BASE_ICONS = {
+//     completed: 'utility:success',
+//     error: 'utility:error',
+//     warning: 'utility:warning'
+// };
 
 export default class ProgressStep extends LightningElement {
     @api value;
+    @api label;
+    // iconName
+    // iconSrc
+    // iconPosition
+    // buttonLabel
+    // buttonName
+    // buttonIconName
+    // buttonIconPosition
+    // buttonDisabled
+    // buttonTitle
+    // buttonVariant
+    // assistiveText
+
+    get computedButtonClass() {
+        const classes = classSet('slds-button slds-progress__marker');
+        if (this.hasIcon) {
+            classes
+                .add('slds-button_icon')
+                .add('slds-progress__marker')
+                .add('slds-progress__marker_icon');
+        }
+        return classes.toString();
+    }
+
+    get hasIcon() {
+        return false;
+    }
 }
