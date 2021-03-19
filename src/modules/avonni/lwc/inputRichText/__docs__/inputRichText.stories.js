2dfe90ea9e2afde5f626ee80577988409c01bb10
Nawel Aammar <nawel.aa@hotmail.fr>
2021-03-19 13:58:53 -0400

    Stories improved for inputRichText and inputToggle

--- src/modules/avonni/lwc/inputRichText/__docs__/inputRichText.stories.js
+++ src/modules/avonni/lwc/inputRichText/__docs__/inputRichText.stories.js
@@ -6,21 +6,45 @@ export default {
         label: {
             control: {
                 type: 'text'
+            },
+            table: {
+                type: { summary: 'string' }
+            }
+        },
+        labelVisible: {
+            name: 'label-visible',
+            control: {
+                type: 'boolean'
+            },
+            defaultValue: 0,
+            table: {
+                defaultValue: { summary: 'false' },
+                type: { summary: 'boolean' }
             }
         },
         value: {
             control: {
                 type: 'text'
+            },
+            table: {
+                type: { summary: 'string' }
             }
         },
         placeholder: {
             control: {
                 type: 'text'
+            },
+            table: {
+                type: { summary: 'string' }
             }
         },
         messageWhenBadInput: {
+            name: 'message-when-bad-input',
             control: {
                 type: 'text'
+            },
+            table: {
+                type: { summary: 'string' }
             }
         },
         variant: {
@@ -30,46 +54,64 @@ export default {
             },
             defaultValue: 'top-toolbar',
             table: {
-                defaultValue: { summary: 'top-toolbar' }
+                defaultValue: { summary: 'top-toolbar' },
+                type: { summary: 'string' },
+                category: 'Toolbar'
             }
         },
-        required: {
+        disabled: {
             control: {
                 type: 'boolean'
             },
             defaultValue: 0,
             table: {
-                defaultValue: { summary: 'false' }
+                defaultValue: { summary: 'false' },
+                type: { summary: 'boolean' }
             }
         },
         hideControls: {
+            name: 'hide-controls',
             control: {
                 type: 'boolean'
             },
             defaultValue: 0,
             table: {
-                defaultValue: { summary: 'false' }
+                defaultValue: { summary: 'false' },
+                type: { summary: 'boolean' },
+                category: 'Toolbar'
             }
         },
-        labelVisible: {
+        isPublisher: {
+            name: 'is-publisher',
             control: {
                 type: 'boolean'
             },
             defaultValue: 0,
             table: {
-                defaultValue: { summary: 'false' }
+                defaultValue: { summary: 'false' },
+                type: { summary: 'boolean' },
+                category: 'Toolbar'
             }
         },
         disabledCategories: {
+            name: 'disabled-categories',
             control: {
                 type: 'object'
+            },
+            table: {
+                type: { summary: 'string[]' },
+                category: 'Toolbar'
             }
         },
         formats: {
             control: {
                 type: 'object'
             },
-            defaultValue: []
+            defaultValue: [],
+            table: {
+                type: { summary: 'string[]' },
+                category: 'Toolbar'
+            }
         }
     }
 };
@@ -78,7 +120,17 @@ const Template = (args) => InputRichText(args);
 
 export const Base = Template.bind({});
 
-Base.args = {
+export const BottomToolbar = Template.bind({});
+BottomToolbar.args = {
+    label: 'Input with bottom toolbar',
+    labelVisible: true,
+    placeholder: 'Write here'
+};
+
+export const CustomToolbar = Template.bind({});
+CustomToolbar.args = {
+    label: 'Input with custom toolbar',
+    labelVisible: true,
     formats: [
         'font',
         'size',
@@ -89,11 +141,19 @@ Base.args = {
         'list',
         'indent',
         'align',
-        'link',
-        'image',
-        'clean',
         'table',
         'header',
         'color'
-    ]
+    ],
+    placeholder: 'Write here',
+    value: 'Note that disabled-categories takes precedence on formats',
+    disabledCategories: ['FORMAT_TEXT']
+};
+
+export const Publisher = Template.bind({});
+Publisher.args = {
+    label: 'Publisher input',
+    labelVisible: true,
+    isPublisher: true,
+    value: 'Note that the toolbar contains more actions.'
 };
