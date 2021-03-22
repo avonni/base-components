e1a32276b8c42cc52f02bf737c9f1edd93b848b1
Renaud Archer-Fortin <71739112+renaudaf@users.noreply.github.com>
2021-03-22 11:28:48 -0400

    added 8 stories to image

--- src/modules/avonni/lwc/image/__docs__/image.stories.js
+++ src/modules/avonni/lwc/image/__docs__/image.stories.js
@@ -41,7 +41,15 @@ export default {
         rounded: {
             control: {
                 type: 'select',
-                options: ['top', 'right', 'bottom', 'left', 'circle', false]
+                options: [
+                    'top',
+                    'right',
+                    'bottom',
+                    'left',
+                    'circle',
+                    false,
+                    true
+                ]
             },
             defaultValue: false,
             table: {
@@ -125,9 +133,87 @@ export default {
 
 const Template = (args) => Image(args);
 
+export const BaseSmall = Template.bind({});
+BaseSmall.args = {
+    src: 'https://picsum.photos/150/75/?image=41',
+    alt: 'Alt text',
+    blankColor: 'transparent',
+    width: '150',
+    height: '75'
+};
+
 export const Base = Template.bind({});
 Base.args = {
     src: 'https://picsum.photos/300/150/?image=41',
     alt: 'Alt text',
+    blankColor: 'transparent'
+};
+
+export const BaseLarge = Template.bind({});
+BaseLarge.args = {
+    src: 'https://picsum.photos/600/300/?image=41',
+    alt: 'Alt text',
+    blankColor: 'transparent',
+    width: '600',
+    height: '300'
+};
+
+export const Thumbnail = Template.bind({});
+Thumbnail.args = {
+    src: 'https://picsum.photos/300/150/?image=41',
+    alt: 'Alt text',
+    blankColor: 'transparent',
+    thumbnail: 'true'
+};
+
+export const CenterCornerRounded = Template.bind({});
+CenterCornerRounded.args = {
+    src: 'https://picsum.photos/300/150/?image=41',
+    alt: 'Alt text',
+    rounded: 'true',
+    blankColor: 'transparent',
+    center: 'true'
+};
+
+export const RightCornerTop = Template.bind({});
+RightCornerTop.args = {
+    src: 'https://picsum.photos/300/150/?image=41',
+    alt: 'Alt text',
+    rounded: 'top',
+    blankColor: 'transparent',
+    right: 'true'
+};
+
+export const CornerBottom = Template.bind({});
+CornerBottom.args = {
+    src: 'https://picsum.photos/300/150/?image=41',
+    alt: 'Alt text',
+    rounded: 'bottom',
+    blankColor: 'transparent'
+};
+
+export const CornerRight = Template.bind({});
+CornerRight.args = {
+    src: 'https://picsum.photos/300/150/?image=41',
+    alt: 'Alt text',
+    rounded: 'right',
+    blankColor: 'transparent'
+};
+
+export const CornerLeft = Template.bind({});
+CornerLeft.args = {
+    src: 'https://picsum.photos/300/150/?image=41',
+    alt: 'Alt text',
+    rounded: 'left',
+    blankColor: 'transparent'
+};
+
+export const Circle = Template.bind({});
+Circle.args = {
+    src: 'https://picsum.photos/300/300/?image=41',
+    alt: 'Alt text',
+    height: '300',
+    width: '300',
+    rounded: 'circle',
     blankColor: 'transparent'
 };
