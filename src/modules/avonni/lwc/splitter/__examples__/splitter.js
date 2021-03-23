bb4f1250088c3dc3e15bddde9ae5bcccdfc86f0e
jbverger <jb-github@easycloud.ca>
2021-03-22 20:06:45 -0400

    [remove] splitter from /lwc

--- src/modules/avonni/lwc/splitter/__examples__/splitter.js
+++
@@ -1,9 +0,0 @@
-import Component from '../../storybookWrappers/splitter/splitter';
-
-customElements.define('ac-avonni-splitter', Component.CustomElementConstructor);
-
-export const Splitter = ({ orientation }) => {
-    const element = document.createElement('ac-avonni-splitter');
-    element.orientation = orientation;
-    return element;
-};
