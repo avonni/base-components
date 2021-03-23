f4beb951923e7a7a0dc002f052d4ac4a4c42f185
Nawel Aammar <nawel.aa@hotmail.fr>
2021-03-23 09:10:11 -0400

    ABC 107 (#10)
    * Setup
    
    * Logic working, need to add visual
    
    * Implemented buttonAlignmentBump, actionPosition and navigationPosition
    
    * Bullet indicator type implemented
    
    * Different rendering depending on the variant
    
    * Changed variant handling to prevent code duplication
    
    * wizard-navigation created
    
    - No variant anymore
    - No title and title slot anymore
    
    * Wizard compatible with modal or any other child component
    
    - Detection method of children changed
    - Modal story added
    
    * Removed useless event listener, fixed fractions alignement
    
    * Removed dispatched events from navigation
    
    * type, next, previous and title added to wizard. beforeChange added to wizardStep. wizardNavigation changed for primitive.
    
    * show/hide methods working
    
    * iconName added to card and base types
    
    Small other fixes:
    - Added categories/subcategories to story controls.
    - Changed slot 'action' for 'actions' to follow other components convention
    
    * indicatorPosition working
    
    * hideNavigation working
    
    * Previous button is hidden for first step
    
    * beforeChangeErrorMessage added with new storybook example
    
    * Cleaned up code and improved stories

---
+++ src/modules/base/storybookWrappers/wizard/modal.js
@@ -0,0 +1,28 @@
+import { LightningElement, api } from 'lwc';
+
+export default class Modal extends LightningElement {
+    @api title;
+    @api iconName;
+    @api variant;
+    @api currentStep;
+    @api hideNavigation;
+    @api indicatorType;
+    @api indicatorPosition;
+    @api hideIndicator;
+    @api buttonPreviousIconName;
+    @api buttonPreviousIconPosition;
+    @api buttonPreviousLabel;
+    @api buttonPreviousVariant;
+    @api buttonNextIconName;
+    @api buttonNextIconPosition;
+    @api buttonNextLabel;
+    @api buttonNextVariant;
+    @api buttonFinishIconName;
+    @api buttonFinishIconPosition;
+    @api buttonFinishVariant;
+    @api buttonFinishLabel;
+    @api buttonAlignmentBump;
+    @api actionPosition;
+    @api fractionPrefixLabel;
+    @api fractionLabel;
+}
---
+++ src/modules/base/wizard/__docs__/wizard.stories.js
@@ -0,0 +1,380 @@
+import { Wizard } from '../__examples__/wizard';
+import { ModalWizard } from '../__examples__/modal';
+import { BeforeChangeWizard } from '../__examples__/beforeChange';
+
+export default {
+    title: 'Example/Wizard',
+    argTypes: {
+        title: {
+            control: {
+                type: 'text'
+            },
+            description:
+                'The title can include text, and is displayed in the header. To include additional markup or another component, use the title slot.',
+            table: {
+                type: { summary: 'string' }
+            }
+        },
+        iconName: {
+            control: {
+                type: 'text'
+            },
+            description:
+                "The Lightning Design System name of the icon. Specify the name in the format 'utility:down' where 'utility' is the category, and 'down' is the specific icon to be displayed. The icon is displayed in the header before the title.",
+            table: {
+                type: { summary: 'string' }
+            }
+        },
+        variant: {
+            control: false,
+            defaultValue: 'base',
+            description:
+                'Variant of the wizard. Valid values include base, modal and card.',
+            table: {
+                type: { summary: 'string' },
+                defaultValue: { summary: 'base' }
+            }
+        },
+        currentStep: {
+            control: {
+                type: 'text'
+            },
+            description:
+                'Set current-step to match the name attribute of one of wizard-step components. If current-step is not provided, the name of the first wizard-step component is used.',
+            table: {
+                type: { summary: 'string' }
+            }
+        },
+        hideNavigation: {
+            control: {
+                type: 'boolean'
+            },
+            description:
+                'If true, hide the navigation (buttons and indicator).',
+            table: {
+                type: { summary: 'boolean' },
+                defaultValue: { summary: false },
+                category: 'Navigation'
+            }
+        },
+        indicatorType: {
+            control: {
+                type: 'select',
+                options: [
+                    'base',
+                    'base-shaded',
+                    'path',
+                    'bullet',
+                    'fractions',
+                    'bar'
+                ]
+            },
+            defaultValue: 'base',
+            description:
+                'Changes the visual pattern of the indicator. Valid values are base, base-shaded, path, bullet, fractions, bar.',
+            table: {
+                defaultValue: { summary: 'base' },
+                type: { summary: 'string' },
+                category: 'Navigation',
+                subcategory: 'Indicator'
+            }
+        },
+        indicatorPosition: {
+            control: {
+                type: 'select',
+                options: ['footer', 'header']
+            },
+            defaultValue: 'footer',
+            description:
+                'Changes the indicator position. Valid values are footer and header.',
+            table: {
+                defaultValue: { summary: 'footer' },
+                type: { summary: 'string' },
+                category: 'Navigation',
+                subcategory: 'Indicator'
+            }
+        },
+        hideIndicator: {
+            control: {
+                type: 'boolean'
+            },
+            description: 'If true, hide the indicator.',
+            defaultValue: false,
+            table: {
+                defaultValue: { summary: 'false' },
+                type: { summary: 'boolean' },
+                category: 'Navigation',
+                subcategory: 'Indicator'
+            }
+        },
+        buttonPreviousIconName: {
+            control: {
+                type: 'text'
+            },
+            table: {
+                type: { summary: 'string' },
+                category: 'Navigation',
+                subcategory: 'Previous button'
+            }
+        },
+        buttonPreviousIconPosition: {
+            control: {
+                type: 'select',
+                options: ['left', 'right']
+            },
+            defaultValue: 'left',
+            description: 'Valid values include left and right.',
+            table: {
+                defaultValue: { summary: 'left' },
+                type: { summary: 'string' },
+                category: 'Navigation',
+                subcategory: 'Previous button'
+            }
+        },
+        buttonPreviousLabel: {
+            control: {
+                type: 'text'
+            },
+            defaultValue: 'Previous',
+            table: {
+                defaultValue: { summary: 'Previous' },
+                type: { summary: 'string' },
+                category: 'Navigation',
+                subcategory: 'Previous button'
+            }
+        },
+        buttonPreviousVariant: {
+            control: {
+                type: 'select',
+                options: [
+                    'bare',
+                    'neutral',
+                    'brand',
+                    'brand-outline',
+                    'inverse',
+                    'destructive',
+                    'destructive-text',
+                    'success'
+                ]
+            },
+            defaultValue: 'neutral',
+            table: {
+                defaultValue: { summary: 'neutral' },
+                type: { summary: 'string' },
+                category: 'Navigation',
+                subcategory: 'Previous button'
+            }
+        },
+        buttonNextIconName: {
+            control: {
+                type: 'text'
+            },
+            table: {
+                type: { summary: 'string' },
+                category: 'Navigation',
+                subcategory: 'Next button'
+            }
+        },
+        buttonNextIconPosition: {
+            control: {
+                type: 'select',
+                options: ['left', 'right']
+            },
+            defaultValue: 'left',
+            description: 'Valid values include left and right.',
+            table: {
+                defaultValue: { summary: 'left' },
+                type: { summary: 'string' },
+                category: 'Navigation',
+                subcategory: 'Next button'
+            }
+        },
+        buttonNextLabel: {
+            control: {
+                type: 'text'
+            },
+            defaultValue: 'Next',
+            table: {
+                defaultValue: { summary: 'Next' },
+                type: { summary: 'string' },
+                category: 'Navigation',
+                subcategory: 'Next button'
+            }
+        },
+        buttonNextVariant: {
+            control: {
+                type: 'select',
+                options: [
+                    'bare',
+                    'neutral',
+                    'brand',
+                    'brand-outline',
+                    'inverse',
+                    'destructive',
+                    'destructive-text',
+                    'success'
+                ]
+            },
+            defaultValue: 'neutral',
+            table: {
+                defaultValue: { summary: 'neutral' },
+                type: { summary: 'string' },
+                category: 'Navigation',
+                subcategory: 'Next button'
+            }
+        },
+        buttonFinishIconName: {
+            control: {
+                type: 'text'
+            },
+            table: {
+                type: { summary: 'string' },
+                category: 'Navigation',
+                subcategory: 'Finish button'
+            }
+        },
+        buttonFinishIconPosition: {
+            control: {
+                type: 'select',
+                options: ['left', 'right']
+            },
+            defaultValue: 'left',
+            description: 'Valid values include left and right.',
+            table: {
+                defaultValue: { summary: 'left' },
+                type: { summary: 'string' },
+                category: 'Navigation',
+                subcategory: 'Finish button'
+            }
+        },
+        buttonFinishLabel: {
+            control: {
+                type: 'text'
+            },
+            defaultValue: 'Finish',
+            table: {
+                defaultValue: { summary: 'Finish' },
+                type: { summary: 'string' },
+                category: 'Navigation',
+                subcategory: 'Finish button'
+            }
+        },
+        buttonFinishVariant: {
+            control: {
+                type: 'select',
+                options: [
+                    'bare',
+                    'neutral',
+                    'brand',
+                    'brand-outline',
+                    'inverse',
+                    'destructive',
+                    'destructive-text',
+                    'success'
+                ]
+            },
+            defaultValue: 'neutral',
+            description:
+                'Valid values include bare, neutral, brand, brand-outline, inverse, destructive, destructive-text, success.',
+            table: {
+                defaultValue: { summary: 'neutral' },
+                type: { summary: 'string' },
+                category: 'Navigation',
+                subcategory: 'Finish button'
+            }
+        },
+        buttonAlignmentBump: {
+            control: {
+                type: 'select',
+                options: ['left', 'right']
+            },
+            description: 'Valid values include left and right.',
+            table: {
+                type: { summary: 'string' },
+                category: 'Navigation'
+            }
+        },
+        actionPosition: {
+            control: {
+                type: 'select',
+                options: ['left', 'right']
+            },
+            defaultValue: 'left',
+            description: 'Valid values include left and right.',
+            table: {
+                defaultValue: { summary: 'left' },
+                type: { summary: 'string' },
+                category: 'Navigation'
+            }
+        },
+        fractionPrefixLabel: {
+            control: {
+                type: 'text'
+            },
+            defaultValue: 'Step',
+            description:
+                'Label displayed in front of fraction. Example: fraction-prefix-label == “Step” => Step 1 of 3',
+            table: {
+                defaultValue: { summary: 'Step' },
+                type: { summary: 'string' },
+                category: 'Navigation',
+                subcategory: 'Indicator'
+            }
+        },
+        fractionLabel: {
+            control: {
+                type: 'text'
+            },
+            defaultValue: 'of',
+            description:
+                'Label displayed between current index and max number of slides. Example: fraction-label == “of” => 1 of 3',
+            table: {
+                defaultValue: { summary: 'of' },
+                type: { summary: 'string' },
+                category: 'Navigation',
+                subcategory: 'Indicator'
+            }
+        }
+    }
+};
+
+const Template = (args) => Wizard(args);
+const ModalTemplate = (args) => ModalWizard(args);
+const BeforeChangeTemplate = (args) => BeforeChangeWizard(args);
+
+export const Base = Template.bind({});
+export const Modal = ModalTemplate.bind({});
+export const Card = Template.bind({});
+export const BeforeChangeOnSteps = BeforeChangeTemplate.bind({});
+
+Modal.args = {
+    title: 'Modal Wizard Example',
+    variant: 'modal',
+    indicatorPosition: 'header',
+    indicatorType: 'path',
+    buttonPreviousLabel: 'Back',
+    buttonPreviousIconName: 'utility:back',
+    buttonNextIconName: 'utility:forward',
+    buttonNextIconPosition: 'right',
+    buttonNextLabel: 'Continue',
+    buttonNextVariant: 'brand',
+    buttonFinishIconName: 'utility:check',
+    buttonFinishLabel: 'Done',
+    buttonFinishVariant: 'success'
+};
+
+Card.args = {
+    title: 'Card Wizard Example',
+    variant: 'card',
+    iconName: 'custom:custom26',
+    buttonAlignmentBump: 'right',
+    actionPosition: 'right',
+    indicatorType: 'fractions',
+    buttonNextVariant: 'brand',
+    buttonFinishVariant: 'destructive'
+};
+
+BeforeChangeOnSteps.args = {
+    iconName: 'utility:touch_action',
+    indicatorType: 'bar'
+};
