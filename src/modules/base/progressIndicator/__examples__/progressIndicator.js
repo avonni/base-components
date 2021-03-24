daebca2f13b2e12b2943516d9b94f64e50e24716
Renaud Archer-Fortin <71739112+renaudaf@users.noreply.github.com>
2021-03-23 20:15:18 -0400

    ABC-105 (#11)
    * files created and component added to lwc.config.json
    
    * stories and example done
    
    * variant is working
    
    * created progressStep
    
    * added progressStep to lwc.config.json
    
    * base done for progress step
    
    * change in progressStep template to make variant shade works
    
    * back to square 1 for current step
    
    * added step object in stories and iteration in progressIndicator.html
    
    * able to change the current step needs to work on the icon
    
    * success iconworks
    
    * restarting from the beginning for progressBarValue
    
    * progressBarValue is working
    
    * steps work with every values
    
    * commit pre-cleanup
    
    * commit before changing progressBar
    
    * line works with ::before, still need to align
    
    * lines are now aligned
    
    * added colorsto lines
    
    * added all the steps definition in progressIndicator and progressStep
    
    * added buttonposition
    
    * fixed problem with before and after
    
    * save pre cleanup
    
    * button fully functional
    
    * everything is centered with the button
    
    * still have work to do on centering
    
    * change updateSteps for completedsteps and currentStep
    
    * everything is aligned
    
    * commit before code cleaning
    
    * back without button popover icon
    
    * hover, show and button works for popover
    
    * changed steps to accept any value and not just integer
    
    * disabled steps is working
    
    * variant shaded works again
    
    * fixed the rendered callback problem
    
    * fixed current step problem
    
    * changed the size of before and after
    
    * fix the problem with warning icon
    
    * hover & focus works for popoverButtonIconName
    
    * fixed the problem with lesser steps markup
    
    * added story for lwc
    
    * made story for base
    
    * commit before chaning popover
    
    * commit before refactoring and noting
    
    * last changes in classes in progress step
    
    * added brand to popover
    
    * added popoverlabel, description and ratio
    
    * all the attributes work
    
    * icon is now the right color
    
    * added popover size medium small
    
    * added other stories
    
    * code cleaning
    
    * added popoversize large and fixed position for each size
    
    * added popover variant and no border on focus
    
    * added blur and focus on popoverButton so the icon stays when focus
    
    * removed arrow type, added computedPopoverIconSize
    
    * no hover and focus on base popover
    
    * no more popoverIconSize and commit before trying again for hover problem
    
    * hover and focus works
    
    * fixed the problem with with icon when completed
    
    * code refactored
    
    * added progressIndicator to lwc.config.js
    
    * change progress-step to primitive-progress-step
    
    * no more inline styling

---
+++ src/modules/base/progressIndicator/__examples__/progressIndicator.js
@@ -0,0 +1,28 @@
+import Component from 'base/progressIndicator';
+
+customElements.define(
+    'ac-base-progress-indicator',
+    Component.CustomElementConstructor
+);
+
+export const ProgressIndicator = ({
+    completedSteps,
+    disabledSteps,
+    errorSteps,
+    warningSteps,
+    currentStep,
+    type,
+    variant,
+    steps
+}) => {
+    const element = document.createElement('ac-base-progress-indicator');
+    element.completedSteps = completedSteps;
+    element.disabledSteps = disabledSteps;
+    element.errorSteps = errorSteps;
+    element.warningSteps = warningSteps;
+    element.currentStep = currentStep;
+    element.type = type;
+    element.variant = variant;
+    element.steps = steps;
+    return element;
+};
