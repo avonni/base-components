c7cc1970e893fbed459f629cb634382761172662
Renaud Archer-Fortin <71739112+renaudaf@users.noreply.github.com>
2021-03-25 21:10:03 -0400

    ABC-125 (#24)
    * added thickness to js file, examples and stories
    
    * change labelfor title
    
    * added titlePosition to examples, stories and js file
    
    * added label to example, stories and JS
    
    * added round to examples, stories and js
    
    * label works inside progress circle
    
    * title position top done
    
    * added avonni-progress-ring-with-title-top
    
    * added thickness to every sizes
    
    * refactored css classes
    
    * added new stories with thickness and label
    
    * added top to title to have bigger space between title top and progress circle
    
    * added styling for label depending on size
    
    * changed the color of label to a lighter gray

--- src/modules/base/progressCircle/__examples__/progressCircle.js
+++ src/modules/base/progressCircle/__examples__/progressCircle.js
@@ -1,22 +1,33 @@
-import { generateColors } from '../../utilsPrivate/colorUtils'
+import { generateColors } from '../../utilsPrivate/colorUtils';
 import Component from 'base/progressCircle';
 
-customElements.define('ac-base-progress-circle', Component.CustomElementConstructor);
+customElements.define(
+    'ac-base-progress-circle',
+    Component.CustomElementConstructor
+);
 
 export const ProgressCircle = ({
-    label,
+    title,
+    titlePosition,
     value,
+    label,
     variant,
     color,
     direction,
-    size
+    size,
+    thickness,
+    round
 }) => {
     const element = document.createElement('ac-base-progress-circle');
-    element.label = label;
+    element.title = title;
+    element.titlePosition = titlePosition;
     element.value = value;
+    element.label = label;
     element.variant = variant;
     element.color = generateColors(color).hex;
     element.direction = direction;
     element.size = size;
+    element.thickness = thickness;
+    element.round = round;
     return element;
 };
