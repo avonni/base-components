33b0c14240aa3888f861af0728240b02e712be8c
Renaud Archer-Fortin <71739112+renaudaf@users.noreply.github.com>
2021-03-29 17:31:39 -0400

    created everyfiles for duallistbox

---
+++ src/modules/base/dualListbox/__docs__/dualListbox.stories.js
@@ -0,0 +1,302 @@
+import { DualListbox } from '../__examples__/dualListbox';
+
+export default {
+    title: 'Example/Dual Listbox',
+    argTypes: {
+        addButtonLabel: {
+            name: 'add-button-label',
+            control: {
+                type: 'text'
+            },
+            description: 'Label for add button.',
+            table: {
+                type: { summary: 'string' }
+            }
+        },
+        disableReordering: {
+            name: 'disable-reordering',
+            control: {
+                type: 'boolean'
+            },
+            defaultValue: false,
+            description:
+                'If present, the Up and Down buttons used for reordering are hidden.',
+            table: {
+                type: { summary: 'boolean' },
+                defaultValue: { summary: false }
+            }
+        },
+        disabled: {
+            control: {
+                type: 'boolean'
+            },
+            defaultValue: false,
+            description:
+                'If present, the listbox is disabled and users cannot interact with it.',
+            table: {
+                type: { summary: 'boolean' },
+                defaultValue: { summary: false }
+            }
+        },
+        downButtonLabel: {
+            name: 'down-button-label',
+            control: {
+                type: 'text'
+            },
+            description: 'Label for down button.',
+            table: {
+                type: { summary: 'string' }
+            }
+        },
+        fieldLevelHelp: {
+            name: 'field-level-help',
+            control: {
+                type: 'text'
+            },
+            description:
+                'Help text detailing the purpose and function of the dual listbox.',
+            table: {
+                type: { summary: 'string' }
+            }
+        },
+        label: {
+            type: { name: 'string', required: true },
+            control: {
+                type: 'text'
+            },
+            description: 'Label for the dual listbox.',
+            table: {
+                type: { summary: 'string' }
+            }
+        },
+        max: {
+            control: {
+                type: 'number'
+            },
+            description:
+                'Maximum number of options allowed in the selected options listbox.',
+            table: {
+                type: { summary: 'number' }
+            }
+        },
+        min: {
+            control: {
+                type: 'number'
+            },
+            defaultValue: '0',
+            description:
+                'Minimum number of options required in the selected options listbox.',
+            table: {
+                type: { summary: 'number' },
+                defaultValue: { summary: '0' }
+            }
+        },
+        messageWhenRangeOverflow: {
+            name: 'message-when-range-overflow',
+            control: {
+                type: 'text'
+            },
+            description:
+                'Error message to be displayed when a range overflow is detected.',
+            table: {
+                type: { summary: 'string' }
+            }
+        },
+        messageWhenRangeUnderflow: {
+            name: 'message-when-range-underflow',
+            control: {
+                type: 'text'
+            },
+            description:
+                'Error message to be displayed when a range underflow is detected.',
+            table: {
+                type: { summary: 'string' }
+            }
+        },
+        messageWhenValueMissing: {
+            name: 'message-when-value-missing',
+            control: {
+                type: 'text'
+            },
+            description:
+                'Error message to be displayed when the value is missing and input is required.',
+            table: {
+                type: { summary: 'string' }
+            }
+        },
+        name: {
+            control: {
+                type: 'string'
+            },
+            description: 'Specifies the name of an input element.',
+            table: {
+                type: { summary: 'string' }
+            }
+        },
+        options: {
+            type: { name: 'object', required: true },
+            control: {
+                type: 'object'
+            },
+            description:
+                'A list of options that are available for selection. Each option has the following attributes: label, value and icon.',
+            table: {
+                type: { summary: 'object []' }
+            }
+        },
+        optionIconName: {
+            name: 'option-icon-name',
+            control: {
+                type: 'string'
+            },
+            description:
+                "The Lightning Design System name of the icon. Specify the name in the format 'utility:favorite' where 'utility' is the category, and 'favorite' is the specific icon to be displayed.",
+            table: {
+                type: { summary: 'string' }
+            }
+        },
+        removeButtonLabel: {
+            name: 'remove-button-label',
+            control: {
+                type: 'text'
+            },
+            description: 'Label for remove button.',
+            table: {
+                type: { summary: 'string' }
+            }
+        },
+        required: {
+            control: {
+                type: 'boolean'
+            },
+            defaultValue: false,
+            description:
+                'If present, the user must add an item to the selected listbox before submitting the form.',
+            table: {
+                type: { summary: 'boolean' },
+                defaultValue: { summary: false }
+            }
+        },
+        requiredOptions: {
+            control: {
+                type: 'object'
+            },
+            description:
+                'A list of required options that cannot be removed from selected options listbox. This list is populated with values from the options attribute.',
+            table: {
+                type: { summary: 'object []' }
+            }
+        },
+        searchEngine: {
+            name: 'search-engine',
+            control: {
+                type: 'boolean'
+            },
+            defaultValue: false,
+            description:
+                'If present, a search box is added to the first listbox.',
+            table: {
+                type: { summary: 'boolean' },
+                defaultValue: { summary: false }
+            }
+        },
+        selectedlabel: {
+            name: 'selected-label',
+            type: { name: 'string', required: true },
+            control: {
+                type: 'text'
+            },
+            description: 'Label for the selected options listbox.',
+            table: {
+                type: { summary: 'string' }
+            }
+        },
+        showActivityIndicator: {
+            name: 'show-activity-indicator',
+            control: {
+                type: 'boolean'
+            },
+            defaultValue: false,
+            description:
+                'If present, a spinner is displayed in the first listbox to indicate loading activity.',
+            table: {
+                type: { summary: 'boolean' },
+                defaultValue: { summary: false }
+            }
+        },
+        size: {
+            control: {
+                type: 'number'
+            },
+            description:
+                'Number of items that display in the listboxes before vertical scrollbars are displayed. Determines the vertical size of the listbox.',
+            table: {
+                type: { summary: 'number' }
+            }
+        },
+        sourcelabel: {
+            name: 'source-label',
+            type: { name: 'string', required: true },
+            control: {
+                type: 'text'
+            },
+            description: 'Label for the source options listbox.',
+            table: {
+                type: { summary: 'string' }
+            }
+        },
+        upButtonLabel: {
+            name: 'up-button-label',
+            control: {
+                type: 'text'
+            },
+            description: 'Label for up button.',
+            table: {
+                type: { summary: 'string' }
+            }
+        },
+        validity: {
+            control: {
+                type: 'string'
+            },
+            description:
+                'Represents the validity states that an element can be in, with respect to constraint validation.',
+            table: {
+                type: { summary: 'string' }
+            }
+        },
+        value: {
+            control: {
+                type: 'string'
+            },
+            description:
+                'A list of default options that are included in the selected options listbox. This list is populated with values from the options attribute.',
+            table: {
+                type: { summary: 'string []' }
+            }
+        },
+        variant: {
+            control: {
+                type: 'select',
+                options: [
+                    'standard',
+                    'label-hidden',
+                    'label-inline',
+                    'label-stacked'
+                ]
+            },
+            defaultValue: 'standard',
+            description:
+                'The variant changes the appearance of the dual listbox. Valid variants include standard, label-hidden, label-inline, and label-stacked. Use label-hidden to hide the label but make it available to assistive technology. Use label-inline to horizontally align the label and dual listbox. Use label-stacked to place the label above the dual listbox.',
+            table: {
+                type: { summary: 'string' },
+                defaultValue: { summary: 'standard' }
+            }
+        }
+    }
+};
+
+const Template = (args) => DualListbox(args);
+
+export const Base = Template.bind({});
+Base.args = {};
---
+++ src/modules/base/dualListbox/__examples__/dualListbox.js
@@ -0,0 +1,63 @@
+import Component from 'base/dualListbox';
+
+customElements.define(
+    'ac-base-dual-listbox',
+    Component.CustomElementConstructor
+);
+
+export const DualListbox = ({
+    addButtonLabel,
+    disableReordering,
+    disabled,
+    downButtonLabel,
+    fieldLevelHelp,
+    label,
+    max,
+    min,
+    messageWhenRangeOverflow,
+    messageWhenRangeUnderflow,
+    messageWhenValueMissing,
+    name,
+    options,
+    optionsIconName,
+    removeButtonLabel,
+    required,
+    requiredOptions,
+    searchEngine,
+    selectedLabel,
+    showActivityIndicator,
+    size,
+    sourceLabel,
+    upButtonLabel,
+    validity,
+    value,
+    variant
+}) => {
+    const element = document.createElement('ac-base-dual-listbox');
+    element.addButtonLabel = addButtonLabel;
+    element.disableReordering = disableReordering;
+    element.disabled = disabled;
+    element.downButtonLabel = downButtonLabel;
+    element.fieldLevelHelp = fieldLevelHelp;
+    element.label = label;
+    element.max = max;
+    element.min = min;
+    element.messageWhenRangeOverflow = messageWhenRangeOverflow;
+    element.messageWhenRangeUnderflow = messageWhenRangeUnderflow;
+    element.messageWhenValueMissing = messageWhenValueMissing;
+    element.name = name;
+    element.options = options;
+    element.optionsIconName = optionsIconName;
+    element.removeButtonLabel = removeButtonLabel;
+    element.required = required;
+    element.requiredOptions = requiredOptions;
+    element.searchEngine = searchEngine;
+    element.selectedLabel = selectedLabel;
+    element.showActivityIndicator = showActivityIndicator;
+    element.size = size;
+    element.sourceLabel = sourceLabel;
+    element.upButtonLabel = upButtonLabel;
+    element.validity = validity;
+    element.value = value;
+    element.variant = variant;
+};
