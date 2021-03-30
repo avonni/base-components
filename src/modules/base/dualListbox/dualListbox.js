ebd06733e7b778578bd8d97de2f424fac9e74c97
Renaud Archer-Fortin <71739112+renaudaf@users.noreply.github.com>
2021-03-30 10:18:35 -0400

    added markup in html

--- src/modules/base/dualListbox/dualListbox.js
+++ src/modules/base/dualListbox/dualListbox.js
@@ -1,3 +1,5 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api } from 'lwc';
 
-export default class DualListbox extends LightningElement {}
+export default class DualListbox extends LightningElement {
+    @api label;
+}
