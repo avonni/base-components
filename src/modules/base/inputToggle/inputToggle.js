c61d3b9137ea0b34881a52dacc13ef10ee1863f8
Nawel Aammar <nawel.aa@hotmail.fr>
2021-04-08 10:18:16 -0400

    Added blur and focus events in inputToggle

--- src/modules/base/inputToggle/inputToggle.js
+++ src/modules/base/inputToggle/inputToggle.js
@@ -322,12 +322,14 @@ export default class InputToggle extends LightningElement {
     }
 
     handleBlur() {
-        if (this.required && !this.checked) {
-            this.valid = false;
-        } else {
-            this.valid = true;
-        }
+        this.valid = !(this.required && !this.checked);
         this.updateClassList();
+
+        this.dispatchEvent(new CustomEvent('blur'));
+    }
+
+    handleFocus() {
+        this.dispatchEvent(new CustomEvent('focus'));
     }
 
     handleChange(event) {
@@ -335,7 +337,9 @@ export default class InputToggle extends LightningElement {
 
         this.dispatchEvent(
             new CustomEvent('change', {
-                detail: event.target.checked,
+                detail: {
+                    checked: event.target.checked
+                },
                 bubbles: true,
                 cancelable: false,
                 composed: true
