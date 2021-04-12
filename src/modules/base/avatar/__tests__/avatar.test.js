4dda76b6e53823e79cfd0486f89780711745c377
Renaud Archer-Fortin <71739112+renaudaf@users.noreply.github.com>
2021-04-12 14:25:51 -0400

    made test for alternative text, fallback icon name and initials

--- src/modules/base/avatar/__tests__/avatar.test.js
+++ src/modules/base/avatar/__tests__/avatar.test.js
@@ -36,4 +36,90 @@ describe('Avatar', () => {
         expect(element.tertiaryText).toBeUndefined();
         expect(element.textPosition).toBe('right');
     });
+
+    /* ----- ATTRIBUTES ----- */
+
+    // alternative-text with image
+    it('Avatar alternative text with image', () => {
+        const element = createElement('base-avatar', {
+            is: Avatar
+        });
+        document.body.appendChild(element);
+
+        element.alternativeText = 'This is an alternative text';
+        element.src =
+            'https://www.lightningdesignsystem.com/assets/images/avatar2.jpg';
+        element.hideAvatarDetails = true;
+
+        return Promise.resolve().then(() => {
+            const image = element.shadowRoot.querySelector('img');
+            expect(image.alt).toBe('This is an alternative text');
+        });
+    });
+
+    // alternative-text with icon
+    it('Avatar alternative text with icon', () => {
+        const element = createElement('base-avatar', {
+            is: Avatar
+        });
+        document.body.appendChild(element);
+
+        element.alternativeText = 'This is an alternative text';
+        element.fallbackIconName = 'standard:account';
+        element.hideAvatarDetails = true;
+
+        return Promise.resolve().then(() => {
+            const icon = element.shadowRoot.querySelector('lightning-icon');
+            expect(icon.title).toBe('This is an alternative text');
+        });
+    });
+
+    // alternative-text with initials
+    it('Avatar alternative text with initials', () => {
+        const element = createElement('base-avatar', {
+            is: Avatar
+        });
+        document.body.appendChild(element);
+
+        element.alternativeText = 'This is an alternative text';
+        element.initials = 'JD';
+        element.hideAvatarDetails = true;
+
+        return Promise.resolve().then(() => {
+            const abbr = element.shadowRoot.querySelector('abbr');
+            expect(abbr.title).toBe('This is an alternative text');
+        });
+    });
+
+    // fallback icon name
+    it('Avatar fallback icon name', () => {
+        const element = createElement('base-avatar', {
+            is: Avatar
+        });
+        document.body.appendChild(element);
+
+        element.fallbackIconName = 'standard:account';
+        element.hideAvatarDetails = true;
+
+        return Promise.resolve().then(() => {
+            const icon = element.shadowRoot.querySelector('lightning-icon');
+            expect(icon.iconName).toBe('standard:account');
+        });
+    });
+
+    // Itinitals
+    it('Avatar initials', () => {
+        const element = createElement('base-avatar', {
+            is: Avatar
+        });
+        document.body.appendChild(element);
+
+        element.initials = 'JD';
+        element.hideAvatarDetails = true;
+
+        return Promise.resolve().then(() => {
+            const abbr = element.shadowRoot.querySelector('abbr');
+            expect(abbr.textContent).toBe('JD');
+        });
+    });
 });
