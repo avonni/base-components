f4e863529e739e5c541493f06115f24696dc81c5
Renaud Archer-Fortin <71739112+renaudaf@users.noreply.github.com>
2021-04-12 15:04:12 -0400

    added size, src, variant, presence, presence title, presence position tests

--- src/modules/base/avatar/__tests__/avatar.test.js
+++ src/modules/base/avatar/__tests__/avatar.test.js
@@ -39,7 +39,7 @@ describe('Avatar', () => {
 
     /* ----- ATTRIBUTES ----- */
 
-    // alternative-text with image
+    // alternative-text
     it('Avatar alternative text with image', () => {
         const element = createElement('base-avatar', {
             is: Avatar
@@ -57,7 +57,6 @@ describe('Avatar', () => {
         });
     });
 
-    // alternative-text with icon
     it('Avatar alternative text with icon', () => {
         const element = createElement('base-avatar', {
             is: Avatar
@@ -74,7 +73,6 @@ describe('Avatar', () => {
         });
     });
 
-    // alternative-text with initials
     it('Avatar alternative text with initials', () => {
         const element = createElement('base-avatar', {
             is: Avatar
@@ -120,6 +118,398 @@ describe('Avatar', () => {
         return Promise.resolve().then(() => {
             const abbr = element.shadowRoot.querySelector('abbr');
             expect(abbr.textContent).toBe('JD');
+        });
+    });
+
+    //size
+    it('Avatar Size xx-small', () => {
+        const element = createElement('base-avatar', {
+            is: Avatar
+        });
+        document.body.appendChild(element);
+
+        element.size = 'xx-small';
+        element.hideAvatarDetails = true;
+        element.src =
+            'https://www.lightningdesignsystem.com/assets/images/avatar2.jpg';
+
+        return Promise.resolve().then(() => {
+            const div = element.shadowRoot.querySelector('div');
+            expect(div.className).toContain('avonni-avatar_xx-small');
+        });
+    });
+
+    it('Avatar Size x-small', () => {
+        const element = createElement('base-avatar', {
+            is: Avatar
+        });
+        document.body.appendChild(element);
+
+        element.size = 'x-small';
+        element.hideAvatarDetails = true;
+        element.src =
+            'https://www.lightningdesignsystem.com/assets/images/avatar2.jpg';
+
+        return Promise.resolve().then(() => {
+            const div = element.shadowRoot.querySelector('div');
+            expect(div.className).toContain('slds-avatar_x-small');
+        });
+    });
+
+    it('Avatar Size small', () => {
+        const element = createElement('base-avatar', {
+            is: Avatar
+        });
+        document.body.appendChild(element);
+
+        element.size = 'small';
+        element.hideAvatarDetails = true;
+        element.src =
+            'https://www.lightningdesignsystem.com/assets/images/avatar2.jpg';
+
+        return Promise.resolve().then(() => {
+            const div = element.shadowRoot.querySelector('div');
+            expect(div.className).toContain('slds-avatar_small');
+        });
+    });
+
+    it('Avatar Size medium', () => {
+        const element = createElement('base-avatar', {
+            is: Avatar
+        });
+        document.body.appendChild(element);
+
+        element.size = 'medium';
+        element.hideAvatarDetails = true;
+        element.src =
+            'https://www.lightningdesignsystem.com/assets/images/avatar2.jpg';
+
+        return Promise.resolve().then(() => {
+            const div = element.shadowRoot.querySelector('div');
+            expect(div.className).toContain('slds-avatar_medium');
+        });
+    });
+
+    it('Avatar Size large', () => {
+        const element = createElement('base-avatar', {
+            is: Avatar
+        });
+        document.body.appendChild(element);
+
+        element.size = 'large';
+        element.hideAvatarDetails = true;
+        element.src =
+            'https://www.lightningdesignsystem.com/assets/images/avatar2.jpg';
+
+        return Promise.resolve().then(() => {
+            const div = element.shadowRoot.querySelector('div');
+            expect(div.className).toContain('slds-avatar_large');
+        });
+    });
+
+    it('Avatar Size x-large', () => {
+        const element = createElement('base-avatar', {
+            is: Avatar
+        });
+        document.body.appendChild(element);
+
+        element.size = 'x-large';
+        element.hideAvatarDetails = true;
+        element.src =
+            'https://www.lightningdesignsystem.com/assets/images/avatar2.jpg';
+
+        return Promise.resolve().then(() => {
+            const div = element.shadowRoot.querySelector('div');
+            expect(div.className).toContain('avonni-avatar_x-large');
+        });
+    });
+
+    it('Avatar Size xx-large', () => {
+        const element = createElement('base-avatar', {
+            is: Avatar
+        });
+        document.body.appendChild(element);
+
+        element.size = 'xx-large';
+        element.hideAvatarDetails = true;
+        element.src =
+            'https://www.lightningdesignsystem.com/assets/images/avatar2.jpg';
+
+        return Promise.resolve().then(() => {
+            const div = element.shadowRoot.querySelector('div');
+            expect(div.className).toContain('avonni-avatar_xx-large');
+        });
+    });
+
+    // src
+    it('Avatar Src', () => {
+        const element = createElement('base-avatar', {
+            is: Avatar
+        });
+        document.body.appendChild(element);
+
+        element.size = 'xx-large';
+        element.hideAvatarDetails = true;
+        element.src =
+            'https://www.lightningdesignsystem.com/assets/images/avatar2.jpg';
+
+        return Promise.resolve().then(() => {
+            const img = element.shadowRoot.querySelector('img');
+            expect(img.src).toBe(
+                'https://www.lightningdesignsystem.com/assets/images/avatar2.jpg'
+            );
+        });
+    });
+
+    // variant
+    it('Avatar variant square', () => {
+        const element = createElement('base-avatar', {
+            is: Avatar
+        });
+        document.body.appendChild(element);
+
+        element.variant = 'square';
+        element.hideAvatarDetails = true;
+        element.src =
+            'https://www.lightningdesignsystem.com/assets/images/avatar2.jpg';
+
+        return Promise.resolve().then(() => {
+            const div = element.shadowRoot.querySelector('div');
+            expect(div.className).toContain('avonni-avatar_square');
+        });
+    });
+
+    it('Avatar variant circle', () => {
+        const element = createElement('base-avatar', {
+            is: Avatar
+        });
+        document.body.appendChild(element);
+
+        element.variant = 'circle';
+        element.hideAvatarDetails = true;
+        element.src =
+            'https://www.lightningdesignsystem.com/assets/images/avatar2.jpg';
+
+        return Promise.resolve().then(() => {
+            const div = element.shadowRoot.querySelector('div');
+            expect(div.className).toContain('avonni-avatar_circle');
+        });
+    });
+
+    // presence
+    it('Avatar presence online', () => {
+        const element = createElement('base-avatar', {
+            is: Avatar
+        });
+        document.body.appendChild(element);
+
+        element.presence = 'online';
+        element.hideAvatarDetails = true;
+        element.src =
+            'https://www.lightningdesignsystem.com/assets/images/avatar2.jpg';
+
+        return Promise.resolve().then(() => {
+            const presence = element.shadowRoot.querySelector(
+                '.avonni-avatar__presence.avonni-avatar__presence_online'
+            );
+            expect(presence).toBeTruthy();
+        });
+    });
+
+    it('Avatar presence busy', () => {
+        const element = createElement('base-avatar', {
+            is: Avatar
+        });
+        document.body.appendChild(element);
+
+        element.presence = 'busy';
+        element.hideAvatarDetails = true;
+        element.src =
+            'https://www.lightningdesignsystem.com/assets/images/avatar2.jpg';
+
+        return Promise.resolve().then(() => {
+            const presence = element.shadowRoot.querySelector(
+                '.avonni-avatar__presence.avonni-avatar__presence_busy'
+            );
+            expect(presence).toBeTruthy();
+        });
+    });
+
+    it('Avatar presence focus', () => {
+        const element = createElement('base-avatar', {
+            is: Avatar
+        });
+        document.body.appendChild(element);
+
+        element.presence = 'focus';
+        element.hideAvatarDetails = true;
+        element.src =
+            'https://www.lightningdesignsystem.com/assets/images/avatar2.jpg';
+
+        return Promise.resolve().then(() => {
+            const presence = element.shadowRoot.querySelector(
+                '.avonni-avatar__presence.avonni-avatar__presence_focus'
+            );
+            expect(presence).toBeTruthy();
+        });
+    });
+
+    it('Avatar presence offline', () => {
+        const element = createElement('base-avatar', {
+            is: Avatar
+        });
+        document.body.appendChild(element);
+
+        element.presence = 'offline';
+        element.hideAvatarDetails = true;
+        element.src =
+            'https://www.lightningdesignsystem.com/assets/images/avatar2.jpg';
+
+        return Promise.resolve().then(() => {
+            const presence = element.shadowRoot.querySelector(
+                '.avonni-avatar__presence.avonni-avatar__presence_offline'
+            );
+            expect(presence).toBeTruthy();
+        });
+    });
+
+    it('Avatar presence blocked', () => {
+        const element = createElement('base-avatar', {
+            is: Avatar
+        });
+        document.body.appendChild(element);
+
+        element.presence = 'blocked';
+        element.hideAvatarDetails = true;
+        element.src =
+            'https://www.lightningdesignsystem.com/assets/images/avatar2.jpg';
+
+        return Promise.resolve().then(() => {
+            const presence = element.shadowRoot.querySelector(
+                '.avonni-avatar__presence.avonni-avatar__presence_blocked'
+            );
+            expect(presence).toBeTruthy();
+        });
+    });
+
+    it('Avatar presence away', () => {
+        const element = createElement('base-avatar', {
+            is: Avatar
+        });
+        document.body.appendChild(element);
+
+        element.presence = 'away';
+        element.hideAvatarDetails = true;
+        element.src =
+            'https://www.lightningdesignsystem.com/assets/images/avatar2.jpg';
+
+        return Promise.resolve().then(() => {
+            const presence = element.shadowRoot.querySelector(
+                '.avonni-avatar__presence.avonni-avatar__presence_away'
+            );
+            expect(presence).toBeTruthy();
+        });
+    });
+
+    // presence title
+    it('Avatar presence title', () => {
+        const element = createElement('base-avatar', {
+            is: Avatar
+        });
+        document.body.appendChild(element);
+
+        element.presence = 'online';
+        element.presenceTitle = 'Presence Title';
+        element.hideAvatarDetails = true;
+        element.src =
+            'https://www.lightningdesignsystem.com/assets/images/avatar2.jpg';
+
+        return Promise.resolve().then(() => {
+            const presence = element.shadowRoot.querySelector(
+                '.avonni-avatar__presence.avonni-avatar__presence_online'
+            );
+            expect(presence.title).toBe('Presence Title');
+        });
+    });
+
+    // presence position
+    it('Avatar presence bottom-right', () => {
+        const element = createElement('base-avatar', {
+            is: Avatar
+        });
+        document.body.appendChild(element);
+
+        element.presence = 'online';
+        element.presencePosition = 'bottom-right';
+        element.hideAvatarDetails = true;
+        element.src =
+            'https://www.lightningdesignsystem.com/assets/images/avatar2.jpg';
+
+        return Promise.resolve().then(() => {
+            const presence = element.shadowRoot.querySelector(
+                '.avonni-avatar__presence.avonni-avatar__presence_online.avonni-avatar_bottom-right'
+            );
+            expect(presence).toBeTruthy();
+        });
+    });
+
+    it('Avatar presence bottom-left', () => {
+        const element = createElement('base-avatar', {
+            is: Avatar
+        });
+        document.body.appendChild(element);
+
+        element.presence = 'online';
+        element.presencePosition = 'bottom-left';
+        element.hideAvatarDetails = true;
+        element.src =
+            'https://www.lightningdesignsystem.com/assets/images/avatar2.jpg';
+
+        return Promise.resolve().then(() => {
+            const presence = element.shadowRoot.querySelector(
+                '.avonni-avatar__presence.avonni-avatar__presence_online.avonni-avatar_bottom-left'
+            );
+            expect(presence).toBeTruthy();
+        });
+    });
+
+    it('Avatar presence top-left', () => {
+        const element = createElement('base-avatar', {
+            is: Avatar
+        });
+        document.body.appendChild(element);
+
+        element.presence = 'online';
+        element.presencePosition = 'top-left';
+        element.hideAvatarDetails = true;
+        element.src =
+            'https://www.lightningdesignsystem.com/assets/images/avatar2.jpg';
+
+        return Promise.resolve().then(() => {
+            const presence = element.shadowRoot.querySelector(
+                '.avonni-avatar__presence.avonni-avatar__presence_online.avonni-avatar_top-left'
+            );
+            expect(presence).toBeTruthy();
+        });
+    });
+
+    it('Avatar presence top-right', () => {
+        const element = createElement('base-avatar', {
+            is: Avatar
+        });
+        document.body.appendChild(element);
+
+        element.presence = 'online';
+        element.presencePosition = 'top-right';
+        element.hideAvatarDetails = true;
+        element.src =
+            'https://www.lightningdesignsystem.com/assets/images/avatar2.jpg';
+
+        return Promise.resolve().then(() => {
+            const presence = element.shadowRoot.querySelector(
+                '.avonni-avatar__presence.avonni-avatar__presence_online.avonni-avatar_top-right'
+            );
+            expect(presence).toBeTruthy();
         });
     });
 });
