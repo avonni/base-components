77a1610bdb686062c18fd1e4d3a4a5c9bba9053a
Renaud Archer-Fortin <71739112+renaudaf@users.noreply.github.com>
2021-04-13 14:10:18 -0400

    added size, vvariant and items tests

--- src/modules/base/avatarGroup/__tests__/avatarGroup.test.js
+++ src/modules/base/avatarGroup/__tests__/avatarGroup.test.js
@@ -43,6 +43,9 @@ describe('Avatar Group', () => {
         expect(element.listButtonIconPosition).toBe('left');
     });
 
+    /* ----- ATTRIBUTES ----- */
+
+    // size
     it('Avatar group size x-small', () => {
         const element = createElement('base-avatar-group', {
             is: AvatarGroup
@@ -50,13 +53,221 @@ describe('Avatar Group', () => {
         document.body.appendChild(element);
 
         element.size = 'x-small';
-        element.items = [...items, ...items, ...items];
+        element.items = items;
 
         return Promise.resolve().then(() => {
-            const div = element.shadowRoot.querySelector(
-                '.slds-avatar_x-small'
+            const avatars = element.shadowRoot.querySelectorAll(
+                '.slds-avatar-group'
             );
-            expect(div).toBe('avonni-avatar_x-small');
+            avatars.forEach((avatar) => {
+                expect(avatar.className).toContain('slds-avatar-group_x-small');
+            });
+        });
+    });
+
+    it('Avatar group size small', () => {
+        const element = createElement('base-avatar-group', {
+            is: AvatarGroup
+        });
+        document.body.appendChild(element);
+
+        element.size = 'small';
+        element.items = items;
+
+        return Promise.resolve().then(() => {
+            const avatars = element.shadowRoot.querySelectorAll(
+                '.slds-avatar-group'
+            );
+            avatars.forEach((avatar) => {
+                expect(avatar.className).toContain('slds-avatar-group_small');
+            });
+        });
+    });
+
+    it('Avatar group size medium', () => {
+        const element = createElement('base-avatar-group', {
+            is: AvatarGroup
+        });
+        document.body.appendChild(element);
+
+        element.size = 'medium';
+        element.items = items;
+
+        return Promise.resolve().then(() => {
+            const avatars = element.shadowRoot.querySelectorAll(
+                '.slds-avatar-group'
+            );
+            avatars.forEach((avatar) => {
+                expect(avatar.className).toContain('slds-avatar-group_medium');
+            });
+        });
+    });
+
+    it('Avatar group size large', () => {
+        const element = createElement('base-avatar-group', {
+            is: AvatarGroup
+        });
+        document.body.appendChild(element);
+
+        element.size = 'large';
+        element.items = items;
+
+        return Promise.resolve().then(() => {
+            const avatars = element.shadowRoot.querySelectorAll(
+                '.slds-avatar-group'
+            );
+            avatars.forEach((avatar) => {
+                expect(avatar.className).toContain('slds-avatar-group_large');
+            });
+        });
+    });
+
+    it('Avatar group size xx-large', () => {
+        const element = createElement('base-avatar-group', {
+            is: AvatarGroup
+        });
+        document.body.appendChild(element);
+
+        element.size = 'xx-large';
+        element.items = items;
+
+        return Promise.resolve().then(() => {
+            const avatars = element.shadowRoot.querySelectorAll(
+                '.slds-avatar-group'
+            );
+            avatars.forEach((avatar) => {
+                expect(avatar.className).toContain(
+                    'avonni-avatar-group_xx-large'
+                );
+            });
+        });
+    });
+
+    // Variant
+    it('Avatar group variant square', () => {
+        const element = createElement('base-avatar-group', {
+            is: AvatarGroup
+        });
+        document.body.appendChild(element);
+
+        element.variant = 'square';
+        element.items = items;
+
+        return Promise.resolve().then(() => {
+            const avatars = element.shadowRoot.querySelectorAll(
+                '.slds-avatar-group'
+            );
+            avatars.forEach((avatar) => {
+                expect(avatar.className).not.toContain(
+                    'avonni-avatar-group_circle'
+                );
+            });
+        });
+    });
+
+    it('Avatar group variant circle', () => {
+        const element = createElement('base-avatar-group', {
+            is: AvatarGroup
+        });
+        document.body.appendChild(element);
+
+        element.variant = 'circle';
+        element.items = items;
+
+        return Promise.resolve().then(() => {
+            const avatars = element.shadowRoot.querySelectorAll(
+                '.slds-avatar-group'
+            );
+            avatars.forEach((avatar) => {
+                expect(avatar.className).toContain(
+                    'avonni-avatar-group_circle'
+                );
+            });
+        });
+    });
+
+    // items
+    it('Avatar group items', () => {
+        const element = createElement('base-avatar-group', {
+            is: AvatarGroup
+        });
+        document.body.appendChild(element);
+
+        const item = [
+            {
+                src:
+                    'https://www.lightningdesignsystem.com/assets/images/avatar1.jpg',
+                fallbackIconName: 'standard:user',
+                alternativeText: 'This is the alternative text',
+                status: 'locked',
+                statusTitle: 'Locked',
+                statusPosition: 'top-left',
+                entityIconName: 'standard:account',
+                entitySrc:
+                    'https://www.lightningdesignsystem.com/assets/images/avatar1.jpg',
+                entityInitials: 'FC',
+                entityVariant: 'circle',
+                entityPosition: 'bottom-right',
+                primaryText: 'John Doe',
+                secondaryText: 'VP, Human Resources',
+                tertiaryText: 'FakeCompany Inc.',
+                initials: 'JD',
+                presence: 'busy',
+                presenceTitle: 'Busy',
+                presencePosition: 'top-right'
+            }
+        ];
+
+        element.variant = 'circle';
+        element.size = 'xx-large';
+        element.items = item;
+
+        return Promise.resolve().then(() => {
+            const avatars = element.shadowRoot.querySelectorAll('c-avatar');
+            avatars.forEach((avatar, index) => {
+                const correspondingField = item[index];
+                expect(correspondingField).toBeTruthy();
+                expect(avatar.alternativeText).toBe(
+                    correspondingField.alternativeText
+                );
+                expect(avatar.fallbackIconName).toBe(
+                    correspondingField.fallbackIconName
+                );
+                expect(avatar.initials).toBe(correspondingField.initials);
+                expect(avatar.src).toBe(correspondingField.src);
+                expect(avatar.status).toBe(correspondingField.status);
+                expect(avatar.statusTitle).toBe(correspondingField.statusTitle);
+                expect(avatar.statusPosition).toBe(
+                    correspondingField.statusPosition
+                );
+                expect(avatar.presence).toBe(correspondingField.presence);
+                expect(avatar.presenceTitle).toBe(
+                    correspondingField.presenceTitle
+                );
+                expect(avatar.presencePosition).toBe(
+                    correspondingField.presencePosition
+                );
+                expect(avatar.entityIconName).toBe(
+                    correspondingField.entityIconName
+                );
+                expect(avatar.entityInitials).toBe(
+                    correspondingField.entityInitials
+                );
+                expect(avatar.entityVariant).toBe(
+                    correspondingField.entityVariant
+                );
+                expect(avatar.entitySrc).toBe(correspondingField.entitySrc);
+                expect(avatar.entityPosition).toBe(
+                    correspondingField.entityPosition
+                );
+                expect(avatar.primaryText).toBe(correspondingField.primaryText);
+                expect(avatar.secondaryText).toBe(
+                    correspondingField.secondaryText
+                );
+                expect(avatar.tertiaryText).toBe(
+                    correspondingField.tertiaryText
+                );
+            });
         });
     });
 });
