40f8a9eeedd21aba527d2930d328b34c90eb491c
Renaud Archer-Fortin <71739112+renaudaf@users.noreply.github.com>
2021-04-14 15:38:19 -0400

    added tests for accesskey, alternativeText, disabled and variant

--- src/modules/base/buttonIconPopover/__tests__/buttonIconPopover.test.js
+++ src/modules/base/buttonIconPopover/__tests__/buttonIconPopover.test.js
@@ -1,3 +1,198 @@
-describe('ButtonIconPopover', () => {
-    test.todo('please pass');
+import { createElement } from 'lwc';
+import ButtonIconPopover from 'c/buttonIconPopover';
+
+describe('Button Icon Popover', () => {
+    afterEach(() => {
+        while (document.body.firstChild) {
+            document.body.removeChild(document.body.firstChild);
+        }
+    });
+
+    it('Default attributes', () => {
+        const element = createElement('base-button-icon-popover', {
+            is: ButtonIconPopover
+        });
+
+        expect(element.accessKey).toBeUndefined();
+        expect(element.alternativeText).toBeUndefined();
+        expect(element.disabled).toBeFalsy();
+        expect(element.variant).toBe('border');
+        expect(element.size).toBe('medium');
+        expect(element.tooltip).toBeUndefined();
+        expect(element.iconClass).toBeUndefined();
+        expect(element.iconName).toBeUndefined();
+        expect(element.title).toBeUndefined();
+        expect(element.popoverSize).toBe('medium');
+        expect(element.placement).toBe('left');
+        expect(element.isLoading).toBeFalsy();
+        expect(element.loadingStateAlternativeText).toBeUndefined();
+        expect(element.triggers).toBe('click');
+        expect(element.popoverVariant).toBe('base');
+    });
+
+    /* ----- ATTRIBUTES ----- */
+
+    // access-key
+    it('Button Icon Popover access-key', () => {
+        const element = createElement('base-button-icon-popover', {
+            is: ButtonIconPopover
+        });
+        document.body.appendChild(element);
+
+        element.accessKey = 'K';
+        const button = element.shadowRoot.querySelector(
+            'lightning-button-icon'
+        );
+
+        return Promise.resolve().then(() => {
+            expect(button.accessKey).toBe('K');
+        });
+    });
+
+    // alternative-text
+    it('Button Icon Popover alternative-text', () => {
+        const element = createElement('base-button-icon-popover', {
+            is: ButtonIconPopover
+        });
+        document.body.appendChild(element);
+
+        element.alternativeText = 'This is an alternative text';
+        const button = element.shadowRoot.querySelector(
+            'lightning-button-icon'
+        );
+
+        return Promise.resolve().then(() => {
+            expect(button.alternativeText).toBe('This is an alternative text');
+        });
+    });
+
+    // disabled
+    it('Button Icon Popover disabled', () => {
+        const element = createElement('base-button-icon-popover', {
+            is: ButtonIconPopover
+        });
+        document.body.appendChild(element);
+
+        element.disabled = true;
+        const button = element.shadowRoot.querySelector(
+            'lightning-button-icon'
+        );
+
+        return Promise.resolve().then(() => {
+            expect(button.disabled).toBeTruthy();
+        });
+    });
+
+    // variant
+    it('Button Icon Popover variant border', () => {
+        const element = createElement('base-button-icon-popover', {
+            is: ButtonIconPopover
+        });
+        document.body.appendChild(element);
+
+        element.variant = 'border';
+        const button = element.shadowRoot.querySelector(
+            'lightning-button-icon'
+        );
+
+        return Promise.resolve().then(() => {
+            expect(button.variant).toBe('border');
+        });
+    });
+
+    it('Button Icon Popover variant bare', () => {
+        const element = createElement('base-button-icon-popover', {
+            is: ButtonIconPopover
+        });
+        document.body.appendChild(element);
+
+        element.variant = 'bare';
+        const button = element.shadowRoot.querySelector(
+            'lightning-button-icon'
+        );
+
+        return Promise.resolve().then(() => {
+            expect(button.variant).toBe('bare');
+        });
+    });
+
+    it('Button Icon Popover variant container', () => {
+        const element = createElement('base-button-icon-popover', {
+            is: ButtonIconPopover
+        });
+        document.body.appendChild(element);
+
+        element.variant = 'container';
+        const button = element.shadowRoot.querySelector(
+            'lightning-button-icon'
+        );
+
+        return Promise.resolve().then(() => {
+            expect(button.variant).toBe('container');
+        });
+    });
+
+    it('Button Icon Popover variant brand', () => {
+        const element = createElement('base-button-icon-popover', {
+            is: ButtonIconPopover
+        });
+        document.body.appendChild(element);
+
+        element.variant = 'brand';
+        const button = element.shadowRoot.querySelector(
+            'lightning-button-icon'
+        );
+
+        return Promise.resolve().then(() => {
+            expect(button.variant).toBe('brand');
+        });
+    });
+
+    it('Button Icon Popover variant border-filled', () => {
+        const element = createElement('base-button-icon-popover', {
+            is: ButtonIconPopover
+        });
+        document.body.appendChild(element);
+
+        element.variant = 'border-filled';
+        const button = element.shadowRoot.querySelector(
+            'lightning-button-icon'
+        );
+
+        return Promise.resolve().then(() => {
+            expect(button.variant).toBe('border-filled');
+        });
+    });
+
+    it('Button Icon Popover variant bare-inverse', () => {
+        const element = createElement('base-button-icon-popover', {
+            is: ButtonIconPopover
+        });
+        document.body.appendChild(element);
+
+        element.variant = 'bare-inverse';
+        const button = element.shadowRoot.querySelector(
+            'lightning-button-icon'
+        );
+
+        return Promise.resolve().then(() => {
+            expect(button.variant).toBe('bare-inverse');
+        });
+    });
+
+    it('Button Icon Popover variant border-inverse', () => {
+        const element = createElement('base-button-icon-popover', {
+            is: ButtonIconPopover
+        });
+        document.body.appendChild(element);
+
+        element.variant = 'border-inverse';
+        const button = element.shadowRoot.querySelector(
+            'lightning-button-icon'
+        );
+
+        return Promise.resolve().then(() => {
+            expect(button.variant).toBe('border-inverse');
+        });
+    });
 });
