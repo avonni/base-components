6231af9693797ef7e005bd37628e981bdf20444e
Renaud Archer-Fortin <71739112+renaudaf@users.noreply.github.com>
2021-04-15 16:25:42 -0400

    added test for attributes for calendar

--- src/modules/base/calendar/__tests__/calendar.test.js
+++ src/modules/base/calendar/__tests__/calendar.test.js
@@ -1,3 +1,25 @@
+import { createElement } from 'lwc';
+import Calendar from 'c/calendar';
+
 describe('Calendar', () => {
-    test.todo('please pass');
+    afterEach(() => {
+        while (document.body.firstChild) {
+            document.body.removeChild(document.body.firstChild);
+        }
+    });
+
+    it('Default attributes', () => {
+        const element = createElement('base-calendar', {
+            is: Calendar
+        });
+
+        expect(element.value).toBeUndefined();
+        expect(element.disabled).toBeFalsy();
+        expect(element.disabledDates).toMatchObject([]);
+        expect(element.markedDates).toMatchObject([]);
+        expect(element.max).toMatchObject(new Date(2099, 11, 31));
+        expect(element.min).toMatchObject(new Date(1900, 0, 1));
+        expect(element.weekNumber).toBeFalsy();
+        expect(element.multiValue).toBeUndefined();
+    });
 });
