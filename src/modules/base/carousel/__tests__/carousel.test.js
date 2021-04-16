9201f51b21e05e6793403937772a73dcfe6757f5
Renaud Archer-Fortin <71739112+renaudaf@users.noreply.github.com>
2021-04-16 12:02:14 -0400

    added tests for default attributes for carousel

--- src/modules/base/carousel/__tests__/carousel.test.js
+++ src/modules/base/carousel/__tests__/carousel.test.js
@@ -1,3 +1,32 @@
+import { createElement } from 'lwc';
+import Carousel from 'c/carousel';
+
 describe('Carousel', () => {
-    test.todo('please pass');
+    afterEach(() => {
+        while (document.body.firstChild) {
+            document.body.removeChild(document.body.firstChild);
+        }
+    });
+
+    it('Default attributes', () => {
+        const element = createElement('base-carousel', {
+            is: Carousel
+        });
+
+        expect(element.assistiveText).toMatchObject({
+            autoplayButton: 'Start / Stop auto-play',
+            nextPanel: 'Next Panel',
+            previousPanel: 'Previous Panel'
+        });
+        expect(element.items).toMatchObject([]);
+        expect(element.disableAutoRefresh).toBeFalsy();
+        expect(element.disableAutoScroll).toBeFalsy();
+        expect(element.scrollDuration).toBe(5);
+        expect(element.indicatorVariant).toBe('base');
+        expect(element.isIfinite).toBeFalsy();
+        expect(element.currentPanel).toBeUndefined();
+        expect(element.hideIndicator).toBeFalsy();
+        expect(element.hidePreviousNextPanelNavigation).toBeFalsy();
+        expect(element.itemsPerPanel).toBe(1);
+    });
 });
