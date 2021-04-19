81ced29eb7404c2eccb27398c9b805d0705eb9be
Renaud Archer-Fortin <71739112+renaudaf@users.noreply.github.com>
2021-04-19 13:34:47 -0400

    added variantvalue and events

--- src/modules/base/checkboxGroup/__tests__/checkboxGroup.test.js
+++ src/modules/base/checkboxGroup/__tests__/checkboxGroup.test.js
@@ -112,6 +112,7 @@ describe('Checkbox Group', () => {
     });
 
     // Message when value is missing
+    // Depends on required, focus(), blur() and showHelpMessageIfInvalid()
     it('Checkbox Group message when value is missing', () => {
         const element = createElement('base-checkbox-group', {
             is: CheckboxGroup
@@ -136,6 +137,43 @@ describe('Checkbox Group', () => {
             });
     });
 
+    // name
+    it('Checkbox Group name', () => {
+        const element = createElement('base-checkbox-group', {
+            is: CheckboxGroup
+        });
+        document.body.appendChild(element);
+
+        element.options = options;
+        element.name = 'Checkbox group name';
+
+        return Promise.resolve().then(() => {
+            const inputs = element.shadowRoot.querySelectorAll('input');
+            inputs.forEach((input) => {
+                expect(input.name).toBe('Checkbox group name');
+            });
+        });
+    });
+
+    // options
+    it('Checkbox Group options', () => {
+        const element = createElement('base-checkbox-group', {
+            is: CheckboxGroup
+        });
+        document.body.appendChild(element);
+
+        element.options = options;
+
+        return Promise.resolve().then(() => {
+            options.forEach((option, index) => {
+                const correspondingOption = options[index];
+                expect(correspondingOption).toBeTruthy();
+                expect(option.label).toBe(correspondingOption.label);
+                expect(option.value).toBe(correspondingOption.value);
+            });
+        });
+    });
+
     // required
     it('Checkbox Group required', () => {
         const element = createElement('base-checkbox-group', {
@@ -149,6 +187,182 @@ describe('Checkbox Group', () => {
         return Promise.resolve().then(() => {
             const abbr = element.shadowRoot.querySelector('abbr');
             expect(abbr).toBeTruthy();
+        });
+    });
+
+    // value
+    it('Checkbox Group value', () => {
+        const element = createElement('base-checkbox-group', {
+            is: CheckboxGroup
+        });
+        document.body.appendChild(element);
+
+        element.options = options;
+        element.value = ['mon', 'wed'];
+
+        return Promise.resolve().then(() => {
+            const values = [];
+            const inputs = element.shadowRoot.querySelectorAll('input');
+            inputs.forEach((input) => {
+                if (input.checked) {
+                    values.push(input.value);
+                }
+            });
+            expect(values).toHaveLength(2);
+        });
+    });
+
+    // variant
+    it('Checkbox Group variant standard', () => {
+        const element = createElement('base-checkbox-group', {
+            is: CheckboxGroup
+        });
+        document.body.appendChild(element);
+
+        element.options = options;
+
+        return Promise.resolve().then(() => {
+            expect(element.className).not.toContain(
+                'slds-form-element_stacked'
+            );
+            expect(element.className).not.toContain(
+                'slds-form-element_horizontal'
+            );
+            const label = element.shadowRoot.querySelector(
+                '.slds-form-element__legend.slds-form-element__label'
+            );
+            expect(label.className).not.toContain('slds-assistive-text');
+        });
+    });
+
+    it('Checkbox Group variant label hidden', () => {
+        const element = createElement('base-checkbox-group', {
+            is: CheckboxGroup
+        });
+        document.body.appendChild(element);
+
+        element.options = options;
+        element.variant = 'label-hidden';
+
+        return Promise.resolve().then(() => {
+            expect(element.className).not.toContain(
+                'slds-form-element_stacked'
+            );
+            expect(element.className).not.toContain(
+                'slds-form-element_horizontal'
+            );
+            const label = element.shadowRoot.querySelector(
+                '.slds-form-element__legend.slds-form-element__label'
+            );
+            expect(label.className).toContain('slds-assistive-text');
+        });
+    });
+
+    it('Checkbox Group variant label inline', () => {
+        const element = createElement('base-checkbox-group', {
+            is: CheckboxGroup
+        });
+        document.body.appendChild(element);
+
+        element.options = options;
+        element.variant = 'label-inline';
+
+        return Promise.resolve().then(() => {
+            expect(element.className).not.toContain(
+                'slds-form-element_stacked'
+            );
+            expect(element.className).toContain('slds-form-element_horizontal');
+            const label = element.shadowRoot.querySelector(
+                '.slds-form-element__legend.slds-form-element__label'
+            );
+            expect(label.className).not.toContain('slds-assistive-text');
+        });
+    });
+
+    it('Checkbox Group variant label stacked', () => {
+        const element = createElement('base-checkbox-group', {
+            is: CheckboxGroup
+        });
+        document.body.appendChild(element);
+
+        element.options = options;
+        element.variant = 'label-stacked';
+
+        return Promise.resolve().then(() => {
+            expect(element.className).toContain('slds-form-element_stacked');
+            expect(element.className).not.toContain(
+                'slds-form-element_horizontal'
+            );
+            const label = element.shadowRoot.querySelector(
+                '.slds-form-element__legend.slds-form-element__label'
+            );
+            expect(label.className).not.toContain('slds-assistive-text');
+        });
+    });
+
+    /* ----- EVENTS ----- */
+
+    // change event
+    it('Checkbox group change event', () => {
+        const element = createElement('base-checkbox-group', {
+            is: CheckboxGroup
+        });
+        document.body.appendChild(element);
+
+        element.options = options;
+
+        return Promise.resolve().then(() => {
+            const input = element.shadowRoot.querySelector('input');
+            const handleChange = (event) => {
+                expect(event.detail).toBeTruthy();
+                expect(event.bubbles).toBeTruthy();
+                expect(event.cancelable).toBeTruthy();
+                expect(event.composed).toBeTruthy();
+            };
+            element.addEventListener('change', handleChange);
+            input.click();
+        });
+    });
+
+    // blur event
+    it('Checkbox group blur event', () => {
+        const element = createElement('base-checkbox-group', {
+            is: CheckboxGroup
+        });
+        document.body.appendChild(element);
+
+        element.options = options;
+
+        return Promise.resolve().then(() => {
+            const input = element.shadowRoot.querySelector('input');
+            const handleBlur = (event) => {
+                expect(event.bubbles).toBeFalsy();
+                expect(event.cancelable).toBeFalsy();
+                expect(event.composed).toBeFalsy();
+            };
+            element.addEventListener('blur', handleBlur);
+            input.blur();
+        });
+    });
+
+    // focus event
+    it('Checkbox group focus event', () => {
+        const element = createElement('base-checkbox-group', {
+            is: CheckboxGroup
+        });
+        document.body.appendChild(element);
+
+        element.options = options;
+
+        return Promise.resolve().then(() => {
+            const input = element.shadowRoot.querySelector('input');
+            const handleFocus = (event) => {
+                expect(event.bubbles).toBeFalsy();
+                expect(event.cancelable).toBeFalsy();
+                expect(event.composed).toBeFalsy();
+            };
+            element.addEventListener('focus', handleFocus);
+            input.focus();
         });
     });
 });
