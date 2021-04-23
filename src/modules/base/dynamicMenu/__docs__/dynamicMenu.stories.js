1a3af313cea694fdc258ff65966f8f9c76810539
Renaud Archer-Fortin <71739112+renaudaf@users.noreply.github.com>
2021-04-23 15:55:31 -0400

    added info to dynamicMenu in storybook

--- src/modules/base/dynamicMenu/__docs__/dynamicMenu.stories.js
+++ src/modules/base/dynamicMenu/__docs__/dynamicMenu.stories.js
@@ -6,50 +6,95 @@ export default {
         value: {
             control: {
                 type: 'text'
+            },
+            description:
+                'The value for the button element. This value is optional and can be used when submitting a form.',
+            table: {
+                type: { summary: 'string' }
             }
         },
         alternativeText: {
+            name: 'alternative-text',
             control: {
                 type: 'text'
+            },
+            description: 'The assistive text for the button.',
+            table: {
+                type: { summary: 'string' }
             }
         },
         loadingStateAlternativeText: {
+            name: 'loading-state-alternative-text',
             control: {
                 type: 'text'
+            },
+            description:
+                'Message displayed while the menu is in the loading state.',
+            table: {
+                type: { summary: 'string' }
             }
         },
         label: {
             control: {
                 type: 'text'
+            },
+            description: 'Optional text to be shown on the button.',
+            table: {
+                type: { summary: 'string' }
             }
         },
         accessKey: {
+            name: 'access-key',
             control: {
                 type: 'text'
+            },
+            description: 'The keyboard shortcut for the button.',
+            table: {
+                type: { summary: 'string' }
             }
         },
         title: {
             control: {
                 type: 'text'
+            },
+            description:
+                'Displays tooltip text when the mouse moves over the button menu.',
+            table: {
+                type: { summary: 'string' }
             }
         },
         searchInputPlaceholder: {
+            name: 'search-input-placeholder',
             control: {
                 type: 'text'
             },
             defaultValue: 'Search…',
+            description:
+                'Text that is displayed when the field is empty, to prompt the user for a valid entry.',
             table: {
-                defaultValue: { summary: 'Search…' }
+                defaultValue: { summary: 'Search…' },
+                type: { summary: 'string' }
             }
         },
         tooltip: {
             control: {
                 type: 'text'
+            },
+            description:
+                'Text to display when the user mouses over or focuses on the button. The tooltip is auto-positioned relative to the button and screen space.',
+            table: {
+                type: { summary: 'string' }
             }
         },
         iconName: {
+            name: 'icon-name',
             control: {
                 type: 'text'
+            },
+            description:
+                "The name of the icon to be used in the format 'utility:down'.",
+            table: {
+                type: { summary: 'string' }
             }
         },
         variant: {
@@ -57,19 +102,24 @@ export default {
                 type: 'select'
             },
             options: [
-                'border',
-                'border-inverse',
-                'border-filled',
                 'bare',
+                'container',
+                'brand',
+                'border',
+                'border-filled',
                 'bare-inverse',
-                'container'
+                'border-inverse'
             ],
             defaultValue: 'border',
+            description:
+                'The variant changes the appearance of the button. Accepted variants include base, neutral, brand, brand-outline, destructive, destructive-text, inverse, and success.',
             table: {
+                type: { summary: 'string' },
                 defaultValue: { summary: 'border' }
             }
         },
         menuAlignment: {
+            name: 'menu-alignement',
             control: {
                 type: 'select'
             },
@@ -82,25 +132,35 @@ export default {
                 'bottom-right'
             ],
             defaultValue: 'left',
+            description:
+                'Determines the alignment of the menu relative to the button. Available options are: auto, left, center, right, bottom-left, bottom-center, bottom-right. The auto option aligns the dropdown menu based on available space.',
             table: {
+                type: { summary: 'string' },
                 defaultValue: { summary: 'left' }
             }
         },
         withSearch: {
+            name: 'with-search',
             control: {
                 type: 'boolean'
             },
             defaultValue: 0,
+            description: 'If present, display search box.',
             table: {
+                type: { summary: 'boolean' },
                 defaultValue: { summary: 'false' }
             }
         },
         isLoading: {
+            name: 'is-loading',
             control: {
                 type: 'boolean'
             },
             defaultValue: 0,
+            description:
+                'If present, the menu is in a loading state and shows a spinner.',
             table: {
+                type: { summary: 'boolean' },
                 defaultValue: { summary: 'false' }
             }
         },
@@ -109,13 +169,20 @@ export default {
                 type: 'boolean'
             },
             defaultValue: 0,
+            description: 'If present, the menu can be opened by users.',
             table: {
+                type: { summary: 'boolean' },
                 defaultValue: { summary: 'false' }
             }
         },
         items: {
             control: {
                 type: 'object'
+            },
+            description:
+                'Fields: label, meta, id, value, avatar : {fallbackIconName, initials, src, alternativeText, size, variant}',
+            table: {
+                type: { summary: 'object[]' }
             }
         }
     },
