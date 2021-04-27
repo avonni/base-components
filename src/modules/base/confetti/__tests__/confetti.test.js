2ba5569ce8f3ff985d10ecb240de522582037d61
Renaud Archer-Fortin <71739112+renaudaf@users.noreply.github.com>
2021-04-27 15:09:08 -0400

    added default attributes test and change default in js file for confetti

--- src/modules/base/confetti/__tests__/confetti.test.js
+++ src/modules/base/confetti/__tests__/confetti.test.js
@@ -1,3 +1,48 @@
+import { createElement } from 'lwc';
+import Confetti from 'c/confetti';
+
 describe('Confetti', () => {
-    test.todo('please pass');
+    afterEach(() => {
+        while (document.body.firstChild) {
+            document.body.removeChild(document.body.firstChild);
+        }
+    });
+
+    it('Confetti Default attributes', () => {
+        const element = createElement('base-confetti', {
+            is: Confetti
+        });
+
+        expect(element.variant).toBe('base');
+        expect(element.name).toBeUndefined();
+        expect(element.colors).toMatchObject([
+            '#529EE0',
+            '#F0E442',
+            '#FFB03B',
+            '#E16032',
+            '#4FD2D2',
+            '#006699',
+            '#E287B2'
+        ]);
+        expect(element.originX).toBe(0.5)
+        expect(element.originY).toBe(0.5)
+        expect(element.zIndex).toBe(100)
+    });
+
+    /* ----- ATTRIBUTES ----- */
+
+    // label
+    it('Confetti label', () => {
+        const element = createElement('base-confetti', {
+            is: Confetti
+        });
+        document.body.appendChild(element);
+
+        element.label = 'This is a label';
+
+        return Promise.resolve().then(() => {
+            const span = element.shadowRoot.querySelector('span');
+            expect(span.textContent).toBe('This is a label');
+        });
+    });
 });
