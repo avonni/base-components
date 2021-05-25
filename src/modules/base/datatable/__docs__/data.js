96e22895490f80ce4b1290a1d32f015adcd03185
Nawel Aammar <nawel.aa@hotmail.fr>
2021-05-25 16:18:14 -0400

    [doc] (ABC-190): Add regular data types to Storybook's tests

--- src/modules/base/datatable/__docs__/data.js
+++ src/modules/base/datatable/__docs__/data.js
@@ -31,6 +31,25 @@ const avatarGroupItems = [
 
 const columnsAB = [
     {
+        label: 'Action',
+        type: 'action',
+        typeAttributes: {
+            rowActions: [
+                {
+                    label: 'Add',
+                    name: 'add',
+                    iconName: 'utility:add'
+                },
+                {
+                    label: 'Delete',
+                    name: 'delete',
+                    iconName: 'utility:delete'
+                }
+            ],
+            menuAlignment: 'left'
+        }
+    },
+    {
         label: 'Avatar',
         fieldName: 'avatar',
         type: 'avatar',
@@ -58,6 +77,28 @@ const columnsAB = [
             variant: 'circle',
             maxCount: 3,
             size: 'small'
+        }
+    },
+    {
+        label: 'Boolean',
+        fieldName: 'boolean',
+        type: 'boolean',
+        editable: true
+    },
+    {
+        label: 'Button',
+        type: 'button',
+        typeAttributes: {
+            disabled: { fieldName: 'buttonDisabled' },
+            label: { fieldName: 'avatarPrimaryText' }
+        }
+    },
+    {
+        label: 'Button icon',
+        type: 'button-icon',
+        typeAttributes: {
+            disabled: { fieldName: 'buttonIconDisabled' },
+            iconName: 'utility:alert'
         }
     }
 ];
@@ -90,6 +131,7 @@ const columnsCE = [
             label: 'Pick a color',
             opacity: true
         },
+        fixedWidth: 190,
         editable: true
     },
     {
@@ -102,6 +144,30 @@ const columnsCE = [
         editable: true
     },
     {
+        label: 'Date',
+        fieldName: 'date',
+        type: 'date',
+        typeAttributes: {
+            day: 'numeric',
+            month: 'long',
+            year: 'numeric',
+            hour: '2-digit',
+            timeZone: 'Pacific/Honolulu'
+        },
+        editable: true
+    },
+    {
+        label: 'Date local',
+        fieldName: 'date',
+        type: 'date-local',
+        typeAttributes: {
+            day: 'numeric',
+            month: 'long',
+            year: 'numeric'
+        },
+        editable: true
+    },
+    {
         label: 'Dynamic icon',
         fieldName: 'dynamicIcon',
         type: 'dynamic-icon',
@@ -109,6 +175,12 @@ const columnsCE = [
             alternativeText: { fieldName: 'dynamicIcon' },
             option: { fieldName: 'dynamicIconOption' }
         }
+    },
+    {
+        label: 'Email',
+        fieldName: 'email',
+        type: 'email',
+        editable: true
     }
 ];
 
@@ -170,10 +242,37 @@ const columnsFN = [
         cellAttributes: {
             alignment: 'center'
         }
+    },
+    {
+        label: 'Location',
+        type: 'location',
+        fieldName: 'location',
+        editable: true
+    },
+    {
+        label: 'Number',
+        type: 'number',
+        fieldName: 'number',
+        editable: true,
+        typeAttributes: {
+            minimumFractionDigits: 2
+        }
     }
 ];
 
 const columnsOQ = [
+    {
+        label: 'Percent',
+        fieldName: 'percent',
+        type: 'percent',
+        editable: true
+    },
+    {
+        label: 'Phone',
+        fieldName: 'phone',
+        type: 'phone',
+        editable: true
+    },
     {
         label: 'Progress Bar',
         fieldName: 'progress',
@@ -183,7 +282,8 @@ const columnsOQ = [
             theme: { fieldName: 'progressBarTheme' },
             variant: 'circular',
             thickness: 'large'
-        }
+        },
+        initialWidth: 150
     },
     {
         label: 'Progress Circle',
@@ -226,7 +326,8 @@ const columnsRZ = [
             label: 'Rating on 5',
             disabled: { fieldName: 'ratingDisabled' }
         },
-        editable: true
+        editable: true,
+        initialWidth: 200
     },
     {
         label: 'Slider',
@@ -238,6 +339,20 @@ const columnsRZ = [
             step: { fieldName: 'sliderStep' }
         },
         editable: true
+    },
+    {
+        label: 'Text',
+        fieldName: 'text',
+        editable: true
+    },
+    {
+        label: 'URL',
+        fieldName: 'url',
+        type: 'url',
+        typeAttributes: {
+            label: { fieldName: 'urlLabel' },
+            target: '_blank'
+        }
     }
 ];
 
@@ -255,7 +370,8 @@ const dataAB = [
             avatarGroupItems[1],
             avatarGroupItems[4],
             avatarGroupItems[3]
-        ]
+        ],
+        boolean: true
     },
     {
         id: 2,
@@ -271,7 +387,8 @@ const dataAB = [
             avatarGroupItems[3],
             avatarGroupItems[4],
             avatarGroupItems[2]
-        ]
+        ],
+        boolean: false
     },
     {
         id: 3,
@@ -285,7 +402,9 @@ const dataAB = [
             avatarGroupItems[0],
             avatarGroupItems[1],
             avatarGroupItems[3]
-        ]
+        ],
+        buttonDisabled: true,
+        buttonIconDisabled: true
     },
     {
         id: 4,
@@ -297,7 +416,8 @@ const dataAB = [
             avatarGroupItems[1],
             avatarGroupItems[2],
             avatarGroupItems[0]
-        ]
+        ],
+        boolean: true
     },
     {
         id: 5,
@@ -322,14 +442,18 @@ const dataCE = [
         id: 1,
         colorPicker: '#00a1e0',
         currency: 200,
-        dynamicIcon: 'ellie'
+        dynamicIcon: 'ellie',
+        date: new Date('2022/03/24'),
+        email: 'nina.gomez@email.com'
     },
     {
         id: 2,
         currency: 230,
         colorPicker: '#e65cd1',
         dynamicIcon: 'score',
-        dynamicIconOption: 'negative'
+        dynamicIconOption: 'negative',
+        date: new Date('2022/03/21'),
+        email: 'dave.mckinsley@email.com'
     },
     {
         id: 3,
@@ -338,20 +462,26 @@ const dataCE = [
         currency: 3045,
         colorPickerDisabled: true,
         dynamicIcon: 'strength',
-        dynamicIconOption: -3
+        dynamicIconOption: -3,
+        date: new Date('2022/05/04'),
+        email: 'jung.phung@email.com'
     },
     {
         id: 4,
         colorPicker: '#f4bc25',
         currency: 432,
-        dynamicIcon: 'eq'
+        dynamicIcon: 'eq',
+        date: new Date('2021/02/14'),
+        email: 'lily.murray@email.com'
     },
     {
         id: 5,
         checkboxButton: true,
         colorPicker: '#f99120',
         currency: 217,
-        dynamicIcon: 'waffle'
+        dynamicIcon: 'waffle',
+        date: new Date('2022/10/12'),
+        email: 'reginald.martin@email.com'
     }
 ];
 const dataFN = [
@@ -362,7 +492,12 @@ const dataFN = [
         inputDateRange: {
             startDate: new Date('2021/10/02'),
             endDate: new Date('2021/10/05')
-        }
+        },
+        location: {
+            latitude: '45.53',
+            longitude: '-73.61'
+        },
+        number: 6
     },
     {
         id: 2,
@@ -373,7 +508,12 @@ const dataFN = [
             startDate: new Date('2021/09/12'),
             endDate: new Date('2021/10/05')
         },
-        inputToggle: true
+        inputToggle: true,
+        location: {
+            latitude: '45.53',
+            longitude: '-73.58'
+        },
+        number: 18
     },
     {
         id: 3,
@@ -384,7 +524,12 @@ const dataFN = [
         imageHeight: 50,
         inputCounterDisabled: true,
         inputDateRangeDisabled: true,
-        inputToggleDisabled: true
+        inputToggleDisabled: true,
+        location: {
+            latitude: '45.54',
+            longitude: '-73.60'
+        },
+        number: 1789
     },
     {
         id: 4,
@@ -394,7 +539,12 @@ const dataFN = [
         inputDateRange: {
             startDate: new Date('2021/09/17'),
             endDate: new Date('2021/09/25')
-        }
+        },
+        location: {
+            latitude: '45.55',
+            longitude: '-73.62'
+        },
+        number: 345
     },
     {
         id: 5,
@@ -405,13 +555,20 @@ const dataFN = [
         inputDateRange: {
             startDate: new Date('2021/08/02'),
             endDate: new Date('2021/09/15')
-        }
+        },
+        location: {
+            latitude: '45.56',
+            longitude: '-73.56'
+        },
+        number: 9
     }
 ];
 
 const dataOQ = [
     {
         id: 1,
+        percent: 0.34,
+        phone: '5142223333',
         progress: 34,
         progressBarReferenceLines: [
             {
@@ -430,6 +587,8 @@ const dataOQ = [
     },
     {
         id: 2,
+        percent: 0.45,
+        phone: '5144546767',
         progress: 100,
         progressBarTheme: 'success',
         progressRingVariant: 'base-autocomplete',
@@ -440,11 +599,15 @@ const dataOQ = [
     },
     {
         id: 3,
+        percent: 0.67,
+        phone: '6785643214',
         progress: 43,
         qrcode: 'https://www.avonni.app/'
     },
     {
         id: 4,
+        percent: 4,
+        phone: '3547789900',
         progress: 5,
         progressBarTheme: 'info',
         progressRingVariant: 'expired',
@@ -452,6 +615,8 @@ const dataOQ = [
     },
     {
         id: 5,
+        percent: 0.05,
+        phone: '5143245564',
         progress: 66,
         progressBarTheme: 'alt-inverse',
         progressBarReferenceLines: [
@@ -472,24 +637,40 @@ const dataOQ = [
 const dataRZ = [
     {
         id: 1,
-        slider: 36
+        slider: 36,
+        text: 'Nina Gomez',
+        url: 'https://www.avonnicomponents.com/',
+        urlLabel: 'Avonni Components Documentation'
     },
     {
         id: 2,
-        slider: 78
+        slider: 78,
+        text: 'Dave McKinsley',
+        url: 'https://www.avonni.app/',
+        urlLabel: 'Avonni'
     },
     {
         id: 3,
         sliderDisabled: true,
-        ratingDisabled: true
+        ratingDisabled: true,
+        text: 'Jung Phung',
+        url:
+            'https://developer.salesforce.com/docs/component-library/overview/components',
+        urlLabel: 'Salesforce documentation'
     },
     {
         id: 4,
         slider: 3,
-        sliderStep: 4
+        sliderStep: 4,
+        text: 'Lily Murray',
+        url: 'https://www.lightningdesignsystem.com/',
+        urlLabel: 'Lightning Design System'
     },
     {
-        id: 5
+        id: 5,
+        text: 'Reginald Martin',
+        url: 'https://lwc.dev/',
+        urlLabel: 'LWC Documentation'
     }
 ];
 
