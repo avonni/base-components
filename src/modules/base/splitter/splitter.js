e8d42ea898f4abfdf358e9db4065ba91e31ac6c3
Nawel Aammar <nawel.aa@hotmail.fr>
2021-05-26 15:55:59 -0400

    [refactor] (ABC-191): Remove useless slot removal, to fix error in tests

--- src/modules/base/splitter/splitter.js
+++ src/modules/base/splitter/splitter.js
@@ -262,7 +262,7 @@ export default class Splitter extends LightningElement {
                 }
             });
         }
-        slot.remove();
+        // slot.remove();
 
         this.template
             .querySelectorAll('.splitter-orientation-horizontal')
