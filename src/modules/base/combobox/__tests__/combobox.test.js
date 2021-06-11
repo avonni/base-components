28766699a97febf93ef0d5cbbf0c516d9a496019
Nawel Aammar <nawel.aa@hotmail.fr>
2021-06-11 09:32:01 -0400

    [license] (ABC-222): Add license at top of files

--- src/modules/base/combobox/__tests__/combobox.test.js
+++ src/modules/base/combobox/__tests__/combobox.test.js
@@ -1,3 +1,35 @@
+/**
+ * BSD 3-Clause License
+ *
+ * Copyright (c) 2021, Avonni Labs, Inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * - Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ *
+ * - Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * - Neither the name of the copyright holder nor the names of its
+ *   contributors may be used to endorse or promote products derived from
+ *   this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 import { createElement } from 'lwc';
 import Combobox from 'c/combobox';
 
@@ -14,6 +46,30 @@ describe('Combobox', () => {
         });
 
         expect(element.actions).toMatchObject([]);
+        expect(element.allowSearch).toBeFalsy();
+        expect(element.disabled).toBeFalsy();
+        expect(element.dropdownAlignment).toBe('left');
+        expect(element.dropdownLength).toBe('7-items');
+        expect(element.fieldLevelLevel).toBeUndefined();
+        expect(element.groups).toMatchObject([]);
+        expect(element.hideSelectedOptions).toBeFalsy();
+        expect(element.isLoading).toBeFalsy();
+        expect(element.isMultiSelect).toBeFalsy();
+        expect(element.label).toBeUndefined();
+        expect(element.messageWhenValueMissing).toBe('Complete this field.');
+        expect(element.multiLevelGroups).toBeFalsy();
+        expect(element.name).toBeUndefined();
+        expect(element.options).toMatchObject([]);
+        expect(element.placeholder).toBe('Select an Option');
+        expect(element.readOnly).toBeFalsy();
+        expect(element.removeSelectedOptions).toBeFalsy();
+        expect(element.required).toBeFalsy();
+        expect(element.scopes).toMatchObject([]);
+        expect(element.scopesTitle).toBe('Suggested for you');
+        expect(element.search).toBeInstanceOf(Function);
+        expect(element.validity).toMatchObject({});
+        expect(element.value).toMatchObject([]);
+        expect(element.variant).toBe('standard');
     });
 
     // /* ----- ATTRIBUTES ----- */
@@ -21,7 +77,7 @@ describe('Combobox', () => {
     // // actions
     // it('actions', () => {
     //     const element = createElement('base-combobox', {
-    //         is: Chip
+    //         is: Combobox
     //     });
     //     document.body.appendChild(element);
 
