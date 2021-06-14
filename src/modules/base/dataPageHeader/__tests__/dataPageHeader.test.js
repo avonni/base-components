727377724b548c826e665898c4ebd657dbc88f3a
Renaud Archer-Fortin <71739112+renaudaf@users.noreply.github.com>
2021-06-14 11:07:58 -0400

    [feat] (ABC-244): created data page header

---
+++ src/modules/base/dataPageHeader/__tests__/dataPageHeader.test.js
@@ -0,0 +1,412 @@
+/**
+ * BSD 3-Clause License
+ *
+ * Copyright (c) 2021, Avonni Labs, Inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * - Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ *
+ * - Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * - Neither the name of the copyright holder nor the names of its
+ *   contributors may be used to endorse or promote products derived from
+ *   this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+import { createElement } from 'lwc';
+import DataPageHeader from 'c/dataPageHeader';
+
+describe('DataPageHeader', () => {
+    afterEach(() => {
+        while (document.body.firstChild) {
+            document.body.removeChild(document.body.firstChild);
+        }
+    });
+
+    it('Default attributes', () => {
+        const element = createElement('data-page-header', {
+            is: DataPageHeader
+        });
+
+        expect(element.iconName).toBeUndefined();
+        expect(element.label).toBeUndefined();
+        expect(element.title).toBeUndefined();
+        expect(element.info).toBeUndefined();
+        expect(element.variant).toBe('base');
+        expect(element.fields).toEqual([]);
+    });
+
+    // icon-name
+    it('iconName', () => {
+        const element = createElement('data-page-header', {
+            is: DataPageHeader
+        });
+        document.body.appendChild(element);
+
+        const icon = element.shadowRoot.querySelector('c-primitive-icon');
+        element.iconName = 'utility:apps';
+
+        return Promise.resolve().then(() => {
+            expect(icon.iconName).toBe('utility:apps');
+        });
+    });
+
+    // label
+    // Depends on variant
+    it('label', () => {
+        const element = createElement('data-page-header', {
+            is: DataPageHeader
+        });
+        document.body.appendChild(element);
+
+        element.variant = 'object-home';
+        element.label = 'A string label';
+
+        return Promise.resolve().then(() => {
+            const label = element.shadowRoot.querySelector(
+                '.slds-page-header__name > .slds-page-header__name-title > span'
+            );
+
+            expect(label.textContent).toBe('A string label');
+        });
+    });
+
+    // title
+    it('title', () => {
+        const element = createElement('data-page-header', {
+            is: DataPageHeader
+        });
+        document.body.appendChild(element);
+
+        element.title = 'A string title';
+
+        return Promise.resolve().then(() => {
+            const title = element.shadowRoot.querySelector(
+                '.slds-page-header__title'
+            );
+
+            expect(title.textContent).toBe('A string title');
+        });
+    });
+
+    // info
+    // Depends on variant
+    it('info with the default (base) variant', () => {
+        const element = createElement('data-page-header', {
+            is: DataPageHeader
+        });
+        document.body.appendChild(element);
+
+        element.info = 'A string info';
+
+        return Promise.resolve().then(() => {
+            const info = element.shadowRoot.querySelector(
+                '.slds-page-header__name-meta'
+            );
+
+            expect(info.textContent).toBe('A string info');
+        });
+    });
+
+    it('info with the object-home variant', () => {
+        const element = createElement('data-page-header', {
+            is: DataPageHeader
+        });
+        document.body.appendChild(element);
+
+        element.info = 'A string info';
+        element.variant = 'object-home';
+
+        return Promise.resolve().then(() => {
+            const info = element.shadowRoot.querySelector(
+                '.slds-page-header__meta-text'
+            );
+
+            expect(info.textContent).toBe('A string info');
+        });
+    });
+
+    // variant
+    it('variant = base', () => {
+        const element = createElement('data-page-header', {
+            is: DataPageHeader
+        });
+        document.body.appendChild(element);
+
+        element.variant = 'base';
+
+        return Promise.resolve().then(() => {
+            const wrapper = element.shadowRoot.querySelector(
+                '.slds-page-header'
+            );
+            const label = element.shadowRoot.querySelector(
+                '.slds-page-header__name-title > slot[name="label"]'
+            );
+            const bodyInfo = element.shadowRoot.querySelector(
+                '.slds-page-header__meta-text'
+            );
+            const headerInfo = element.shadowRoot.querySelector(
+                '.slds-page-header__title + .slds-page-header__name-meta'
+            );
+            const details = element.shadowRoot.querySelector(
+                'slot[name="details"]'
+            );
+
+            expect(label).toBeFalsy();
+            expect(bodyInfo).toBeFalsy();
+            expect(headerInfo).toBeTruthy();
+            expect(details).toBeFalsy();
+
+            expect(wrapper.classList).not.toContain(
+                'slds-page-header_object-home'
+            );
+            expect(wrapper.classList).not.toContain(
+                'slds-page-header_record-home'
+            );
+            expect(wrapper.classList).not.toContain(
+                'slds-page-header_vertical'
+            );
+        });
+    });
+
+    it('variant = object-home', () => {
+        const element = createElement('data-page-header', {
+            is: DataPageHeader
+        });
+        document.body.appendChild(element);
+
+        element.variant = 'object-home';
+
+        return Promise.resolve().then(() => {
+            const wrapper = element.shadowRoot.querySelector(
+                '.slds-page-header'
+            );
+            const label = element.shadowRoot.querySelector(
+                '.slds-page-header__name-title > slot[name="label"]'
+            );
+            const bodyInfo = element.shadowRoot.querySelector(
+                '.slds-page-header__meta-text'
+            );
+            const headerInfo = element.shadowRoot.querySelector(
+                '.slds-page-header__title + .slds-page-header__name-meta'
+            );
+            const details = element.shadowRoot.querySelector(
+                'slot[name="details"]'
+            );
+
+            expect(label).toBeTruthy();
+            expect(bodyInfo).toBeTruthy();
+            expect(headerInfo).toBeFalsy();
+            expect(details).toBeFalsy();
+
+            expect(wrapper.classList).toContain('slds-page-header_object-home');
+            expect(wrapper.classList).not.toContain(
+                'slds-page-header_record-home'
+            );
+            expect(wrapper.classList).not.toContain(
+                'slds-page-header_vertical'
+            );
+        });
+    });
+
+    it('variant = record-home', () => {
+        const element = createElement('data-page-header', {
+            is: DataPageHeader
+        });
+        document.body.appendChild(element);
+
+        element.variant = 'record-home';
+
+        return Promise.resolve().then(() => {
+            const wrapper = element.shadowRoot.querySelector(
+                '.slds-page-header'
+            );
+            const label = element.shadowRoot.querySelector(
+                '.slds-page-header__name-title > slot[name="label"]'
+            );
+            const bodyInfo = element.shadowRoot.querySelector(
+                '.slds-page-header__meta-text'
+            );
+            const headerInfo = element.shadowRoot.querySelector(
+                '.slds-page-header__title + .slds-page-header__name-meta'
+            );
+            const details = element.shadowRoot.querySelector(
+                'slot[name="details"]'
+            );
+
+            expect(label).toBeTruthy();
+            expect(bodyInfo).toBeTruthy();
+            expect(headerInfo).toBeFalsy();
+            expect(details).toBeTruthy();
+
+            expect(wrapper.classList).not.toContain(
+                'slds-page-header_object-home'
+            );
+            expect(wrapper.classList).toContain('slds-page-header_record-home');
+            expect(wrapper.classList).not.toContain(
+                'slds-page-header_vertical'
+            );
+        });
+    });
+
+    it('variant = record-home-vertical', () => {
+        const element = createElement('data-page-header', {
+            is: DataPageHeader
+        });
+        document.body.appendChild(element);
+
+        element.variant = 'record-home-vertical';
+
+        return Promise.resolve().then(() => {
+            const wrapper = element.shadowRoot.querySelector(
+                '.slds-page-header'
+            );
+            const label = element.shadowRoot.querySelector(
+                '.slds-page-header__name-title > slot[name="label"]'
+            );
+            const bodyInfo = element.shadowRoot.querySelector(
+                '.slds-page-header__meta-text'
+            );
+            const headerInfo = element.shadowRoot.querySelector(
+                '.slds-page-header__title + .slds-page-header__name-meta'
+            );
+            const details = element.shadowRoot.querySelector(
+                'slot[name="details"]'
+            );
+
+            expect(label).toBeTruthy();
+            expect(bodyInfo).toBeTruthy();
+            expect(headerInfo).toBeFalsy();
+            expect(details).toBeTruthy();
+
+            expect(wrapper.classList).not.toContain(
+                'slds-page-header_object-home'
+            );
+            expect(wrapper.classList).not.toContain(
+                'slds-page-header_record-home'
+            );
+            expect(wrapper.classList).toContain('slds-page-header_vertical');
+        });
+    });
+
+    // fields
+    // Depends on variant
+    it('fields with variant = record-home', () => {
+        const element = createElement('data-page-header', {
+            is: DataPageHeader
+        });
+        document.body.appendChild(element);
+        const fields = [
+            {
+                label: 'Currency',
+                value: 70,
+                type: 'currency',
+                typeAttributes: {
+                    currencyCode: 'EUR',
+                    currencyDisplayAs: 'name',
+                    minimumIntegerDigits: 2
+                }
+            },
+            {
+                label: 'Email',
+                value: 'Avonni@Avonni.com',
+                type: 'email',
+                typeAttributes: {
+                    hideIcon: 'true'
+                }
+            }
+        ];
+
+        element.fields = fields;
+        element.variant = 'record-home';
+
+        return Promise.resolve().then(() => {
+            const detailsSlot = element.shadowRoot.querySelector(
+                'slot[name="details"]'
+            );
+            const primitiveFields = element.shadowRoot.querySelectorAll(
+                'c-output-data'
+            );
+
+            expect(detailsSlot).toBeFalsy();
+            primitiveFields.forEach((field, index) => {
+                const correspondingField = fields[index];
+                expect(correspondingField).toBeTruthy();
+                expect(field.typeAttributes).toMatchObject(
+                    correspondingField.typeAttributes
+                );
+                expect(field.label).toBe(correspondingField.label);
+                expect(field.value).toBe(correspondingField.value);
+                expect(field.type).toBe(correspondingField.type);
+            });
+        });
+    });
+
+    it('fields with variant = record-home-vertical', () => {
+        const element = createElement('data-page-header', {
+            is: DataPageHeader
+        });
+        document.body.appendChild(element);
+        const fields = [
+            {
+                label: 'Currency',
+                value: 70,
+                type: 'currency',
+                typeAttributes: {
+                    currencyCode: 'EUR',
+                    currencyDisplayAs: 'name',
+                    minimumIntegerDigits: 2
+                }
+            },
+            {
+                label: 'Email',
+                value: 'Avonni@Avonni.com',
+                type: 'email',
+                typeAttributes: {
+                    hideIcon: 'true'
+                }
+            }
+        ];
+
+        element.fields = fields;
+        element.variant = 'record-home-vertical';
+
+        return Promise.resolve().then(() => {
+            const detailsSlot = element.shadowRoot.querySelector(
+                'slot[name="details"]'
+            );
+            const primitiveFields = element.shadowRoot.querySelectorAll(
+                'c-output-data'
+            );
+
+            expect(detailsSlot).toBeFalsy();
+            primitiveFields.forEach((field, index) => {
+                const correspondingField = fields[index];
+                expect(correspondingField).toBeTruthy();
+                expect(field.typeAttributes).toMatchObject(
+                    correspondingField.typeAttributes
+                );
+                expect(field.label).toBe(correspondingField.label);
+                expect(field.value).toBe(correspondingField.value);
+                expect(field.type).toBe(correspondingField.type);
+            });
+        });
+    });
+});
---
+++ src/modules/base/dataPageHeader/dataPageHeader.js
@@ -0,0 +1,176 @@
+/**
+ * BSD 3-Clause License
+ *
+ * Copyright (c) 2021, Avonni Labs, Inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * - Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ *
+ * - Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * - Neither the name of the copyright holder nor the names of its
+ *   contributors may be used to endorse or promote products derived from
+ *   this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+import { LightningElement, api } from 'lwc';
+import { normalizeString, normalizeArray } from 'c/utilsPrivate';
+import { classSet } from 'c/utils';
+import dataPageHeader from './dataPageHeader.html';
+import dataPageHeaderVertical from './dataPageHeaderVertical.html';
+import { computeSldsClass } from 'c/iconUtils';
+
+const PAGE_HEADER_VARIANTS = {
+    valid: ['base', 'object-home', 'record-home', 'record-home-vertical'],
+    default: 'base'
+};
+
+export default class PageHeader extends LightningElement {
+    @api iconName;
+    @api label;
+    @api title;
+    @api info;
+
+    _variant = PAGE_HEADER_VARIANTS.default;
+    _fields = [];
+    showTitle = true;
+    showLabel = true;
+    showActions = true;
+    showDetails = true;
+    showInfo = true;
+    showControls = true;
+
+    render() {
+        if (this._variant === 'record-home-vertical') {
+            return dataPageHeaderVertical;
+        }
+        return dataPageHeader;
+    }
+
+    renderedCallback() {
+        if (this.titleSlot) {
+            this.showTitle = this.titleSlot.assignedElements().length !== 0;
+        }
+        if (this.labelSlot) {
+            this.showLabel = this.labelSlot.assignedElements().length !== 0;
+        }
+        if (this.actionsSlot) {
+            this.showActions = this.actionsSlot.assignedElements().length !== 0;
+        }
+        if (this.detailsSlot) {
+            this.showDetails = this.detailsSlot.assignedElements().length !== 0;
+        }
+        if (this.infoSlot) {
+            this.showInfo = this.infoSlot.assignedElements().length !== 0;
+        }
+        if (this.controlsSlot) {
+            this.showControls =
+                this.controlsSlot.assignedElements().length !== 0;
+        }
+    }
+
+    get titleSlot() {
+        return this.template.querySelector('slot[name=title]');
+    }
+
+    get labelSlot() {
+        return this.template.querySelector('slot[name=label]');
+    }
+
+    get actionsSlot() {
+        return this.template.querySelector('slot[name=actions]');
+    }
+
+    get detailsSlot() {
+        return this.template.querySelector('slot[name=details]');
+    }
+
+    get infoSlot() {
+        return this.template.querySelector('slot[name=info]');
+    }
+
+    get controlsSlot() {
+        return this.template.querySelector('slot[name=controls]');
+    }
+
+    @api
+    get variant() {
+        return this._variant;
+    }
+
+    set variant(value) {
+        this._variant = normalizeString(value, {
+            fallbackValue: PAGE_HEADER_VARIANTS.default,
+            validValues: PAGE_HEADER_VARIANTS.valid
+        });
+    }
+
+    @api
+    get fields() {
+        return this._fields;
+    }
+
+    set fields(value) {
+        this._fields = normalizeArray(value);
+    }
+
+    get computedOuterClass() {
+        return classSet('slds-page-header')
+            .add({
+                'slds-page-header_object-home': this._variant === 'object-home',
+                'slds-page-header_record-home': this._variant === 'record-home'
+            })
+            .toString();
+    }
+
+    get computedIconClass() {
+        return classSet('slds-icon_container')
+            .add(computeSldsClass(this.iconName))
+            .toString();
+    }
+
+    get isBaseVariant() {
+        return this._variant === 'base';
+    }
+
+    get isObjectHomeVariant() {
+        return this._variant === 'object-home';
+    }
+
+    get isRecordHomeVariant() {
+        return this._variant === 'record-home';
+    }
+
+    get hasStringTitle() {
+        return !!this.title;
+    }
+
+    get hasStringLabel() {
+        return !!this.label;
+    }
+
+    get hasStringInfo() {
+        return !!this.info;
+    }
+
+    get fieldsIsEmpty() {
+        return this._fields.length === 0;
+    }
+}
