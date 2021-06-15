435a8780460dd101eb1f110421ccf52c799e93d7
Nawel Aammar <nawel.aa@hotmail.fr>
2021-06-15 13:29:27 -0400

    [feat] (ABC-240): Setup of the scheduler

---
+++ src/modules/base/scheduler/__docs__/scheduler.stories.js
@@ -0,0 +1,250 @@
+/**
+ * BSD 3-Clause License
+ *
+ * Copyright (c) 2021, Avonni Labs, Inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * - Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ *
+ * - Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * - Neither the name of the copyright holder nor the names of its
+ *   contributors may be used to endorse or promote products derived from
+ *   this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+import { Scheduler } from '../__examples__/scheduler';
+
+export default {
+    title: 'Example/Scheduler',
+    argTypes: {
+        start: {
+            control: {
+                type: 'date'
+            },
+            description:
+                'Specifies the minimum date the calendar can show. It can be a Date object, timestamp, or an ISO8601 formatted string.',
+            defaultValue: new Date(1900, 0, 1),
+            table: {
+                type: { summary: 'object' },
+                defaultValue: { summary: 'Date(1900, 0, 1)' },
+                category: 'Available dates'
+            }
+        },
+        end: {
+            control: {
+                type: 'date'
+            },
+            description:
+                'Specifies the maximum date the calendar can show. It can be a Date object, timestamp, or an ISO8601 formatted string.',
+            defaultValue: new Date(2099, 11, 31),
+            table: {
+                type: { summary: 'object' },
+                defaultValue: { summary: 'Date(2099, 11, 31)' },
+                category: 'Available dates'
+            }
+        },
+        headers: {
+            control: {
+                type: 'object'
+            },
+            description:
+                'Array of date/time scheduler header objects. \nThe headers are displayed in rows above the schedule, and used to create its columns.',
+            table: {
+                type: { summary: 'object' }
+            }
+        },
+        columns: {
+            control: {
+                type: 'object'
+            },
+            description:
+                'Array of datatable column objects. The columns are displayed to the left of the schedule.',
+            table: {
+                type: { summary: 'object' }
+            }
+        },
+        rows: {
+            control: {
+                type: 'object'
+            },
+            description:
+                'Array of datatable data objects. Each object represent a row of the scheduler.',
+            table: {
+                type: { summary: 'object' }
+            }
+        },
+        visibleSpan: {
+            name: 'visible-span',
+            control: {
+                type: 'object'
+            },
+            description:
+                'Object used to set the visible date/time span on the screen. It has two keys: unit (valid values include minute, hour, day, month and year) and span (number).',
+            defaultValue: { unit: 'hour', span: 12 },
+            table: {
+                type: { summary: 'object' },
+                defaultValue: { summary: "{ unit: 'hour', span: 12 }" }
+            }
+        },
+        events: {
+            control: {
+                type: 'object'
+            },
+            description: 'Array of event objects.',
+            table: {
+                type: { summary: 'object' },
+                category: 'Events'
+            }
+        },
+        availableTimeFrames: {
+            name: 'available-time-frames',
+            control: {
+                type: 'object'
+            },
+            defaultValue: ['00:00-00:00'],
+            description:
+                'Array of available time frames. If present, the scheduler will only show the available time frames. Defaults to the full day being available. \nEach time frame string must follow the pattern ‘start-end’, with start and end being ISO8601 formatted time strings.',
+            table: {
+                type: { summary: 'object' },
+                defaultValue: { summary: "['00:00-00:00']" },
+                category: 'Available dates'
+            }
+        },
+        availableDaysOfTheWeek: {
+            name: 'available-days-of-the-week',
+            control: {
+                type: 'object'
+            },
+            defaultValue: [0, 1, 2, 3, 4, 5, 6],
+            description:
+                'Array of available days of the week. If present, the scheduler will only show the available days of the week. Defaults to all days being available. \nThe days are represented by a number, starting from 0 for Sunday, and ending with 6 for Saturday.',
+            table: {
+                type: { summary: 'object' },
+                defaultValue: { summary: '[0, 1, 2, 3, 4, 5, 6]' },
+                category: 'Available dates'
+            }
+        },
+        availableMonths: {
+            name: 'available-months',
+            control: {
+                type: 'object'
+            },
+            defaultValue: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11],
+            description:
+                'Array of available months. If present, the scheduler will only show the available months. Defaults to all months being available. \nThe months are represented by a number, starting from 0 for January, and ending with 11 for December.',
+            table: {
+                type: { summary: 'object' },
+                defaultValue: {
+                    summary: '[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]'
+                },
+                category: 'Available dates'
+            }
+        },
+        disabledDatesTimes: {
+            name: 'disabled-dates-times',
+            control: {
+                type: 'object'
+            },
+            description: 'Array of disabled date/time objects.',
+            table: {
+                type: { summary: 'object' },
+                category: 'Available dates'
+            }
+        },
+        theme: {
+            control: {
+                type: 'radio'
+            },
+            options: ['default', 'inverse'],
+            defaultValue: 'default',
+            description:
+                'Theme of the scheduler. Valid values include default and inverse.',
+            table: {
+                type: { summary: 'string' },
+                defaultValue: { summary: 'default' }
+            }
+        },
+        eventsTheme: {
+            name: 'events-theme',
+            control: {
+                type: 'select'
+            },
+            options: ['default', 'transparent', 'line', 'hollow', 'rounded'],
+            defaultValue: 'default',
+            description:
+                'Theme of the events. Valid values include default, transparent, line, hollow and rounded.',
+            table: {
+                type: { summary: 'string' },
+                defaultValue: { summary: 'default' },
+                category: 'Events'
+            }
+        },
+        eventsPalette: {
+            name: 'events-palette',
+            control: {
+                type: 'select'
+            },
+            options: [
+                'aurora',
+                'bluegrass',
+                'dusk',
+                'fire',
+                'heat',
+                'lake',
+                'mineral',
+                'nightfall',
+                'ocean',
+                'pond',
+                'sunrise',
+                'water',
+                'watermelon',
+                'wildflowers'
+            ],
+            defaultValue: 'aurora',
+            description:
+                'Default palette used for the event colors. Valid values include aurora, bluegrass, dusk, fire, heat, lake, mineral, nightfall, ocean, pond, sunrise, water, watermelon, wildflowers.',
+            table: {
+                type: { summary: 'string' },
+                defaultValue: { summary: 'aurora' },
+                category: 'Events'
+            }
+        },
+        customEventsPalette: {
+            name: 'custom-events-palette',
+            control: {
+                type: 'object'
+            },
+            description:
+                'Array of colors to use as a palette for the events. If present, it will overwrite the events-palette selected. \nThe color strings have to be valid CSS color values.',
+            table: {
+                type: { summary: 'object' },
+                category: 'Events'
+            }
+        }
+    }
+};
+
+const Template = (args) => Scheduler(args);
+
+export const Base = Template.bind({});
+Base.args = {
+    start: new Date()
+};
---
+++ src/modules/base/scheduler/scheduler.js
@@ -0,0 +1,279 @@
+/**
+ * BSD 3-Clause License
+ *
+ * Copyright (c) 2021, Avonni Labs, Inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * - Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ *
+ * - Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * - Neither the name of the copyright holder nor the names of its
+ *   contributors may be used to endorse or promote products derived from
+ *   this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+import { LightningElement, api } from 'lwc';
+import { normalizeArray, normalizeString } from 'c/utilsPrivate';
+
+const EVENTS_THEMES = {
+    valid: ['default', 'transparent', 'line', 'hollow', 'rounded'],
+    default: 'default'
+};
+
+const EVENTS_PALETTES = {
+    valid: [
+        'aurora',
+        'bluegrass',
+        'dusk',
+        'fire',
+        'heat',
+        'lake',
+        'mineral',
+        'nightfall',
+        'ocean',
+        'pond',
+        'sunrise',
+        'water',
+        'watermelon',
+        'wildflowers'
+    ],
+    default: 'aurora'
+};
+
+const THEMES = {
+    valid: ['default', 'inverse'],
+    default: 'default'
+};
+
+const DEFAULT_END_DATE = new Date(2099, 11, 31);
+const DEFAULT_START_DATE = new Date(1900, 0, 1);
+const DEFAULT_VISIBLE_SPAN = {
+    unit: 'hour',
+    span: 12
+};
+
+const PALETTES = {
+    aurora: ['#3296ed', '#77b9f2', '#9d53f2', '#c398f5', '#26aba4', '#4ed4cd'],
+    bluegrass: [
+        '#c7f296',
+        '#94e7a8',
+        '#51d2bb',
+        '#27aab0',
+        '#116985',
+        '#053661'
+    ],
+    dusk: ['#98c9f5', '#bac6a4', '#e0bc3d', '#d49b08', '#966002', '#613102'],
+    fire: ['#f5de98', '#f5c066', '#f59527', '#d56613', '#952f13', '#610514'],
+    heat: ['#c7f296', '#d8e167', '#e3c52c', '#d19214', '#934214', '#610514'],
+    lake: ['#98c9f5', '#72c9bd', '#44c972', '#38ab3d', '#4d6719', '#613102'],
+    mineral: ['#529ee0', '#d9a6c2', '#08916d', '#f59b00', '#006699', '#f0e442'],
+    nightfall: [
+        '#faca9b',
+        '#ce86bc',
+        '#9232e0',
+        '#5d19d4',
+        '#2a2396',
+        '#053661'
+    ],
+    ocean: ['#96f2a9', '#64cfc6', '#289ee3', '#1c6bd0', '#40308a', '#61054f'],
+    pond: ['#c398f5', '#8593f5', '#358aef', '#0c7fc5', '#0a6e67', '#0a611b'],
+    sunrise: ['#f5de98', '#f5c062', '#f59623', '#ce6716', '#762f3d', '#300561'],
+    water: ['#96F2EE', '#68CEEE', '#2D9CED', '#0E6ECE', '#073E92', '#051C61'],
+    watermelon: [
+        '#f598a7',
+        '#f56580',
+        '#f4284e',
+        '#c11c2f',
+        '#5c3f22',
+        '#0a611b'
+    ],
+    wildflowers: [
+        '#00a1e0',
+        '#16325c',
+        '#76ded9',
+        '#08a69e',
+        '#e2ce7d',
+        '#e69f00'
+    ]
+};
+
+export default class Scheduler extends LightningElement {
+    _availableDaysOfTheWeek = [];
+    _availableMonths = [];
+    _availableTimeFrames = [];
+    _columns = [];
+    _customEventsPalette = [];
+    _disabledDatesTimes = [];
+    _end = DEFAULT_END_DATE;
+    _eventsPalette = EVENTS_PALETTES.default;
+    _eventsTheme = EVENTS_THEMES.default;
+    _headers = [];
+    _start = DEFAULT_START_DATE;
+    _theme = THEMES.default;
+    _visibleSpan = DEFAULT_VISIBLE_SPAN;
+
+    @api
+    get availableDaysOfTheWeek() {
+        return this._availableDaysOfTheWeek;
+    }
+    set availableDaysOfTheWeek(value) {
+        this._availableDaysOfTheWeek = normalizeArray(value);
+    }
+
+    @api
+    get availableMonths() {
+        return this._availableMonths;
+    }
+    set availableMonths(value) {
+        this._availableMonths = normalizeArray(value);
+    }
+
+    @api
+    get availableTimeFrames() {
+        return this._availableTimeFrames;
+    }
+    set availableTimeFrames(value) {
+        this._availableTimeFrames = normalizeArray(value);
+    }
+
+    @api
+    get columns() {
+        return this._columns;
+    }
+    set columns(value) {
+        this._columns = normalizeArray(value);
+    }
+
+    @api
+    get customEventsPalette() {
+        return this._customEventsPalette;
+    }
+    set customEventsPalette(value) {
+        this._customEventsPalette = normalizeArray(value);
+    }
+
+    @api
+    get disabledDatesTimes() {
+        return this._disabledDatesTimes;
+    }
+    set disabledDatesTimes(value) {
+        this._disabledDatesTimes = normalizeArray(value);
+    }
+
+    @api
+    get end() {
+        return this._end;
+    }
+    set end(value) {
+        const computedDate = this.computeDate(value);
+        this._end = computedDate || DEFAULT_END_DATE;
+    }
+
+    @api
+    get events() {
+        return this._events;
+    }
+    set events(value) {
+        this._events = normalizeArray(value);
+    }
+
+    @api
+    get eventsPalette() {
+        return this._eventsPalette;
+    }
+    set eventsPalette(value) {
+        this._eventsPalette = normalizeString(value, {
+            fallbackValue: EVENTS_PALETTES.default,
+            validValues: EVENTS_PALETTES.valid
+        });
+    }
+
+    @api
+    get eventsTheme() {
+        return this._eventsTheme;
+    }
+    set eventsTheme(value) {
+        this._eventsTheme = normalizeString(value, {
+            fallbackValue: EVENTS_THEMES.default,
+            validValues: EVENTS_THEMES.valid
+        });
+    }
+
+    @api
+    get headers() {
+        return this._headers;
+    }
+    set headers(value) {
+        this._headers = normalizeArray(value);
+    }
+
+    @api
+    get rows() {
+        return this._rows;
+    }
+    set rows(value) {
+        this._rows = normalizeArray(value);
+    }
+
+    @api
+    get start() {
+        return this._start;
+    }
+    set start(value) {
+        const computedDate = this.computeDate(value);
+        this._start = computedDate || DEFAULT_START_DATE;
+    }
+
+    @api
+    get theme() {
+        return this._theme;
+    }
+    set theme(value) {
+        this._theme = normalizeString(value, {
+            fallbackValue: THEMES.default,
+            validValues: THEMES.valid
+        });
+    }
+
+    @api
+    get visibleSpan() {
+        return this._visibleSpan;
+    }
+    set visibleSpan(value) {
+        this._visibleSpan =
+            typeof value === 'object' ? value : DEFAULT_VISIBLE_SPAN;
+    }
+
+    get palette() {
+        return this.customEventsPalette.length
+            ? this.customEventsPalette
+            : PALETTES[this.eventsPalette];
+    }
+
+    /**
+     * @returns {object} Date object or false
+     */
+    computeDate(date) {
+        if (date instanceof Date) return date;
+        if (!isNaN(new Date(date).getTime())) return new Date(date);
+        return false;
+    }
+}
