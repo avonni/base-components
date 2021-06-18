aba60c6fa2278f2de81af5357f74ebecf3911462
Alain Rusnov <alainrusnov@gmail.com>
2021-06-18 15:05:28 -0400

    (feat)-(ABC-241)-Init commit and layout

---
+++ src/modules/base/seperator/__docs__/seperator.stories.js
@@ -0,0 +1,124 @@
+/**
+ * BSD 3-Clause License
+ *
+ * Copyright (c) 2021, Avonni Labs, Inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * - Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ *
+ * - Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * - Neither the name of the copyright holder nor the names of its
+ *   contributors may be used to endorse or promote products derived from
+ *   this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+import { Seperator } from '../__examples__/seperator';
+
+export default {
+    title: 'Example/Seperator',
+    argTypes: {
+        label: {
+            name: 'label',
+            control: {
+                type: 'text'
+            },
+            description: 'Text to display in the seperator',
+            table: {
+                type: { summary: 'string' }
+            }
+        },
+        alignContent: {
+            name: 'align-content',
+            control: {
+                type: 'text'
+            },
+            options: ['start', 'center', 'end'],
+            defaultValue: 'center',
+            description:
+                'Position of the content in the separator. Valid values include start, center and end.',
+            table: {
+                type: { summary: 'string' },
+                defaultValue: { summary: 'center' }
+            }
+        },
+        iconName: {
+            name: 'icon-name',
+            control: {
+                type: 'text'
+            },
+            description:
+                "The name of the icon to be used in the format 'utility:down'.",
+            table: {
+                type: { summary: 'string' }
+            }
+        },
+        iconPosition: {
+            name: 'icon-position',
+            control: {
+                type: 'text'
+            },
+            options: ['left', 'right'],
+            defaultValue: 'left',
+            description:
+                'Describes the position of the icon. Valid values include left and right.',
+            table: {
+                type: { summary: 'string' },
+                defaultValue: { summary: 'left' }
+            }
+        },
+        iconSize: {
+            name: 'icon-size',
+            control: {
+                type: 'text'
+            },
+            options: ['xx-smal', 'x-small', 'small', 'medium', 'large'],
+            defaultValue: 'medium',
+            description:
+                'The size of the icon. Options include xx-small, x-small, small, medium and large.',
+            table: {
+                type: { summary: 'string' },
+                defaultValue: { summary: 'medium' }
+            }
+        },
+        orientation: {
+            name: 'orientation',
+            control: {
+                type: 'text'
+            },
+            options: ['horizontal', 'vertical'],
+            defaultValue: 'horizontal',
+            description: 'Valid values include horizontal and vertical.',
+            table: {
+                type: { summary: 'string' },
+                defaultValue: { summary: 'horizontal' }
+            }
+        }
+    }
+};
+
+const Template = (args) => Seperator(args);
+
+export const Base = Template.bind({});
+Base.args = {
+    iconName: 'utility:user',
+    label: 'Today',
+    orientation: 'horizontal'
+};
