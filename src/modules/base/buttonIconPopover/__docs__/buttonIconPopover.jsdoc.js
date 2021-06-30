4dfd7be585c9cc20826a146915e60c268e54e79d
Renaud Archer-Fortin <71739112+renaudaf@users.noreply.github.com>
2021-06-30 15:44:27 -0400

    [doc] (ABC-251): Added examples in jsdoc file

--- src/modules/base/buttonIconPopover/__docs__/buttonIconPopover.jsdoc.js
+++ src/modules/base/buttonIconPopover/__docs__/buttonIconPopover.jsdoc.js
@@ -19,3 +19,52 @@
  * @memberof slots
  * @name footer
  */
+
+/**
+ * @namespace examples
+ */
+/**
+ * @memberof examples
+ * @name baseWithPopoverBase
+ * @storyId example-button-icon-popover--base-with-popover-base
+ */
+/**
+ * @memberof examples
+ * @name baseWithPopoverLoading
+ * @storyId example-button-icon-popover--base-with-popover-loading
+ */
+/**
+ * @memberof examples
+ * @name baseWithPopoverWarning
+ * @storyId example-button-icon-popover--base-with-popover-warning
+ */
+/**
+ * @memberof examples
+ * @name baseWithToggleInDefaultSlot
+ * @storyId example-button-icon-popover--base-with-toggle-in-default-slot
+ */
+/**
+ * @memberof examples
+ * @name brand
+ * @storyId example-button-icon-popover--brand
+ */
+/**
+ * @memberof examples
+ * @name borderFilledWithGreenSuccessIcon
+ * @storyId example-button-icon-popover--border-filled-with-green-success-icon
+ */
+/**
+ * @memberof examples
+ * @name borderFilledWithLightCheckIcon
+ * @storyId example-button-icon-popover--border-filled-with-light-check-icon
+ */
+/**
+ * @memberof examples
+ * @name borderFilledWithLargePopoverWalkthrough
+ * @storyId example-button-icon-popover--border-filled-with-large-popover-walkthrough
+ */
+/**
+ * @memberof examples
+ * @name containerWithSmallPopoverError
+ * @storyId example-button-icon-popover--container-with-small-popover-error
+ */
--- src/modules/base/buttonPopover/__docs__/buttonPopover.jsdoc.js
+++ src/modules/base/buttonPopover/__docs__/buttonPopover.jsdoc.js
@@ -19,3 +19,57 @@
  * @memberof slots
  * @name footer
  */
+
+/**
+ * @namespace examples
+ */
+/**
+ * @memberof examples
+ * @name neutral
+ * @storyId example-button-popover--neutral
+ */
+/**
+ * @memberof examples
+ * @name neutralWithLargeIconRight
+ * @storyId example-button-popover--neutral-large-with-icon-right
+ */
+/**
+ * @memberof examples
+ * @name neutralWithToggleInDefaultSlot
+ * @storyId example-button-popover--neutral-with-toggle-in-default-slot
+ */
+/**
+ * @memberof examples
+ * @name baseWithPopoverVariantWalkthrough
+ * @storyId example-button-popover--base-with-popover-variant-walkthrough
+ */
+/**
+ * @memberof examples
+ * @name brand
+ * @storyId example-button-popover--brand
+ */
+/**
+ * @memberof examples
+ * @name brandOutlineWithPopoverWarning
+ * @storyId example-button-popover--brand-outline-with-popover-warning
+ */
+/**
+ * @memberof examples
+ * @name destructiveWithPopoverError
+ * @storyId example-button-popover--destructive-with-popover-error
+ */
+/**
+ * @memberof examples
+ * @name destructiveTextWithPopoverError
+ * @storyId example-button-popover--destructive-text-with-popover-error
+ */
+/**
+ * @memberof examples
+ * @name inverseWithPopoverLoading
+ * @storyId example-button-popover--inverse-with-popover-loading
+ */
+/**
+ * @memberof examples
+ * @name success
+ * @storyId example-button-popover--success
+ */
