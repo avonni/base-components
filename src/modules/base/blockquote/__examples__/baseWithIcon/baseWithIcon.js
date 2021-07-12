f6638e875e5c8cfeb397c7be1b16c4f5e08d13ef
Alain Rusnov <alainrusnov@gmail.com>
2021-07-12 17:25:10 -0400

    (JSDocs)-(ABC-288)-Blockquote

---
+++ src/modules/base/blockquote/__examples__/baseWithIcon/baseWithIcon.js
@@ -0,0 +1,3 @@
+import { LightningElement } from 'lwc';
+
+export default class BlockquoteBaseWithIcon extends LightningElement {}
---
+++ src/modules/base/blockquote/__examples__/brand/brand.js
@@ -0,0 +1,3 @@
+import { LightningElement } from 'lwc';
+
+export default class BlockquoteBrand extends LightningElement {}
