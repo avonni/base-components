ecb4be5791ffc3c53d6dc3b71ad2d7ee029c50f5
Alain Rusnov <alainrusnov@gmail.com>
2021-07-12 18:03:49 -0400

    (JSDocs)-(ABC-288)-ButtonDialog

---
+++ src/modules/base/buttonDialog/__docs__/buttonDialog.jsdocs.js
@@ -0,0 +1,43 @@
+/**
+ * @namespace slots
+ */
+
+/**
+ * Modal title.
+ * @memberof slots
+ * @name title
+ */
+/**
+ * Modal footer.
+ * @memberof slots
+ * @name footer
+ */
+
+/**
+ * @namespace examples
+ */
+/**
+ * @memberof examples
+ * @name base
+ * @storyId example-blockquote--base
+ */
+/**
+ * @memberof examples
+ * @name brand
+ * @storyId example-blockquote--brand
+ */
+/**
+ * @memberof examples
+ * @name warning
+ * @storyId example-blockquote--warning
+ */
+/**
+ * @memberof examples
+ * @name error
+ * @storyId example-blockquote--error
+ */
+/**
+ * @memberof examples
+ * @name success
+ * @storyId example-blockquote--success
+ */
