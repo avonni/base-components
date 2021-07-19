4dd37a51c9d2473999492ed3f11e86cd21ce11b0
Alain Rusnov <alainrusnov@gmail.com>
2021-07-19 13:00:00 -0400

    (JSDoc)-(ABC-290)-DynamicMenu

---
+++ src/modules/base/dynamicMenu/__docs__/dynamicMenu.jsdoc.js
@@ -0,0 +1,64 @@
+/**
+ * @namespace slots
+ */
+/**
+ * Placeholder for your content.
+ *
+ * @memberof slots
+ * @name default
+ */
+/**
+ * Placeholder for actionable components, such as lightning-button.
+ *
+ * @memberof slots
+ * @name default
+ */
+
+/**
+ * @namespace examples
+ */
+/**
+ * @memberof examples
+ * @name base
+ * @storyId example-dynamic-menu--base
+ */
+/**
+ * @memberof examples
+ * @name baseWithSearch
+ * @storyId example-dynamic-menu--base-with-search
+ */
+/**
+ * @memberof examples
+ * @name disabled
+ * @storyId example-dynamic-menu--disabled
+ */
+/**
+ * @memberof examples
+ * @name isLoading
+ * @storyId example-dynamic-menu--is-loading
+ */
+/**
+ * @memberof examples
+ * @name baseWithLabel
+ * @storyId example-dynamic-menu--base-with-label
+ */
+/**
+ * @memberof examples
+ * @name borderFilled
+ * @storyId example-dynamic-menu--border-filled
+ */
+/**
+ * @memberof examples
+ * @name bare
+ * @storyId example-dynamic-menu--bare
+ */
+/**
+ * @memberof examples
+ * @name bareInverse
+ * @storyId example-dynamic-menu--bare-inverse
+ */
+/**
+ * @memberof examples
+ * @name container
+ * @storyId example-dynamic-menu--container
+ */
