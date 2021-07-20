fa58063f4622f0f8536e187e9f19f2669db036c8
Alain Rusnov <alainrusnov@gmail.com>
2021-07-20 09:42:49 -0400

    (JSDoc)-(ABC-290)-FilterMenu

---
+++ src/modules/base/filterMenu/__docs__/filterMenu.jsdoc.js
@@ -0,0 +1,58 @@
+/**
+ * @namespace examples
+ */
+/**
+ * @memberof examples
+ * @name base
+ * @storyId example-filter-menu--base
+ */
+/**
+ * @memberof examples
+ * @name search
+ * @storyId example-filter-menu--search
+ */
+/**
+ * @memberof examples
+ * @name disabled
+ * @storyId example-filter-menu--disabled
+ */
+/**
+ * @memberof examples
+ * @name loading
+ * @storyId example-filter-menu--loading
+ */
+/**
+ * @memberof examples
+ * @name xSmallIcon
+ * @storyId example-filter-menu--x-small-icon
+ */
+/**
+ * @memberof examples
+ * @name containerVariantWithCustomButtonLabels
+ * @storyId example-filter-menu--container-variant-with-custom-button-labels
+ */
+/**
+ * @memberof examples
+ * @name largeWidthAndNubbin
+ * @storyId example-filter-menu--large-width-and-nubbin
+ */
+/**
+ * @memberof examples
+ * @name fiveItemsLength
+ * @storyId example-filter-menu--five-items-length
+ */
+/**
+ * @memberof examples
+ * @name vertical
+ * @storyId example-filter-menu--vertical
+ */
+/**
+ * @memberof examples
+ * @name verticalWithSearchAndIcon
+ * @storyId example-filter-menu--vertical-with-search-and-icon
+ */
+/**
+ * @memberof examples
+ * @name verticalLoading
+ * @storyId example-filter-menu--vertical-loading
+ */
