f07b1fe35995cd15e796af2cd50cf5f082a115d8
Alain Rusnov <alainrusnov@gmail.com>
2021-07-20 11:00:07 -0400

    (JSDoc)-(ABC-290)-FilterMenuGroup

---
+++ src/modules/base/filterMenuGroup/__docs__/filterMenuGroup.jsdoc.js
@@ -0,0 +1,18 @@
+/**
+ * @namespace examples
+ */
+/**
+ * @memberof examples
+ * @name base
+ * @storyId example-filter-menu-group--base
+ */
+/**
+ * @memberof examples
+ * @name buttonIcons
+ * @storyId example-filter-menu-group--button-icons
+ */
+/**
+ * @memberof examples
+ * @name vertical
+ * @storyId example-filter-menu-group--vertical
+ */
