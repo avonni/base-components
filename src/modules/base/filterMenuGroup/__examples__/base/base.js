d6712121676130f45d200a2b3ce80f8772608edb
Alain Rusnov <alainrusnov@gmail.com>
2021-07-22 08:48:33 -0400

    (js doc) (abc 290) add js docs avonni dynamic menu to input choice set (#114)
    * (JSDocs)-(ABC-288)-ActivityTimeline
    
    * (JSDocs)-(ABC-288)-Avatar
    
    * (JSDocs)-(ABC-288)-Updated ActivityTimeline and ActivityTimelineItems with new guidelines
    
    * (JSDocs)-(ABC-288)-Blockquote
    
    * (JSDocs)-(ABC-288)-Blockquote-Slots
    
    * (JSDocs)-(ABC-288)-ButtonDialog
    
    * (JSDocs)-(ABC-288)-ButtonDialog and ButtonIconDialog - removed js examples without objects
    
    - removed js examples which did not contain an object for code simplification
    - activityTimlineItem actions removed @public
    
    * (JSDocs)-(ABC-288)-ButtonMenu
    
    * (JSDocs)-(ABC-288)-Calendar
    
    * (JSDocs)-(ABC-288)-Chip
    
    * (JSDocs)-(ABC-288)-Updated Blockquote and Chip slot examples
    
    * (JSDocs)-(ABC-288)-Updated buttonDialog slot examples
    
    * (JSDocs)-(ABC-288)-Updated buttonIconDialog slot examples
    
    * (JSDocs)-(ABC-288)-ColorGradient
    
    * (JSDocs)-(ABC-288)-ColorGradient - Calendar - Updated Type Safety
    
    - typo on calendar
    - this.value to this._value for eslinting and dev. guidelines
    
    * (JSDocs)-(ABC-288)-ColorPalette
    
    * (JSDocs)-(ABC-288)-ColorPicker-revision on calendar and colorPalette
    
    * (JSDocs)-(ABC-288)-Added JSDoc package and local config
    
    * (JSDocs)-(ABC-288)-ColorPickerCustom
    
    * (JSDocs)-(ABC-288)-ColorPickerPanel
    
    * (JSDocs)-(ABC-288)-Confetti
    
    * (JSDocs)-(ABC-288)-Rename on docs-Rename on Params type
    
    * (JSDocs)-(ABC-288)-Spacing on descriptions + added periods at end of descriptions on every component + renamed types
    
    * (JSDocs)-(ABC-288)-ColorPickerPanel- srcElement Deprecated - https://developer.mozilla.org/en-US/docs/Web/API/Event/srcElement
    
    * (JSDocs)-(ABC-288)-Object Types
    
    * (JSDocs)-(ABC-288)-Dialog
    
    * (JSDocs)-(ABC-288)-Avatar-Blockquote-revisited format-DualListBox partial
    
    * (JSDocs)-(ABC-288)-dualListbox.js
    
    - removed computedUniqueId function - confirmed with Renaud - was not being used or called upon.
    
    * (JSDocs)-(ABC-288)-DualListbox-examples-namespaces
    
    * (JSDoc)-(ABC-290)-DynamicMenu
    
    * (JSDoc)-(ABC-290)-ExpandableSection
    
    * (JSDoc)-(ABC-290)-FilterMenu.js
    
    * (JSDoc)-(ABC-290)-FilterMenu
    
    * (JSDoc)-(ABC-290)-FilterMenuGroup
    
    * (JSDoc)-(ABC-290)-Typedef on FilterMenuGroup.js
    
    * (JSDoc)-(ABC-290)-Illustration
    
    * (JSDoc)-(ABC-290)-Rename var Illustration
    
    * (JSDoc)-(ABC-290)-InputChoiceSet
    
    * (JSDocs)-(ABC-288)-Revision for typedefs and gen docs
    
    * (JSdoc)-(ABC-290)-JSDocs Typedef revision dynamicMenu to InputChoiceSet

---
+++ src/modules/base/filterMenuGroup/__examples__/base/base.js
@@ -0,0 +1,152 @@
+import { LightningElement } from 'lwc';
+
+const contact = [
+    {
+        label: 'Call',
+        value: 'call',
+        prefixIconName: 'standard:call',
+        iconName: 'utility:voicemail_drop'
+    },
+    {
+        label: 'Email',
+        value: 'email',
+        prefixIconName: 'standard:email'
+    },
+    {
+        label: 'Meeting',
+        value: 'meeting',
+        prefixIconName: 'standard:service_appointment',
+        disabled: true
+    },
+    {
+        label: 'Other',
+        value: 'other',
+        prefixIconName: 'standard:all'
+    }
+];
+const prices = [
+    {
+        label: 'Free',
+        value: 'free'
+    },
+    {
+        label: 'Paid',
+        value: 'paid'
+    }
+];
+
+const editions = [
+    {
+        label: 'Essentials',
+        value: 'essentials'
+    },
+    {
+        label: 'Professional',
+        value: 'professional'
+    },
+    {
+        label: 'Enterprise',
+        value: 'enterprise'
+    },
+    {
+        label: 'Unlimited',
+        value: 'unlimited'
+    },
+    {
+        label: 'Force.com',
+        value: 'forcecom'
+    },
+    {
+        label: 'Developer',
+        value: 'developer'
+    },
+    {
+        label: 'Performance',
+        value: 'performance'
+    }
+];
+
+const languages = [
+    {
+        label: 'Dutch',
+        value: 'dutch'
+    },
+    {
+        label: 'English',
+        value: 'english'
+    },
+    {
+        label: 'Finnish',
+        value: 'finnish'
+    },
+    {
+        label: 'French',
+        value: 'french'
+    },
+    {
+        label: 'German',
+        value: 'german'
+    },
+    {
+        label: 'Danish',
+        value: 'danish'
+    },
+    {
+        label: 'Italian',
+        value: 'italian'
+    },
+    {
+        label: 'Japanese',
+        value: 'japanese'
+    },
+    {
+        label: 'Korean',
+        value: 'korean'
+    },
+    {
+        label: 'Portuguese',
+        value: 'portuguese'
+    }
+];
+
+export default class FilterMenuGroupBase extends LightningElement {
+    menus = [
+        {
+            name: 'contact',
+            label: 'Type',
+            accessKey: 'k',
+            alternativeText: 'Open contact type filter',
+            items: contact,
+            tooltip: 'Type of contact',
+            value: ['email', 'meeting']
+        },
+        {
+            name: 'prices',
+            disabled: true,
+            label: 'Prices',
+            items: prices,
+            dropdownWidth: 'large',
+            droddownNubbin: true,
+            hideSelectedItems: true
+        },
+        {
+            name: 'editions',
+            items: editions,
+            label: 'Editions',
+            showSearchBox: true,
+            dropdownLength: '5-items'
+        },
+        {
+            name: 'ratings',
+            label: 'Ratings',
+            isLoading: true,
+            loadingStateAlternativeText: 'Waiting for the items to load...'
+        },
+        {
+            name: 'languages',
+            items: languages,
+            label: 'Laguages',
+            dropdownLength: '10-items'
+        }
+    ];
+}
---
+++ src/modules/base/inputChoiceSet/__examples__/checkboxes/checkboxes.js
@@ -0,0 +1,12 @@
+import { LightningElement } from 'lwc';
+
+export default class InputChoiceSetCheckboxes extends LightningElement {
+    optionsWithoutIcon = [
+        { label: 'Mon', value: 'mon' },
+        { label: 'Tue', value: 'tue' },
+        { label: 'Wed', value: 'wed' },
+        { label: 'Thu', value: 'thu' },
+        { label: 'Fri', value: 'fri' }
+    ];
+    dayValue = ['fri'];
+}
