df5585d54edc6652fc6b52267cccddcd9b750c91
Alain Rusnov <alainrusnov@gmail.com>
2021-07-28 15:12:13 -0400

    (JSDoc)-(ABC-294)-Publisher

---
+++ src/modules/base/publisher/__docs__/publisher.jsdoc.js
@@ -0,0 +1,44 @@
+/**
+ * @namespace slots
+ */
+/**
+ * Placeholder for your figure component, such as lightning-avatar.
+ *
+ * @memberof slots
+ * @name figure
+ */
+/**
+ * Placeholder for actionable components, such as lightning-button.
+ * Actions are displayed at the bottom of the publisher.
+ *
+ * @memberof slots
+ * @name actions
+ */
+/**
+ * @namespace examples
+ */
+/**
+ * @memberof examples
+ * @name variantBase
+ * @storyId example-publisher--variant-base
+ */
+/**
+ * @memberof examples
+ * @name variantComment
+ * @storyId example-publisher--variant-comment
+ */
+/**
+ * @memberof examples
+ * @name disabled
+ * @storyId example-publisher--disabled
+ */
+/**
+ * @memberof examples
+ * @name withActionsSlot
+ * @storyId example-publisher--with-actions-slot
+ */
+/**
+ * @memberof examples
+ * @name withFigureSlot
+ * @storyId example-publisher--with-figure-slot
+ */
