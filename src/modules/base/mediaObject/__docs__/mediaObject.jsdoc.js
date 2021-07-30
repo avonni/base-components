e404182f9a6699a2194fd58570b785a8bdfcbf8c
Alain Rusnov <alainrusnov@gmail.com>
2021-07-30 10:33:58 -0400

    (js doc) (abc 292) js doc media object to path (#118)
    * (JSDoc)-(ABC-292)-MediaObject
    
    * (JSDoc)-(ABC-292)-menuItemDialog
    
    * (JSDoc)-(ABC-292)-pageHeader.js
    
    * (JSDoc)-(ABC-292)-PageHeader
    
    * (JSDoc)-(ABC-292)-Pagination
    
    * (JSDoc)-(ABC-292)-Panel
    
    - check examples with this.template and css added
    
    * (JSDoc)-(ABC-292)-PanelButtons example
    
    * (JSDoc)-(ABC-292)-Path

---
+++ src/modules/base/mediaObject/__docs__/mediaObject.jsdoc.js
@@ -0,0 +1,54 @@
+/**
+ * @namespace slots
+ */
+/**
+ * Placeholder for your figure content.
+ *
+ * @memberof slots
+ * @name figure
+ */
+/**
+ * Placeholder for your figure-inverse content.
+ *
+ * @memberof slots
+ * @name figure-inverse
+ */
+/**
+ * Placeholder for your content in the media object body.
+ *
+ * @memberof slots
+ * @name default
+ */
+/**
+ * @namespace examples
+ */
+/**
+ * @memberof examples
+ * @name base
+ * @storyId example-media-object--base
+ */
+/**
+ * @memberof examples
+ * @name inverse
+ * @storyId example-media-object--inverse
+ */
+/**
+ * @memberof examples
+ * @name double
+ * @storyId example-media-object--double
+ */
+/**
+ * @memberof examples
+ * @name centerSmall
+ * @storyId example-media-object--center-small
+ */
+/**
+ * @memberof examples
+ * @name base
+ * @storyId example-media-object--end-large
+ */
+/**
+ * @memberof examples
+ * @name base
+ * @storyId example-media-object--in-line
+ */
