39d7f864cb507ac3e09c9f512fa29f501bdf1788
Alain Rusnov <alainrusnov@gmail.com>
2021-08-04 15:17:26 -0400

    (JSDoc)-(ABC-300)-JSDoc Map priv

---
+++ src/modules/base/map/__docs__/map.jsdoc.js
@@ -0,0 +1,18 @@
+/**
+* @namespace examples
+*/
+/**
+* @memberof examples
+* @name singleMarker
+* @storyId example-map--single-marker
+*/
+/**
+* @memberof examples
+* @name multipleMarkers
+* @storyId example-map--multiple-markers
+*/
+/**
+* @memberof examples
+* @name manualCenteringAndZoom
+* @storyId example-map--manual-centering-and-zoom
+*/
