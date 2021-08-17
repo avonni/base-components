dcc1200b3025a82169e376c3e5a052163cdb2612
Aliaksandr Antaniuk <aleksandrantonyuk89@gmail.com>
2021-08-17 13:16:32 +0300

    Fixed issue with the height of the spinner container.

--- src/modules/base/primitiveDatatable/primitiveDatatable.js
+++ src/modules/base/primitiveDatatable/primitiveDatatable.js
@@ -427,6 +427,12 @@ export default class PrimitiveDatatable extends LightningDatatable {
         this.tableWidth();
         this.unscrollableMainDatatable();
 
+        if (this.isLoading) {
+            this.template.querySelector(
+                'lightning-primitive-datatable-loading-indicator'
+            ).style.height = '40px';
+        }
+
         // Make sure custom edited cells stay yellow on hover
         // Make sure error cells appear edited and with a red border
         const edited = Array.from(
