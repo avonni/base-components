a0dd39e26cfe15e34c1ddba44e0cd8e2470b57d8
Renaud Archer-Fortin <71739112+renaudaf@users.noreply.github.com>
2021-11-03 13:48:56 -0400

    [fix] (ABC-387): Fixed error when delete date in datetime

--- src/modules/base/inputDateRange/inputDateRange.js
+++ src/modules/base/inputDateRange/inputDateRange.js
@@ -958,12 +958,18 @@ export default class InputDateRange extends LightningElement {
      * Dispatch changes from start-date input, end-date input, c-calendar for start-date and c-calendar for end-date.
      */
     dispatchChange() {
-        let startDate = this.startTime
-            ? `${this.startDateString} ${this.startTime}`
-            : this.startDateString;
-        let endDate = this.endTime
-            ? `${this.endDateString} ${this.endTime}`
-            : this.endDateString;
+        let startDate =
+            this.startTime && this._startDate
+                ? `${this.startDateString} ${this.startTime}`
+                : this.startDateString;
+        let endDate =
+            this.endTime && this._endDate
+                ? `${this.endDateString} ${this.endTime}`
+                : this.endDateString;
+
+        this.startTime =
+            this.startTime && this._startDate ? this.startTime : '';
+        this.endTime = this.endTime && this._endDate ? this.endTime : '';
 
         if (this.timezone) {
             startDate = new Date(startDate).toLocaleString('default', {
@@ -977,6 +983,8 @@ export default class InputDateRange extends LightningElement {
         startDate = this.startDateString ? new Date(startDate) : startDate;
         endDate = this.endDateString ? new Date(endDate) : endDate;
 
+        this._startDate = startDate;
+        this._endDate = endDate;
         /**
          * The event fired when the value changed.
          *
