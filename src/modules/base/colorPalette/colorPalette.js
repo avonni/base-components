e9dbf3d02de4684eb37203d8c802e99636db3077
Nawel Aammar <nawel.aa@hotmail.fr>
2021-12-01 13:47:30 -0500

    [doc] (ABC-423): Fix JSDoc mistakes throwing errors

--- src/modules/base/colorPalette/colorPalette.js
+++ src/modules/base/colorPalette/colorPalette.js
@@ -119,7 +119,7 @@ export default class ColorPalette extends LightningElement {
     /**
      * Array of colors displayed in the default palette. Each color can either be a string or a color object.
      *
-     * @type {(string|object)[]}
+     * @type {(string[]|object[])}
      * @default [“#e3abec”, “#c2dbf7”, ”#9fd6ff”, ”#9de7da”, ”#9df0bf”, ”#fff099”, ”#fed49a”, ”#d073df”, ”#86b9f3”, ”#5ebbff”, ”#44d8be”, ”#3be281”, ”#ffe654”, ”#ffb758”, ”#bd35bd”, ”#5778c1”, ”#5ebbff”, ”#00aea9”, ”#3bba4c”, ”#f4bc25”, ”#f99120”, ”#580d8c”, ”#001870”, ”#0a2399”, ”#097476”, ”#096a50”, ”#b67d11”, ”#b85d0d”]
      * @public
      */
