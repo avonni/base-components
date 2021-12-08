58a706b7f44fd934aef79b10ae4030a7dc5c64c3
Nawel Aammar <nawel.aa@hotmail.fr>
2021-12-08 11:56:23 -0500

    [feat] (ABC-423): Add npm script to console log doc for a component

--- scripts/generateDocs.sh
+++
@@ -1,5 +0,0 @@
-cmpname=`ls ./src/modules/base/`    
-for cmp in $cmpname
-do
-   ./node_modules/jsdoc/jsdoc.js ./src/modules/base/$cmp/$cmp.js ./src/modules/base/$cmp/__docs__/$cmp.jsdoc.js -t ./jsdoc -c ./jsdoc/jsdoc.config.json -d console > ./docs/$cmp.json
-done
