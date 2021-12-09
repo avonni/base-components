6bd5a8affed379b808b3b53e7eb04bc02889fbd0
Renaud Archer-Fortin <71739112+renaudaf@users.noreply.github.com>
2021-12-09 10:58:25 -0500

    [feat][doc] (ABC-398): Added styling hooks to input date range and input toggle

--- src/modules/base/inputToggle/__docs__/inputToggle.jsdoc.js
+++ src/modules/base/inputToggle/__docs__/inputToggle.jsdoc.js
@@ -1,4 +1,31 @@
 /**
+ * @namespace stylingHooks
+ */
+/**
+ * @memberof stylingHooks
+ * @name --avonni-input-toggle-header-text-color
+ * @default #3e3e3c
+ * @type color
+ */
+/**
+ * @memberof stylingHooks
+ * @name --avonni-input-toggle-header-font-size
+ * @default 0.75rem
+ * @type font
+ */
+/**
+ * @memberof stylingHooks
+ * @name --avonni-input-toggle-header-font-style
+ * @default normal
+ * @type font
+ */
+/**
+ * @memberof stylingHooks
+ * @name --avonni-input-toggle-header-font-weight
+ * @default 400
+ * @type font
+ */
+/**
  * @namespace examples
  */
 /**
