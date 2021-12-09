d06916f5ed150f3c7b8d144d4886434b04336179
Renaud Archer-Fortin <71739112+renaudaf@users.noreply.github.com>
2021-12-09 10:21:21 -0500

    [feat][doc] (ABC-398): Added styling hooks and documentation to input pen

--- src/modules/base/inputPen/__docs__/inputPen.jsdoc.js
+++ src/modules/base/inputPen/__docs__/inputPen.jsdoc.js
@@ -1,4 +1,31 @@
 /**
+ * @namespace stylingHooks
+ */
+/**
+ * @memberof stylingHooks
+ * @name --avonni-input-pen-header-text-color
+ * @default #3e3e3c
+ * @type color
+ */
+/**
+ * @memberof stylingHooks
+ * @name --avonni-input-pen-header-font-size
+ * @default 0.75rem
+ * @type font
+ */
+/**
+ * @memberof stylingHooks
+ * @name --avonni-input-pen-header-font-style
+ * @default normal
+ * @type font
+ */
+/**
+ * @memberof stylingHooks
+ * @name --avonni-input-pen-header-font-weight
+ * @default 400
+ * @type font
+ */
+/**
  * @namespace examples
  */
 /**
