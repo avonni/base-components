c9d80e2a46d7217e6718100e9e97dfa389c16db7
Renaud Archer-Fortin <71739112+renaudaf@users.noreply.github.com>
2021-12-09 10:33:43 -0500

    [feat][doc] (ABC-398): Added styling hooks to input choice set and input counter

--- src/modules/base/inputChoiceSet/__docs__/inputChoiceSet.jsdoc.js
+++ src/modules/base/inputChoiceSet/__docs__/inputChoiceSet.jsdoc.js
@@ -7,6 +7,33 @@
  * @property {string} iconPosition The position of the icon with respect to the label. Valid options include left, right, top and bottom. This value defaults to left.
  */
 /**
+ * @namespace stylingHooks
+ */
+/**
+ * @memberof stylingHooks
+ * @name --avonni-input-choice-set-header-text-color
+ * @default #3e3e3c
+ * @type color
+ */
+/**
+ * @memberof stylingHooks
+ * @name --avonni-input-choice-set-header-font-size
+ * @default 0.75rem
+ * @type font
+ */
+/**
+ * @memberof stylingHooks
+ * @name --avonni-input-choice-set-header-font-style
+ * @default normal
+ * @type font
+ */
+/**
+ * @memberof stylingHooks
+ * @name --avonni-input-choice-set-header-font-weight
+ * @default 400
+ * @type font
+ */
+/**
  * @namespace examples
  */
 /**
