922d184f6d83c32623ea53d41a86fc73e49d7622
Nawel Aammar <nawel.aa@hotmail.fr>
2022-01-10 13:43:22 -0500

    [fix] (ABC-408): Make list item click accessible to keyboard

--- src/modules/base/list/list.js
+++ src/modules/base/list/list.js
@@ -104,8 +104,6 @@ export default class List extends LightningElement {
     computedActions = [];
     computedItems = [];
     _hasImages;
-    menuRole;
-    itemRole;
 
     /**
      * Position of the sortable icon. Valid values include left and right.
@@ -189,11 +187,6 @@ export default class List extends LightningElement {
     }
     set sortable(bool) {
         this._sortable = normalizeBoolean(bool);
-
-        if (this._sortable) {
-            this.menuRole = 'listbox';
-            this.itemRole = 'option';
-        }
     }
 
     /**
@@ -262,6 +255,24 @@ export default class List extends LightningElement {
      */
     get hasMultipleActions() {
         return this._actions.length > 1;
+    }
+
+    /**
+     * ARIA role of the items, if the list is sortable.
+     *
+     * @type {string|undefined}
+     */
+    get itemRole() {
+        return this.sortable ? 'option' : undefined;
+    }
+
+    /**
+     * ARIA role of the menu, if the list is sortable.
+     *
+     * @type {string|undefined}
+     */
+    get menuRole() {
+        return this.sortable ? 'listbox' : undefined;
     }
 
     /**
@@ -345,15 +356,6 @@ export default class List extends LightningElement {
                 'avonni-list__item-divider_around': this._divider === 'around'
             })
             .toString();
-    }
-
-    /**
-     * Get current tab index based on if list is sortable.
-     *
-     * @type {boolean}
-     */
-    get tabindex() {
-        return this.sortable ? '0' : '-1';
     }
 
     /**
@@ -611,21 +613,24 @@ export default class List extends LightningElement {
     }
 
     /**
-     * Handler for keyboard access controls to sortable list.
+     * Handle a key pressed on an item.
      *
      * @param {Event} event
      */
     handleKeyDown(event) {
-        if (!this.sortable) return;
-
         // If space bar is pressed, select or drop the item
-        if (event.key === ' ' || event.key === 'Spacebar') {
+        if (event.key === 'Enter') {
+            this.handleItemClick(event);
+        } else if (
+            (this.sortable && event.key === ' ') ||
+            event.key === 'Spacebar'
+        ) {
             if (this._draggedElement) {
                 this.dragEnd();
             } else {
                 this.dragStart(event);
             }
-        } else if (this._draggedElement) {
+        } else if (this.sortable && this._draggedElement) {
             // If escape is pressed, cancel the move
             if (event.key === 'Escape' || event.key === 'Esc') {
                 this.computedItems = [...this._savedComputedItems];
