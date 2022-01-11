f53e670f0323eb57384f035ded08f360c9bd6e49
Renaud Archer-Fortin <71739112+renaudaf@users.noreply.github.com>
2022-01-11 13:40:32 -0500

    [doc] (ABC-445): Added documentation for styling hooks and items

---
+++ src/modules/base/verticalVisualPicker/__docs__/verticalVisualPicker.jsdoc.js
@@ -0,0 +1,154 @@
+/**
+ * @typedef {Object} VerticalVisualPickerItem
+ * @name items
+ * @property {string} description Description displayed under the title.
+ * @property {boolean} disabled If present, the item is disabled and the user cannot interact with it. Default is false.
+ * @property {string} iconName The Lightning Design System name of the icon. Specify the name in the format 'standard:account' where 'standard' is the category, and 'account' is the specific icon to be displayed.
+ * @property {string} iconPosition If present, sets the position of the icon. Valid values include right and left.
+ * @property {string} iconSize The size of the icon.
+ * @property {string[]} tags Array of tags.
+ * @property {string} title Title of the item.
+ * @property {string} value Unique value of the item.
+ */
+
+/**
+ * @namespace stylingHooks
+ */
+/**
+ * @memberof stylingHooks
+ * @name --avonni-vertical-visual-picker-header-text-color
+ * @default #3e3e3c
+ * @type color
+ */
+/**
+ * @memberof stylingHooks
+ * @name --avonni-vertical-visual-picker-header-font-size
+ * @default 0.75rem
+ * @type font
+ */
+/**
+ * @memberof stylingHooks
+ * @name --avonni-vertical-visual-picker-header-font-style
+ * @default normal
+ * @type font
+ */
+/**
+ * @memberof stylingHooks
+ * @name --avonni-vertical-visual-picker-header-font-weight
+ * @default 700
+ * @type font
+ */
+/**
+ * @memberof stylingHooks
+ * @name --avonni-vertical-visual-picker-title-text-color
+ * @default #080707
+ * @type color
+ */
+/**
+ * @memberof stylingHooks
+ * @name --avonni-vertical-visual-picker-title-font-size
+ * @default 1.25rem
+ * @type font
+ */
+/**
+ * @memberof stylingHooks
+ * @name --avonni-vertical-visual-picker-title-font-style
+ * @default normal
+ * @type font
+ */
+/**
+ * @memberof stylingHooks
+ * @name --avonni-vertical-visual-picker-title-font-weight
+ * @default 400
+ * @type font
+ */
+/**
+ * @memberof stylingHooks
+ * @name --avonni-vertical-visual-picker-description-text-color
+ * @default #3e3e3c
+ * @type color
+ */
+/**
+ * @memberof stylingHooks
+ * @name --avonni-vertical-visual-picker-description-font-size
+ * @default 0.75rem
+ * @type font
+ */
+/**
+ * @memberof stylingHooks
+ * @name --avonni-vertical-visual-picker-description-font-style
+ * @default normal
+ * @type font
+ */
+/**
+ * @memberof stylingHooks
+ * @name --avonni-vertical-visual-picker-description-font-weight
+ * @default 400
+ * @type font
+ */
+/**
+ * @memberof stylingHooks
+ * @name --avonni-vertical-visual-picker-description-line-height
+ * @default 1.25
+ * @type line-height
+ */
+/**
+ * @memberof stylingHooks
+ * @name --avonni-vertical-visual-picker-color-background
+ * @default #ffffff
+ * @type color
+ */
+/**
+ * @memberof stylingHooks
+ * @name --avonni-vertical-visual-picker-color-background-hover
+ * @default #ffffff
+ * @type color
+ */
+/**
+ * @memberof stylingHooks
+ * @name --avonni-vertical-visual-picker-color-background-selected
+ * @default #1b96ff
+ * @type color
+ */
+/**
+ * @memberof stylingHooks
+ * @name --avonni-vertical-visual-picker-color-border
+ * @default #dddbda
+ * @type color
+ */
+/**
+ * @memberof stylingHooks
+ * @name --avonni-vertical-visual-picker-color-border-hover
+ * @default #1b96ff
+ * @type color
+ */
+/**
+ * @memberof stylingHooks
+ * @name --avonni-vertical-visual-picker-color-border-selected
+ * @default #1b96ff
+ * @type color
+ */
+/**
+ * @memberof stylingHooks
+ * @name --avonni-vertical-visual-picker-sizing-border
+ * @default 1px
+ * @type sizing
+ */
+/**
+ * @memberof stylingHooks
+ * @name --avonni-vertical-visual-picker-styling-border
+ * @default solid
+ * @type styling
+ */
+/**
+ * @memberof stylingHooks
+ * @name --avonni-vertical-visual-picker-radius-border
+ * @default 0.25rem
+ * @type radius
+ */
+/**
+ * @memberof stylingHooks
+ * @name --avonni-vertical-visual-picker-shadow
+ * @default 0 2px 2px rgb(0 0 0 / 5%)
+ * @type shadow
+ */
