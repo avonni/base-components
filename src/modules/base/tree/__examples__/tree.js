cb68654d47a6e791544262c606286eaf5d8f2df9
Nawel Aammar <nawel.aa@hotmail.fr>
2022-02-03 10:06:37 -0500

    [feat] (ABC-464): Implement readOnly

--- src/modules/base/tree/__examples__/tree.js
+++ src/modules/base/tree/__examples__/tree.js
@@ -2,10 +2,11 @@ import Component from '../../storybookWrappers/tree/tree';
 
 customElements.define('avonni-tree', Component.CustomElementConstructor);
 
-export const Tree = ({ header, items, selectedItem }) => {
+export const Tree = ({ header, items, readOnly, selectedItem }) => {
     const element = document.createElement('avonni-tree');
     element.header = header;
     element.items = items;
+    element.readOnly = readOnly;
     element.selectedItem = selectedItem;
     return element;
 };
