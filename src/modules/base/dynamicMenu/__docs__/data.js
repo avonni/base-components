69ca58deeea915c863adce00233baad74895a79d
Renaud Archer-Fortin <71739112+renaudaf@users.noreply.github.com>
2022-02-10 09:58:01 -0500

    [fix]Fixed issue with dynamic-menu menu-length

--- src/modules/base/dynamicMenu/__docs__/data.js
+++ src/modules/base/dynamicMenu/__docs__/data.js
@@ -31,82 +31,98 @@ export const baseItems = [
 export const listViewItems = [
     {
         label: 'All Accounts',
-        value: 'all-accounts'
-        // actions: [
-        //     {
-        //         name: 'action-1',
-        //         iconName: 'utility:edit',
-        //         alternativeText: 'Edit'
-        //     },
-        //     {
-        //         name: 'action-2',
-        //         iconName: 'utility:delete',
-        //         alternativeText: 'Delete'
-        //     }
-        // ]
+        value: 'all-accounts',
+        actions: [
+            {
+                name: 'action-1',
+                iconName: 'utility:edit',
+                alternativeText: 'Edit'
+            },
+            {
+                name: 'action-2',
+                iconName: 'utility:delete',
+                alternativeText: 'Delete'
+            }
+        ]
     },
     {
         label: 'All Accounts 2',
-        value: 'all-accounts-2'
-        // actions: [
-        //     {
-        //         name: 'action-1',
-        //         iconName: 'utility:edit',
-        //         alternativeText: 'Edit'
-        //     },
-        //     {
-        //         name: 'action-2',
-        //         iconName: 'utility:delete',
-        //         alternativeText: 'Delete'
-        //     }
-        // ]
+        value: 'all-accounts-2',
+        actions: [
+            {
+                name: 'action-1',
+                iconName: 'utility:edit',
+                alternativeText: 'Edit'
+            },
+            {
+                name: 'action-2',
+                iconName: 'utility:delete',
+                alternativeText: 'Delete'
+            }
+        ]
     },
     {
         label: 'My Accounts',
-        value: 'my-accounts'
-        // actions: [
-        //     {
-        //         name: 'action-1',
-        //         iconName: 'utility:edit',
-        //         alternativeText: 'Edit'
-        //     },
-        //     {
-        //         name: 'action-2',
-        //         iconName: 'utility:delete',
-        //         alternativeText: 'Delete'
-        //     }
-        // ]
+        value: 'my-accounts',
+        actions: [
+            {
+                name: 'action-1',
+                iconName: 'utility:edit',
+                alternativeText: 'Edit'
+            },
+            {
+                name: 'action-2',
+                iconName: 'utility:delete',
+                alternativeText: 'Delete'
+            }
+        ]
     },
     {
         label: 'New Last Week',
-        value: 'last-week'
-        // actions: [
-        //     {
-        //         name: 'action-1',
-        //         iconName: 'utility:edit',
-        //         alternativeText: 'Edit'
-        //     },
-        //     {
-        //         name: 'action-2',
-        //         iconName: 'utility:delete',
-        //         alternativeText: 'Delete'
-        //     }
-        // ]
+        value: 'last-week',
+        actions: [
+            {
+                name: 'action-1',
+                iconName: 'utility:edit',
+                alternativeText: 'Edit'
+            },
+            {
+                name: 'action-2',
+                iconName: 'utility:delete',
+                alternativeText: 'Delete'
+            }
+        ]
     },
     {
         label: 'New This Week',
-        value: 'this-week'
-        // actions: [
-        //     {
-        //         name: 'action-1',
-        //         iconName: 'utility:edit',
-        //         alternativeText: 'Edit'
-        //     },
-        //     {
-        //         name: 'action-2',
-        //         iconName: 'utility:delete',
-        //         alternativeText: 'Delete'
-        //     }
-        // ]
+        value: 'this-week',
+        actions: [
+            {
+                name: 'action-1',
+                iconName: 'utility:edit',
+                alternativeText: 'Edit'
+            },
+            {
+                name: 'action-2',
+                iconName: 'utility:delete',
+                alternativeText: 'Delete'
+            }
+        ]
+    },
+    {
+        label: 'New This Week',
+        value: 'this-week',
+        actions: [
+            {
+                name: 'action-1',
+                iconName: 'utility:edit',
+                alternativeText: 'Edit'
+            },
+            {
+                name: 'action-2',
+                iconName: 'utility:delete',
+                alternativeText: 'Delete'
+            }
+        ]
     }
 ];
