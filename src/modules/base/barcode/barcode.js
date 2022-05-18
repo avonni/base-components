655317204f1c7e61f3a3f52cbdf5ead02921e405
ivanivanov-github <71024413+ivanivanov-github@users.noreply.github.com>
2022-05-18 10:25:09 -0400

    [feat] (ABC-610): Added rendering of barcode using jsbarcode

--- src/modules/base/barcode/barcode.js
+++ src/modules/base/barcode/barcode.js
@@ -1,37 +1,38 @@
-/**
- * BSD 3-Clause License
- *
- * Copyright (c) 2021, Avonni Labs, Inc.
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- * - Redistributions of source code must retain the above copyright notice, this
- *   list of conditions and the following disclaimer.
- *
- * - Redistributions in binary form must reproduce the above copyright notice,
- *   this list of conditions and the following disclaimer in the documentation
- *   and/or other materials provided with the distribution.
- *
- * - Neither the name of the copyright holder nor the names of its
- *   contributors may be used to endorse or promote products derived from
- *   this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
+// /**
+//  * BSD 3-Clause License
+//  *
+//  * Copyright (c) 2021, Avonni Labs, Inc.
+//  * All rights reserved.
+//  *
+//  * Redistribution and use in source and binary forms, with or without
+//  * modification, are permitted provided that the following conditions are met:
+//  *
+//  * - Redistributions of source code must retain the above copyright notice, this
+//  *   list of conditions and the following disclaimer.
+//  *
+//  * - Redistributions in binary form must reproduce the above copyright notice,
+//  *   this list of conditions and the following disclaimer in the documentation
+//  *   and/or other materials provided with the distribution.
+//  *
+//  * - Neither the name of the copyright holder nor the names of its
+//  *   contributors may be used to endorse or promote products derived from
+//  *   this software without specific prior written permission.
+//  *
+//  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+//  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+//  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+//  * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+//  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+//  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+//  * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+//  * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+//  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+//  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//  */
 
 import { LightningElement, api } from 'lwc';
 import { normalizeBoolean, normalizeString } from 'c/utilsPrivate';
+import JsBarcode from 'jsbarcode';
 
 const SYMBOLOGY = {
     valid: [
@@ -40,22 +41,22 @@ const SYMBOLOGY = {
         'UPCE',
         'UPCA',
         'Code11',
-        'Code39',
+        'CODE39',
         'Code39Extended',
         'Code93',
         'Code93Extended',
-        'Code128',
-        'Code128A',
-        'Code128B',
-        'Code128C',
+        'CODE128',
+        'CODE128A',
+        'CODE128B',
+        'CODE128C',
         'GS1-128',
-        'MSImod10',
-        'MSImod11',
-        'MSImod1010',
-        'MSImod1110',
+        'MSI10',
+        'MSI11',
+        'MSI1010',
+        'MSI1110',
         'POSTNET'
     ],
-    default: 'Code39'
+    default: 'CODE39'
 };
 
 const RENDERING_ENGINE = {
@@ -93,6 +94,34 @@ export default class Barcode extends LightningElement {
     _checksum = DEFAULT_CHECKSUM;
     _textColor = DEFAULT_TEXT_COLOR;
     _type = SYMBOLOGY.default;
+
+    renderedCallback() {
+        // let element = this.template.querySelector('.barcode');
+        // if (!element) console.log('hello');
+        // else console.log(element.nodeName);
+
+        // let element = this.template.querySelector('.barcode');
+
+        // JsBarcode('[data-element-id="barcodeId"]', "Example1234");
+
+        // JsBarcode('canvas', "Example1234");
+        // JsBarcode('canvas').init();
+
+        const canvas = this.template.querySelector(
+            '[data-element-id="barcode"]'
+        );
+        console.log(canvas.nodeName);
+        JsBarcode(canvas, this.value, {
+            format: this._type,
+            lineColor: this.color,
+            background: this._background,
+            text: this.value,
+            hideValue: this._hideValue,
+            width: this._size
+        });
+        console.log('hello');
+        JsBarcode('.barcode').init();
+    }
 
     /*
      * ------------------------------------------------------------
@@ -246,3 +275,19 @@ export default class Barcode extends LightningElement {
         `;
     }
 }
+
+// import { LightningElement } from 'lwc';
+// import JsBarcode from 'jsbarcode';
+// // import { JsBarcode } from '@babel/jsbarcode';
+
+// export default class BarcodeGenerator extends LightningElement {
+//     generateBarcode(){
+//         const canvas = this.template.querySelector('[data-id="barcode"]');
+//         console.log(canvas.nodeName);
+//         JsBarcode(canvas, "CODE39 Barcode", {
+//             format: "CODE39"
+//         });
+//         console.log('hello');
+//         JsBarcode(".barcode").init();
+//     }
+// }
