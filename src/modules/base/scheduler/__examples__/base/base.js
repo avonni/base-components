f40991e94619595bc46a3a5f5d6f982d1bde660b
Nawel Aammar <nawel.aammar@avonnicreator.com>
2022-06-17 16:54:56 -0400

    [doc] (ABC-667): Update scheduler objects and examples

--- src/modules/base/scheduler/__examples__/base/base.js
+++ src/modules/base/scheduler/__examples__/base/base.js
@@ -4,15 +4,13 @@ export default class SchedulerBase extends LightningElement {
     columns = [
         {
             label: 'Staff',
-            fieldName: 'resourceAvatarSrc',
+            fieldName: 'avatarSrc',
             type: 'avatar',
             typeAttributes: {
                 alternativeText: 'Avatar',
-                fallbackIconName: {
-                    fieldName: 'resourceAvatarFallbackIconName'
-                },
-                initials: { fieldName: 'resourceAvatarInitials' },
-                primaryText: { fieldName: 'resourceName' }
+                fallbackIconName: { fieldName: 'avatarFallbackIconName' },
+                initials: { fieldName: 'avatarInitials' },
+                primaryText: { fieldName: 'name' }
             }
         },
         {
@@ -24,70 +22,70 @@ export default class SchedulerBase extends LightningElement {
 
     events = [
         {
-            keyFields: ['3', '4', '5'],
+            resourceNames: ['Jung', 'Lily', 'Reginald'],
             name: 'event-1',
             title: 'Event 1',
             from: 1639400400000,
             to: 1639407600000
         },
         {
-            keyFields: ['5', '3', '4'],
+            resourceNames: ['Reginald', 'Jung', 'Lily'],
             name: 'event-2',
             title: 'Event 2',
             from: 1639404000000,
             to: 1639411200000
         },
         {
-            keyFields: ['3', '4', '2'],
+            resourceNames: ['Jung', 'Lily', 'Dave'],
             name: 'event-3',
             title: 'Event 3',
             from: 1639407600000,
             to: 1639414800000
         },
         {
-            keyFields: ['1', '4'],
+            resourceNames: ['Nina', 'Lily'],
             name: 'event-4',
             title: 'Event 4',
             from: 1639411200000,
             to: 1639418400000
         },
         {
-            keyFields: ['2', '1'],
+            resourceNames: ['Dave', 'Nina'],
             name: 'event-5',
             title: 'Event 5',
             from: 1639414800000,
             to: 1639422000000
         },
         {
-            keyFields: ['1', '2', '3'],
+            resourceNames: ['Nina', 'Dave', 'Jung'],
             name: 'event-6',
             title: 'Event 6',
             from: 1639418400000,
             to: 1639425600000
         },
         {
-            keyFields: ['1'],
+            resourceNames: ['Nina'],
             name: 'event-7',
             title: 'Event 7',
             from: 1639422000000,
             to: 1639429200000
         },
         {
-            keyFields: ['3'],
+            resourceNames: ['Jung'],
             name: 'event-8',
             title: 'Event 8',
             from: 1639425600000,
             to: 1639432800000
         },
         {
-            keyFields: ['1'],
+            resourceNames: ['Nina'],
             name: 'event-9',
             title: 'Event 9',
             from: 1639429200000,
             to: 1639436400000
         },
         {
-            keyFields: ['2', '3', '4'],
+            resourceNames: ['Dave', 'Jung', 'Lily'],
             name: 'event-10',
             title: 'Event 10',
             from: 1639432800000,
@@ -97,47 +95,42 @@ export default class SchedulerBase extends LightningElement {
 
     resources = [
         {
-            id: '1',
-            resourceAvatarSrc:
+            avatarSrc:
                 'https://www.lightningdesignsystem.com/assets/images/avatar2.jpg',
-            resourceAvatarFallbackIconName: 'standard:person_account',
-            resourceAvatarInitials: 'NG',
-            resourceName: 'Nina',
+            avatarFallbackIconName: 'standard:person_account',
+            avatarInitials: 'NG',
+            name: 'Nina',
             role: 'Lead developer',
             sharedField: `This shouldn't show up`
         },
         {
-            id: '2',
-            resourceAvatarSrc:
+            avatarSrc:
                 'https://www.lightningdesignsystem.com/assets/images/avatar1.jpg',
-            resourceAvatarFallbackIconName: 'standard:person_account',
-            resourceAvatarInitials: 'DM',
-            resourceName: 'Dave',
+            avatarFallbackIconName: 'standard:person_account',
+            avatarInitials: 'DM',
+            name: 'Dave',
             role: 'UX Specialist',
             customRowField: 'Label coming from a custom field in the row'
         },
         {
-            id: '3',
-            resourceAvatarFallbackIconName: 'standard:person_account',
-            resourceAvatarInitials: 'JP',
-            resourceName: 'Jung',
+            avatarFallbackIconName: 'standard:person_account',
+            avatarInitials: 'JP',
+            name: 'Jung',
             role: 'Product Owner'
         },
         {
-            id: '4',
-            resourceAvatarFallbackIconName: 'standard:article',
-            resourceAvatarInitials: 'LM',
-            resourceName: 'Lily',
+            avatarFallbackIconName: 'standard:article',
+            avatarInitials: 'LM',
+            name: 'Lily',
             role: 'Graphic Designer',
             customField: "This comes from the row's custom field"
         },
         {
-            id: '5',
-            resourceAvatarSrc:
+            avatarSrc:
                 'https://www.lightningdesignsystem.com/assets/images/avatar3.jpg',
-            resourceAvatarFallbackIconName: 'standard:person_account',
-            resourceAvatarInitials: 'RM',
-            resourceName: 'Reginald',
+            avatarFallbackIconName: 'standard:person_account',
+            avatarInitials: 'RM',
+            name: 'Reginald',
             role: 'Developer'
         }
     ];
