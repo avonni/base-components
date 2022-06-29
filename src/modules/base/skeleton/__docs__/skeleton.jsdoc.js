015845b6ea599eccb5d2a8700ece26fbc2f52149
ivanivanov-github <71024413+ivanivanov-github@users.noreply.github.com>
2022-06-29 11:13:48 -0400

    [doc] (ABC-632): added styling hook documentation

--- src/modules/base/skeleton/__docs__/skeleton.jsdoc.js
+++ src/modules/base/skeleton/__docs__/skeleton.jsdoc.js
@@ -8,3 +8,38 @@
  * @default #dedede
  * @type color
  */
+
+/**
+ * @memberof stylingHooks
+ * @name --avonni-chip-sizing-border
+ * @default 1px
+ * @type styling
+ */
+
+/**
+ * @memberof stylingHooks
+ * @name --avonni-chip-label-font-size
+ * @default 0.75rem
+ * @type styling
+ */
+
+/**
+ * @memberof stylingHooks
+ * @name --avonni-chip-line-height
+ * @default normal
+ * @type styling
+ */
+
+/**
+ * @memberof stylingHooks
+ * @name --avonni-avatar-primary-text-font-size
+ * @default 0.875rem
+ * @type styling
+ */
+
+/**
+ * @memberof stylingHooks
+ * @name --avonni-avatar-secondary-text-font-size
+ * @default 0.8125rem
+ * @type styling
+ */
