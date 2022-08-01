b9386b5dab3f4957a97e9d5f5ef16013b8b0eb66
Achille Saint-Hillier <achillesainthillier@gmail.com>
2022-08-01 09:20:42 -0400

    [doc] (ABC-617): Fix avatar JSDoc

--- src/modules/base/kanban/__docs__/kanban.jsdoc.js
+++ src/modules/base/kanban/__docs__/kanban.jsdoc.js
@@ -11,11 +11,14 @@
 /**
  * @typedef {Object} KanbanGroup
  * @name groups
+ * @property {object[]} avatar Avatar object. The avatar is displayed to the left of the group label.
  * @property {string} backgroundColor Color of the group's background.
+ * @property {string} pathColor Color of the path item when the variant is path.
  * @property {string} label Group label.
  * @property {string} value Unique value for the group.
  * @property {object[]} headerActions Array of actions to be displayed in the group header.
  * @property {object[]} footerActions Array of actions to be displayed in the group footer.
+ * @property {boolean} showItemCount If true, displays the number of items contained bu the group in it's header.
  */
 
 /**
@@ -25,6 +28,15 @@
  * @property {string} label Label of the field.
  * @property {string} type Required. The data type to be used for data formatting.
  * @property {object} typeAttributes Provides custom formatting with component attributes for the data type. For example, currency-code for the currency type.
+ */
+
+/**
+ * @typedef {Object} Avatar
+ * @name avatar
+ * @property {string} fallbackIconName The Lightning Design System name of the icon used as a fallback when the image fails to load. The initials fallback relies on this for its background color. Names are written in the format 'standard:account' where 'standard' is the category, and 'account' is the specific icon to be displayed. Only icons from the standard and custom categories are allowed.
+ * @property {string} initials If the record name contains two words, like first and last name, use the first capitalized letter of each. For records that only have a single word name, use the first two letters of that word using one capital and one lower case letter.
+ * @property {string} src Image URL for the avatar of the group. If present, the avatar is displayed before the label.
+ * @property {object} variant The variant changes the shape of the avatar. Valid values are empty, circle, and square. The value defaults to square.
  */
 
 /**
