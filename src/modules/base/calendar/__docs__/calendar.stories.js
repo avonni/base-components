26fd973e457b6da1b854515c8a688855404c0a33
ivanivanov-github <71024413+ivanivanov-github@users.noreply.github.com>
2022-08-01 23:08:47 -0400

    Abc 598 calendar add option to hide navigation (#390)
    * [feat] (ABC-598): added attribute hide-header
    
    * [test] (ABC-632): added test for hide-header
    
    * [feat] (ABC-598): changed hide-header to hide-navigation and added 'navigate' event when previous or next button is clicked (works with keyboard also)
    
    * [test] (ABC-598): added tests for navigate event
    
    * [feat] (ABC-598): added navigate event for keyboard events
    
    * [test] (ABC-598): added tests 'navigate' event with keyboard
    
    * [style] (ABC-598): removed console.log
    
    * [feat] (ABC-598): added 'navigate' event when year is changed with keyboard and combobox
    
    * [style] (ABC-598): Remove console.log
    
    * [doc] (ABC-598): Fix navigate event detail description
    
    Co-authored-by: Nawel Aammar <nawel.aammar@avonnicreator.com>

--- src/modules/base/calendar/__docs__/calendar.stories.js
+++ src/modules/base/calendar/__docs__/calendar.stories.js
@@ -63,6 +63,17 @@ export default {
                 type: { summary: 'string|string[]' }
             }
         },
+        hideNavigation: {
+            name: 'hide-navigation',
+            control: {
+                type: 'boolean'
+            },
+            description: 'Specifies if the calendar header should be hidden.',
+            table: {
+                type: { summary: 'boolean' },
+                defaultValue: { summary: 'false' }
+            }
+        },
         markedDates: {
             name: 'marked-dates',
             control: {
@@ -135,6 +146,7 @@ export default {
         disabled: false,
         dateLabels: [],
         disabledDates: [],
+        hideNavigation: false,
         markedDates: [],
         min: new Date(1900, 0, 1),
         max: new Date(2099, 11, 31),
