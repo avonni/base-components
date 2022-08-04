7b53c7537b9b1fd348fcb093eaec26d56d8de7ae
Nawel Aammar <nawel.aammar@avonnicreator.com>
2022-08-04 15:06:18 -0400

    [feat] (ABC-683): Add border styling hooks to splitter

--- src/modules/base/splitter/__docs__/splitter.jsdoc.js
+++ src/modules/base/splitter/__docs__/splitter.jsdoc.js
@@ -7,3 +7,31 @@
  * @memberof slots
  * @name default
  */
+
+/**
+ * @namespace stylingHooks
+ */
+/**
+ * @memberof stylingHooks
+ * @name --avonni-splitter-vertical-color-border-around
+ * @default #00000014
+ * @type color
+ */
+/**
+ * @memberof stylingHooks
+ * @name --avonni-splitter-horizontal-color-border-around
+ * @default #00000014
+ * @type color
+ */
+/**
+ * @memberof stylingHooks
+ * @name --avonni-splitter-horizontal-color-border-separator
+ * @default #00000014
+ * @type color
+ */
+/**
+ * @memberof stylingHooks
+ * @name --avonni-splitter-vertical-color-border-separator
+ * @default #00000014
+ * @type color
+ */
