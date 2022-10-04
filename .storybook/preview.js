aac25aa0c6acc88afa2454fd87dc36a55a196abb
lgilmet <36134032+lgilmet@users.noreply.github.com>
2022-10-04 16:01:53 -0400

    [debug] (abc-788): fix storycap installation

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -3,5 +3,13 @@ import { withScreenshot } from 'storycap';
 
 export const decorators = [withScreenshot];
 export const parameters = {
-    actions: { argTypesRegex: '^on[A-Z].*' }
+    actions: { argTypesRegex: '^on[A-Z].*' },
+    layout: 'centered',
+    screenshot: {
+        fullPage: false,
+        viewports: {
+            small: { width: 500, height: 300 },
+            large: { width: 700, height: 500 }
+        }
+    }
 };
