aae5a0b4735f58a6e5980a63cb1d776ddd4c3f3e
lgilmet <36134032+lgilmet@users.noreply.github.com>
2022-10-04 16:48:07 -0400

    [debug] (abc-788): debugging storycap screenshot params - it uses the defaults param. very buggy

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -5,11 +5,27 @@ export const decorators = [withScreenshot];
 export const parameters = {
     actions: { argTypesRegex: '^on[A-Z].*' },
     layout: 'centered',
+
+    // These settings fail. It keeps using default viewport [ '800x600' ].
     screenshot: {
+        omitBackground: true,
         fullPage: false,
         viewports: {
-            small: { width: 500, height: 300 },
-            large: { width: 700, height: 500 }
+            small: {
+                width: 300,
+                height: 150,
+                deviceScaleFactor: 2
+            },
+            medium: {
+                width: 450,
+                height: 225,
+                deviceScaleFactor: 2
+            },
+            large: {
+                width: 600,
+                height: 300,
+                deviceScaleFactor: 2
+            }
         }
     }
 };
