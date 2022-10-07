938998b1bd0dce10f239fe56ce251a87c20e50f3
lucas guillemette <36134032+lgilmet@users.noreply.github.com>
2022-10-07 14:11:58 -0400

    Feature abc 610 create barcode component (#431)
    * [feat] (ABC-610): created new barcode component
    
    * [feat] (ABC-610): Added all attributes of bar code component
    
    * [feat] (ABC-610): Added all boiler plate getters and setters for barcode
    
    * [feat] (ABC-610): Added html for barcode component
    
    * [feat] (ABC-610): Added stotybook controls for barcode
    
    * [feat] (ABC-610): Added base variant for barcode
    
    * [feat] (ABC-610): Added bwip-js and jsbarcode libraries
    
    * [feat] (ABC-610): Added rendering of barcode using jsbarcode
    
    * [feat] (ABC-610): Barcode html uses only the canvas
    
    * [feat] (ABC-610): Added type control to barcode story
    
    * [feat] (ABC-610): Added checksum to html and js, and used get and set for JsBarCode
    
    * [feat] (ABC-610): resizing works with hidechecksumvalue but not with hidevalue
    
    * [feat] (ABC-610): barcode value and checksum are aligned with the canvas, and the hide value/checksum works
    
    * [feat] (ABC-610): used host instead of wrapper and computed value and checksum style
    
    * [feat] (ABC-610): resizing works in px
    
    * [feat] (ABC-610): Added text color
    
    * [feat] (ABC-610): added checksum calculation for ean13
    
    * [feat] (ABC-610): added checksum for code39 and ean8
    
    * [refactor] (ABC-610): refactored render switch case to use barcode category instead of type
    
    * [WIP] (ABC-610): added categories to BARCODE_CATOGORY
    
    * [refactor] (ABC-610): commit before refactor with bwipp-js library for barcode
    
    * [refactor] (ABC-610): refactor different encodings use different libraries
    
    * [doc/style] (ABC-610): commit before adding doc and style
    
    * [style/doc] (ABC-610): Added styling and doc barcode
    
    * [test] (ABC-610): added first attribute test
    
    * [fix] (ABC-610): fixed checksum and hidevalue logic
    
    * [test] (ABC-610): Added tests for barcode
    
    * [feat] (ABC-610): Added renderAs attribute barcode
    
    * [fix] (ABC-610): added legal header, erased clg and updated lwc.config for barcode
    
    * [wip] (ABC-610): removed file mocked-version-bwip.js
    
    * [wip] (ABC-610): changed story to use args key for default attributes
    
    * [wip] (ABC-610): removed canvas from package.json
    
    * [wip] (ABC-610): minor fixed to syntax
    
    * [wip] (ABC-610): replaced useless get/set with @api
    
    * [wip] (ABC-610): replaced getSomething functions with get something private functions
    
    * [refactor] (ABC-610): refactored renderWithBwipJs to use checksum value instead of if condition
    
    * [fix] (ABC-610: replaced custom avonni wrapper classes with slds grid classes
    
    * [test/doc] (ABC-610): removed unecessary @api for renderBarcode method
    
    * [fix] (ABC-610):
    invalid format exception caught
    message displayed suggesting the format that should be followed for the corresponding barcode type
    
    * [refactor] (abc-610): Refactor barcode
    - Remove support for jsbarcode
    
    * [refactor][style] (abc-610): Optimizing barcode and style
    
    * [style] (abc-610): Storybook examples
    
    * [wip] (abc-610): Add text align
    
    * [style][test] (abc-610): Add tests
    
    * [fix] (abc-610): remove jsbarcode package
    
    * [refactor] (abc-610): Improve error handling
    
    * [test][style] (abc-610): Test all types, add examples
    
    * [test][doc] (abc-610): Checkout test, examples and doc
    
    * [style] (abc-610): Remove redundant condition
    
    * [style] (abc-610): Clean up and try to make tests
    
    * [style][test] (abc-610): Tests + improvements
    
    * [style][test][doc] (abc-610): Allow value to update after error
    - support all length units
    
    * [fix][style] (abc-610): normalize height and width
    
    Co-authored-by: ivanivanov-github <71024413+ivanivanov-github@users.noreply.github.com>

---
+++ src/modules/base/barcode/__tests__/barcode.test.js
@@ -0,0 +1,220 @@
+/**
+ * BSD 3-Clause License
+ *
+ * Copyright (c) 2021, Avonni Labs, Inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * - Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ *
+ * - Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * - Neither the name of the copyright holder nor the names of its
+ *   contributors may be used to endorse or promote products derived from
+ *   this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+import { createElement } from 'lwc';
+import Barcode from 'c/barcode';
+import bwipjs from 'bwip-js';
+
+const baseParameters = {
+    backgroundcolor: 'ffffff',
+    barcolor: '000000',
+    includecheck: false,
+    includecheckintext: false,
+    includetext: true,
+    scale: 10,
+    textcolor: '000000',
+    textxalign: 'center',
+    textyalign: 'below'
+};
+
+const handler = jest.fn();
+bwipjs.toCanvas = handler;
+let element;
+describe('Barcode', () => {
+    beforeEach(() => {
+        element = createElement('avonni-barcode', {
+            is: Barcode
+        });
+        document.body.appendChild(element);
+    });
+
+    afterEach(() => {
+        while (document.body.firstChild) {
+            document.body.removeChild(document.body.firstChild);
+        }
+        jest.clearAllMocks();
+    });
+
+    it('Barcode: Default attributes', () => {
+        expect(element.background).toBe('#ffffff');
+        expect(element.checksum).toBe(false);
+        expect(element.color).toBe('#000000');
+        expect(element.height).toBeUndefined();
+        expect(element.hideValue).toBe(false);
+        expect(element.textAlignment).toBe('bottom-center');
+        expect(element.textColor).toBe('#000000');
+        expect(element.type).toBeUndefined();
+        expect(element.value).toBeUndefined();
+        expect(element.width).toBe('100%');
+    });
+
+    /* ----- ATTRIBUTES ----- */
+
+    // VALUES
+    it('Barcode: type', () => {
+        element.type = 'code128';
+
+        return Promise.resolve().then(() => {
+            const canvas = element.shadowRoot.querySelector(
+                '[data-element-id="avonni-barcode-canvas"]'
+            );
+            const barcodeParams = { ...baseParameters };
+            barcodeParams.bcid = 'code128';
+
+            expect(handler).toHaveBeenCalledWith(canvas, barcodeParams);
+        });
+    });
+
+    it('Barcode: invalid type', () => {
+        element.type = 'not-a-valid-type';
+
+        return Promise.resolve().then(() => {
+            const canvas = element.shadowRoot.querySelector(
+                '[data-element-id="avonni-barcode-canvas"]'
+            );
+            const barcodeParams = { ...baseParameters };
+            barcodeParams.bcid = '';
+
+            expect(handler).toHaveBeenCalledWith(canvas, barcodeParams);
+        });
+    });
+
+    it('Barcode: value', () => {
+        element.value = '1234';
+
+        return Promise.resolve().then(() => {
+            const canvas = element.shadowRoot.querySelector(
+                '[data-element-id="avonni-barcode-canvas"]'
+            );
+            const barcodeParams = { ...baseParameters };
+            barcodeParams.text = '1234';
+
+            expect(handler).toHaveBeenCalledWith(canvas, barcodeParams);
+        });
+    });
+
+    it('Barcode: checksum', () => {
+        element.value = '12345';
+        element.type = 'code128';
+        element.checksum = false;
+
+        return Promise.resolve().then(() => {
+            const canvas = element.shadowRoot.querySelector(
+                '[data-element-id="avonni-barcode-canvas"]'
+            );
+            const barcodeParams = { ...baseParameters };
+            barcodeParams.text = '12345';
+            barcodeParams.bcid = 'code128';
+            barcodeParams.includecheck = false;
+            barcodeParams.includecheckintext = false;
+
+            expect(handler).toHaveBeenCalledWith(canvas, barcodeParams);
+        });
+    });
+
+    it('Barcode: hide-value', () => {
+        element.value = '12345';
+        element.type = 'code128';
+        element.hideValue = true;
+
+        return Promise.resolve().then(() => {
+            const canvas = element.shadowRoot.querySelector(
+                '[data-element-id="avonni-barcode-canvas"]'
+            );
+            const barcodeParams = { ...baseParameters };
+            barcodeParams.text = '12345';
+            barcodeParams.bcid = 'code128';
+            barcodeParams.includetext = false;
+
+            expect(handler).toHaveBeenCalledWith(canvas, barcodeParams);
+        });
+    });
+
+    // COLORS
+    it('Barcode: background, color and text-color', () => {
+        element.background = '#eeeeee';
+        element.color = '#333333';
+        element.textColor = '#444444';
+
+        return Promise.resolve().then(() => {
+            const canvas = element.shadowRoot.querySelector(
+                '[data-element-id="avonni-barcode-canvas"]'
+            );
+            const barcodeParams = { ...baseParameters };
+            barcodeParams.text = undefined;
+            barcodeParams.bcid = undefined;
+            barcodeParams.backgroundcolor = 'eeeeee';
+            barcodeParams.barcolor = '333333';
+            barcodeParams.textcolor = '444444';
+
+            expect(handler).toHaveBeenCalledWith(canvas, barcodeParams);
+        });
+    });
+
+    // LAYOUT
+    it('Barcode: height and width as numbers and text-alignment', () => {
+        element.value = '1234';
+        element.type = 'code11';
+        element.height = 200;
+        element.width = '200';
+        element.textAlignment = 'top-justify';
+
+        return Promise.resolve().then(() => {
+            const canvas = element.shadowRoot.querySelector(
+                '[data-element-id="avonni-barcode-canvas"]'
+            );
+            const barcodeParams = { ...baseParameters };
+            barcodeParams.text = '1234';
+            barcodeParams.bcid = 'code11';
+            barcodeParams.textxalign = 'justify';
+            barcodeParams.textyalign = 'above';
+
+            expect(handler).toHaveBeenCalledWith(canvas, barcodeParams);
+            expect(canvas.style.maxHeight).toEqual('200px');
+            expect(canvas.style.width).toEqual('200px');
+        });
+    });
+
+    it('Barcode: height and width with units', () => {
+        element.height = '30rem';
+        element.width = '80%';
+
+        return Promise.resolve().then(() => {
+            const canvas = element.shadowRoot.querySelector(
+                '[data-element-id="avonni-barcode-canvas"]'
+            );
+
+            expect(canvas.style.maxHeight).toEqual('30rem');
+            expect(canvas.style.width).toEqual('80%');
+        });
+    });
+});
