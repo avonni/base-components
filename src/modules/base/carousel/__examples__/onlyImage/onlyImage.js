7096385ece9e7ecdb1ea68d5bf9d513e37c1546a
Renaud Archer-Fortin <71739112+renaudaf@users.noreply.github.com>
2023-02-13 10:57:07 -0500

    [feat](ABC-904): Carousel - Added possibility to only have image

---
+++ src/modules/base/carousel/__examples__/onlyImage/onlyImage.js
@@ -0,0 +1,48 @@
+import { LightningElement } from 'lwc';
+
+export default class AvonniOnlyImage extends LightningElement {
+    items = [
+        {
+            name: '1',
+            imageAssistiveText: 'Appy',
+            src: 'https://react.lightningdesignsystem.com/assets/images/carousel/carousel-01.jpg',
+            href: 'https://www.salesforce.com'
+        },
+        {
+            name: '2',
+            imageAssistiveText: 'Appy',
+            src: 'https://react.lightningdesignsystem.com/assets/images/carousel/carousel-02.jpg',
+            href: 'https://www.salesforce.com'
+        },
+        {
+            name: '3',
+            imageAssistiveText: 'Salesforce Apps',
+            src: 'https://react.lightningdesignsystem.com/assets/images/carousel/carousel-03.jpg',
+            href: 'https://www.salesforce.com'
+        },
+        {
+            name: '4',
+            imageAssistiveText: 'Appy',
+            src: 'https://react.lightningdesignsystem.com/assets/images/carousel/carousel-02.jpg',
+            href: 'https://www.salesforce.com'
+        },
+        {
+            name: '5',
+            imageAssistiveText: 'Appy',
+            src: 'https://react.lightningdesignsystem.com/assets/images/carousel/carousel-01.jpg',
+            href: 'https://www.salesforce.com'
+        },
+        {
+            name: '6',
+            imageAssistiveText: 'Salesforce Apps',
+            src: 'https://react.lightningdesignsystem.com/assets/images/carousel/carousel-03.jpg',
+            href: 'https://www.salesforce.com'
+        },
+        {
+            name: '7',
+            imageAssistiveText: 'Appy',
+            src: 'https://react.lightningdesignsystem.com/assets/images/carousel/carousel-02.jpg',
+            href: 'https://www.salesforce.com'
+        }
+    ];
+}
