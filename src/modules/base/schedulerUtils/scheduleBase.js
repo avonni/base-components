e263b51f3dc01edb6bd7b36b07a7c6a917a06b36
Nawel Aammar <nawel.aammar@avonnicreator.com>
2023-02-13 17:34:44 +0100

    [feat] (ABC-901): Add scheduleclick event to scheduler (#538)
    * [feat] (ABC-893): Add scheduleclick event to scheduler
    
    * [fix] (ABC-901): Fix agenda scheduleclick event

--- src/modules/base/schedulerUtils/scheduleBase.js
+++ src/modules/base/schedulerUtils/scheduleBase.js
@@ -1169,6 +1169,31 @@ export class ScheduleBase extends LightningElement {
         window.addEventListener('mouseup', mouseUp);
     }
 
+    dispatchScheduleClick({ from, to }) {
+        if (!from || !to) {
+            return;
+        }
+
+        /**
+         * The event fired when the user clicks on a time slot of the schedule.
+         *
+         * @event
+         * @name scheduleclick
+         * @param {string} from Start of the clicked cell as an ISO 8601 string.
+         * @param {string} to End of the clicked cell as an ISO 8601 string.
+         * @public
+         */
+        this.dispatchEvent(
+            new CustomEvent('scheduleclick', {
+                detail: {
+                    from: this.createDate(Number(from)).toISO(),
+                    to: this.createDate(Number(to)).toISO()
+                },
+                bubbles: true
+            })
+        );
+    }
+
     /**
      * Dispatch the `eventchange` event.
      *
