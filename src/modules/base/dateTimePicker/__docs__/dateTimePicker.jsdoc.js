4489ee3a2c6d9ab1c1be8033ce1563f1631ee458
Nawel Aammar <nawel.aammar@avonnicreator.com>
2023-03-30 10:21:53 -0400

    [feat] (ABC-930): Add styling hooks to date time picker

--- src/modules/base/dateTimePicker/__docs__/dateTimePicker.jsdoc.js
+++ src/modules/base/dateTimePicker/__docs__/dateTimePicker.jsdoc.js
@@ -13,25 +13,133 @@
  */
 /**
  * @memberof stylingHooks
+ * @name --avonni-date-time-picker-button-color-background
+ * @default #ffffff
+ * @type color
+ */
+/**
+ * @memberof stylingHooks
+ * @name --avonni-date-time-picker-button-color-background-disabled
+ * @default #f3f2f2
+ * @type color
+ */
+/**
+ * @memberof stylingHooks
+ * @name --avonni-date-time-picker-button-color-background-selected
+ * @default #d8edff
+ * @type color
+ */
+/**
+ * @memberof stylingHooks
+ * @name --avonni-date-time-picker-button-color-border
+ * @default #dddbda
+ * @type color
+ */
+/**
+ * @memberof stylingHooks
+ * @name --avonni-date-time-picker-button-color-border-selected
+ * @default #0070d2
+ * @type color
+ */
+/**
+ * @memberof stylingHooks
+ * @name --avonni-date-time-picker-button-sizing-border
+ * @default 1px
+ * @type dimension
+ */
+/**
+ * @memberof stylingHooks
+ * @name --avonni-date-time-picker-button-sizing-border-selected
+ * @default 1px
+ * @type dimension
+ */
+/**
+ * @memberof stylingHooks
+ * @name --avonni-date-time-picker-button-styling-border
+ * @default solid
+ * @type keyword
+ */
+/**
+ * @memberof stylingHooks
+ * @name --avonni-date-time-picker-button-styling-border-selected
+ * @default solid
+ * @type keyword
+ */
+/**
+ * @memberof stylingHooks
+ * @name --avonni-date-time-picker-button-text-color
+ * @default #181818
+ * @type color
+ */
+/**
+ * @memberof stylingHooks
+ * @name --avonni-date-time-picker-button-text-color-selected
+ * @default #181818
+ * @type color
+ */
+/**
+ * @memberof stylingHooks
+ * @name --avonni-date-time-picker-button-text-color-disabled
+ * @default #706e6b
+ * @type color
+ */
+/**
+ * @memberof stylingHooks
+ * @name --avonni-date-time-picker-date-label-line-height
+ * @default 1.25
+ * @type number
+ */
+/**
+ * @memberof stylingHooks
+ * @name --avonni-date-time-picker-date-label-font-size
+ * @default 1.25rem
+ * @type dimension
+ */
+/**
+ * @memberof stylingHooks
+ * @name --avonni-date-time-picker-date-label-font-weight
+ * @default 400
+ * @type integer
+ */
+/**
+ * @memberof stylingHooks
+ * @name --avonni-date-time-picker-label-font-size
+ * @default 0.75rem
+ * @type dimension
+ */
+/**
+ * @memberof stylingHooks
+ * @name --avonni-date-time-picker-label-font-style
+ * @default normal
+ * @type keyword
+ */
+/**
+ * @memberof stylingHooks
+ * @name --avonni-date-time-picker-label-font-weight
+ * @default 700
+ * @type integer
+ */
+/**
+ * @memberof stylingHooks
+ * @name --avonni-date-time-picker-label-line-height
+ * @default 1.5
+ * @type number
+ */
+/**
+ * @memberof stylingHooks
  * @name --avonni-date-time-picker-label-text-color
  * @default #444444
  * @type color
  */
 /**
  * @memberof stylingHooks
- * @name --avonni-date-time-picker-label-font-size
- * @default 0.75rem
- * @type font
+ * @name --avonni-date-time-picker-weekly-day-header-text-color
+ * @default #706e6b
+ * @type color
  */
 /**
  * @memberof stylingHooks
- * @name --avonni-date-time-picker-label-font-style
- * @default normal
- * @type font
- */
-/**
- * @memberof stylingHooks
- * @name --avonni-date-time-picker-label-font-weight
- * @default 700
- * @type font
+ * @name --avonni-date-time-picker-weekly-day-header-text-color-today
+ * @default #1589ee
+ * @type color
  */
