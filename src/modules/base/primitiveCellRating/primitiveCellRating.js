5eaf330233a07617f561217179b3d1c616c707e8
Renaud Archer-Fortin <71739112+renaudaf@users.noreply.github.com>
2023-08-09 13:07:45 -0400

    [fix](ABC-1027): Data Table - Rating, CheckboxButton, Slider, Toggle, dispactch cellchangecustom (#649)

--- src/modules/base/primitiveCellRating/primitiveCellRating.js
+++ src/modules/base/primitiveCellRating/primitiveCellRating.js
@@ -66,12 +66,28 @@ export default class PrimitiveCellRating extends LightningElement {
         const detail = {
             value: event.detail.value,
             colKeyValue: this.colKeyValue,
-            rowKeyValue: this.rowKeyValue
+            rowKeyValue: this.rowKeyValue,
+            callbacks: {
+                dispatchCellChangeEvent: this.dispatchCellChangeEvent.bind(this)
+            }
         };
 
         this.dispatchEvent(
             new CustomEvent('privateeditcustomcell', {
                 detail: detail,
+                bubbles: true,
+                composed: true
+            })
+        );
+    }
+
+    dispatchCellChangeEvent(state) {
+        const dirtyValues = state.inlineEdit.dirtyValues;
+        this.dispatchEvent(
+            new CustomEvent('cellchangecustom', {
+                detail: {
+                    draftValues: dirtyValues
+                },
                 bubbles: true,
                 composed: true
             })
