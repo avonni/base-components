e175471bb25184f6a8b20a956b5987c8b997940d
Hubert06 <44268774+Hubert06@users.noreply.github.com>
2023-08-16 10:16:36 -0400

    [refactor](ABC-1030): Follow our own code conventions

--- src/modules/base/focusTrap/focusTrap.js
+++ src/modules/base/focusTrap/focusTrap.js
@@ -18,6 +18,22 @@ export default class FocusTrap extends LightningElement {
         }
     }
 
+    /*
+     * ------------------------------------------------------------
+     *  PRIVATE PROPERTIES
+     * -------------------------------------------------------------
+     */
+
+    get bookendTabIndex() {
+        return this._focused ? '0' : '-1';
+    }
+
+    /*
+     * ------------------------------------------------------------
+     *  PUBLIC METHODS
+     * -------------------------------------------------------------
+     */
+
     /**
      * Focuses the first focusable element in the focus trap.
      */
@@ -26,38 +42,29 @@ export default class FocusTrap extends LightningElement {
         if (!this._focused) {
             // We could potentially add support for focusing the element that has 'autofocus' attribute on it,
             // and if none, then focus on the first element
-            this._focusFirstElement();
+            this.focusFirstElement();
         }
     }
 
-    get _bookendTabIndex() {
-        return this._focused ? '0' : '-1';
-    }
+    /*
+     * ------------------------------------------------------------
+     *  PRIVATE METHODS
+     * -------------------------------------------------------------
+     */
 
-    _handleFocusIn() {
-        if (this._pendingFocusOut) {
-            this._pendingFocusOut = false;
-        }
-        this._focused = true;
-    }
-
-    _handleFocusOut() {
-        // This assumes that a focusin will be dispatched after a focusout
-        this._pendingFocusOut = true;
-        // eslint-disable-next-line @lwc/lwc/no-async-operation
-        requestAnimationFrame(() => {
-            if (this._pendingFocusOut) {
-                this._focused = false;
-            }
-        });
+    /**
+     * Returns a list of the focusable children found within the element.
+     */
+    getFocusableElements() {
+        return findAllTabbableElements(this.template.querySelector('slot'));
     }
 
     /**
      * Focuses on the specified element location.
      * @param {String} elementLocation Could be 'first or 'last'.
      */
-    _moveFocusTo(elementLocation) {
-        const focusableElements = this._getFocusableElements();
+    moveFocusTo(elementLocation) {
+        const focusableElements = this.getFocusableElements();
         if (focusableElements.length > 0) {
             let node;
             if (elementLocation === 'last') {
@@ -69,24 +76,41 @@ export default class FocusTrap extends LightningElement {
         }
     }
 
+    /*
+     * ------------------------------------------------------------
+     *  EVENT HANLDERS AND DISPATCHERS
+     * -------------------------------------------------------------
+     */
+
     /**
      * Focuses the last focusable element in the focus trap.
      */
-    _focusFirstElement() {
-        this._moveFocusTo('first');
+    focusFirstElement() {
+        this.moveFocusTo('first');
     }
 
     /**
      * Focuses the last focusable element in the focus trap.
      */
-    _focusLastElement() {
-        this._moveFocusTo('last');
+    focusLastElement() {
+        this.moveFocusTo('last');
     }
 
-    /**
-     * Returns a list of the focusable children found within the element.
-     */
-    _getFocusableElements() {
-        return findAllTabbableElements(this.template.querySelector('slot'));
+    handleFocusIn() {
+        if (this._pendingFocusOut) {
+            this._pendingFocusOut = false;
+        }
+        this._focused = true;
+    }
+
+    handleFocusOut() {
+        // This assumes that a focusin will be dispatched after a focusout
+        this._pendingFocusOut = true;
+        // eslint-disable-next-line @lwc/lwc/no-async-operation
+        requestAnimationFrame(() => {
+            if (this._pendingFocusOut) {
+                this._focused = false;
+            }
+        });
     }
 }
