3c4c9f158fb56addee86d640390ba43030cdc32c
Céline Ly <celine.ly@polymtl.ca>
2024-08-15 10:38:19 -0400

    [feat](ABC-1247): List - Check/uncheck the checkbox when click on the item

--- src/modules/base/list/list.js
+++ src/modules/base/list/list.js
@@ -786,7 +786,8 @@ export default class List extends LightningElement {
                 'avonni-list__item-divider_sortable':
                     this.sortable && this.divider !== 'none',
                 'avonni-list__item-divider':
-                    !this.sortable && this.divider !== 'none'
+                    !this.sortable && this.divider !== 'none',
+                'avonni-list__item_check-list': this.isCheckList
             })
             .add(`avonni-list__item-divider_${this.divider}`)
             .toString();
@@ -2201,15 +2202,14 @@ export default class List extends LightningElement {
      *
      * @param {Event} event
      */
-    handleItemCheck(event) {
-        const itemIndex = Number(event.currentTarget.dataset.index);
+    handleItemCheck(index, checked) {
+        const itemIndex = Number(index);
         const item = this.computedItems[itemIndex];
 
         if (!item) {
             return;
         }
 
-        const checked = event.currentTarget.checked;
         item.checked = checked;
         this.computedItems = [...this.computedItems];
 
@@ -2267,6 +2267,17 @@ export default class List extends LightningElement {
                 }
             })
         );
+
+        if (this.isCheckList) {
+            const itemCheckbox = event.currentTarget.querySelector(
+                '[data-element-id="item-input-checkbox"]'
+            );
+            if (itemCheckbox) {
+                const checked = !itemCheckbox.checked;
+                const checkboxItemIndex = Number(itemCheckbox.dataset.index);
+                this.handleItemCheck(checkboxItemIndex, checked);
+            }
+        }
     }
 
     /**
