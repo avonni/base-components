0610185af942bd1c381ec8bbf46c3e49683ebc33
Renaud Archer-Fortin <71739112+renaudaf@users.noreply.github.com>
2024-08-26 13:19:55 -0400

    [feat](ABC-1255): Rating - Added icon styling hooks

--- src/modules/base/rating/__docs__/rating.jsdoc.js
+++ src/modules/base/rating/__docs__/rating.jsdoc.js
@@ -27,6 +27,36 @@
  */
 /**
  * @memberof stylingHooks
+ * @name --avonni-rating-icon-color-foreground
+ * @default #c9c7c5
+ * @type color
+ */
+/**
+ * @memberof stylingHooks
+ * @name --avonni-rating-icon-color-foreground-disabled
+ * @default #c9c7c5
+ * @type color
+ */
+/**
+ * @memberof stylingHooks
+ * @name --avonni-rating-icon-color-foreground-disabled-selected
+ * @default #747474
+ * @type color
+ */
+/**
+ * @memberof stylingHooks
+ * @name --avonni-rating-icon-color-foreground-hover
+ * @default #014486
+ * @type color
+ */
+/**
+ * @memberof stylingHooks
+ * @name --avonni-rating-icon-color-foreground-selected
+ * @default #0176d3
+ * @type color
+ */
+/**
+ * @memberof stylingHooks
  * @name --avonni-rating-value-text-color
  * @default #080707
  * @type color
@@ -48,62 +78,4 @@
  * @name --avonni-rating-value-font-weight
  * @default 400
  * @type font
- */
-/**
- * @namespace examples
- */
-/**
- * @memberof examples
- * @name base
- * @storyId example-rating--base
- */
-/**
- * @memberof examples
- * @name stars
- * @storyId example-rating--stars
- */
-/**
- * @memberof examples
- * @name starsMediumLabelInline
- * @storyId example-rating--stars-medium-label-inline
- */
-/**
- * @memberof examples
- * @name starsSmall
- * @storyId example-rating--stars-small
- */
-/**
- * @memberof examples
- * @name starsXSmall
- * @storyId example-rating--stars-x-small
- */
-/**
- * @memberof examples
- * @name emojiValueHidden
- * @storyId example-rating--emoji-value-hidden
- */
-/**
- * @memberof examples
- * @name singleSelection
- * @storyId example-rating--single-selection
- */
-/**
- * @memberof examples
- * @name singleSelectionLabelHidden
- * @storyId example-rating--single-selection-label-hidden
- */
-/**
- * @memberof examples
- * @name singleSelectionLabelStacked
- * @storyId example-rating--single-selection-label-stacked
- */
-/**
- * @memberof examples
- * @name minMax
- * @storyId example-rating--min-max
- */
-/**
- * @memberof examples
- * @name disabled
- * @storyId example-rating--disabled
  */
