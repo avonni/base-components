150ac8541a61e7cfbd8552eab9c7a44de4ec5b9b
Renaud Archer-Fortin <71739112+renaudaf@users.noreply.github.com>
2024-08-27 10:44:03 -0400

    [feat](ABC-1255): Rating - Added new styling hooks for buttons and icons

--- src/modules/base/rating/__docs__/rating.jsdoc.js
+++ src/modules/base/rating/__docs__/rating.jsdoc.js
@@ -3,6 +3,96 @@
  */
 /**
  * @memberof stylingHooks
+ * @name --avonni-rating-button-color-background
+ * @default #ffffff
+ * @type color
+ */
+/**
+ * @memberof stylingHooks
+ * @name --avonni-rating-button-color-background-disabled
+ * @default #ffffff
+ * @type color
+ */
+/**
+ * @memberof stylingHooks
+ * @name ---avonni-rating-button-color-background-disabled-selected
+ * @default #c9c7c5
+ * @type color
+ */
+/**
+ * @memberof stylingHooks
+ * @name --avonni-rating-button-color-background-hover
+ * @default #014486
+ * @type color
+ */
+/**
+ * @memberof stylingHooks
+ * @name --avonni-rating-button-color-background-selected
+ * @default #0176d3
+ * @type color
+ */
+/**
+ * @memberof stylingHooks
+ * @name --avonni-rating-button-color-border
+ * @default #0176d3
+ * @type color
+ */
+/**
+ * @memberof stylingHooks
+ * @name --avonni-rating-button-color-border-disabled
+ * @default #c9c9c9
+ * @type color
+ */
+/**
+ * @memberof stylingHooks
+ * @name ---avonni-rating-button-color-border-disabled-selected
+ * @default #c9c7c5
+ * @type color
+ */
+/**
+ * @memberof stylingHooks
+ * @name --avonni-rating-button-color-border-hover
+ * @default #014486
+ * @type color
+ */
+/**
+ * @memberof stylingHooks
+ * @name --avonni-rating-button-color-border-selected
+ * @default #0176d3
+ * @type color
+ */
+/**
+ * @memberof stylingHooks
+ * @name --avonni-rating-button-text-color
+ * @default #0176d3
+ * @type color
+ */
+/**
+ * @memberof stylingHooks
+ * @name --avonni-rating-button-text-color-disabled
+ * @default #c9c7c5
+ * @type color
+ */
+/**
+ * @memberof stylingHooks
+ * @name --avonni-rating-button-text-color-disabled-selected
+ * @default #ffffff
+ * @type color
+ */
+/**
+ * @memberof stylingHooks
+ * @name --avonni-rating-button-text-color-hover
+ * @default #ffffff
+ * @type color
+ */
+/**
+ * @memberof stylingHooks
+ * @name --avonni-rating-button-text-color-selected
+ * @default #ffffff
+ * @type color
+ */
+/**
+ * @memberof stylingHooks
  * @name --avonni-rating-header-text-color
  * @default #3e3e3c
  * @type color
