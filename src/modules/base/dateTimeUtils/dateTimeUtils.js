8ae2d9716b777133597d58bc1ba69990567c1f86
Nawel Aammar <nawel.aammar@avonnicreator.com>
2024-09-06 10:57:32 -0400

    [feat] (ABC-1276): Add getStartOfWeek to dateTimeUtils

--- src/modules/base/dateTimeUtils/dateTimeUtils.js
+++ src/modules/base/dateTimeUtils/dateTimeUtils.js
@@ -1,3 +1,4 @@
 export { getFormattedDate } from './formatter';
 export { DateTime } from './dateTime';
 export { DATE_FORMAT_PRESETS } from './constants';
+export { getStartOfWeek } from './utils';
--- src/modules/base/dateTimeUtils/utils.js
+++ src/modules/base/dateTimeUtils/utils.js
@@ -1,7 +1,13 @@
+function getStartOfWeek(date) {
+    const day = date.getDay();
+    const timestamp = new Date(date).setDate(date.getDate() - day);
+    return new Date(timestamp);
+}
+
 function pad(value, length) {
     return (typeof value === 'number' && !isNaN(value)) || value
         ? value.toString().padStart(length, '0')
         : '';
 }
 
-export { pad };
+export { getStartOfWeek, pad };
