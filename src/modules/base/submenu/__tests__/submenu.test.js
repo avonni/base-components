2771e700ab6457fe5c3531532fc57866b9e5d0ed
Renaud Archer-Fortin <71739112+renaudaf@users.noreply.github.com>
2025-03-28 09:45:48 -0400

    [fix](ABC-1367): Submenu - Stopped dispatching select on hover (#979)

--- src/modules/base/submenu/__tests__/submenu.test.js
+++ src/modules/base/submenu/__tests__/submenu.test.js
@@ -15,206 +15,210 @@ describe('Submenu', () => {
         });
         document.body.appendChild(element);
     });
+    describe('Attributes', () => {
+        it('Default attributes', () => {
+            expect(element.accessKey).toBeUndefined();
+            expect(element.disabled).toBeFalsy();
+            expect(element.draftAlternativeText).toBeUndefined();
+            expect(element.iconName).toBeUndefined();
+            expect(element.isDraft).toBeFalsy();
+            expect(element.label).toBeUndefined();
+            expect(element.prefixIconName).toBeUndefined();
+            expect(element.tabIndex).toBe('0');
+        });
 
-    it('Submenu: Default attributes', () => {
-        expect(element.accessKey).toBeUndefined();
-        expect(element.disabled).toBeFalsy();
-        expect(element.draftAlternativeText).toBeUndefined();
-        expect(element.iconName).toBeUndefined();
-        expect(element.isDraft).toBeFalsy();
-        expect(element.label).toBeUndefined();
-        expect(element.prefixIconName).toBeUndefined();
-        expect(element.tabIndex).toBe('0');
-    });
+        describe('accessKey', () => {
+            it('Passed to the component', () => {
+                element.accessKey = 'k';
 
-    /* ----- ATTRIBUTES ----- */
+                return Promise.resolve().then(() => {
+                    const link = element.shadowRoot.querySelector(
+                        '[data-element-id="a"]'
+                    );
+                    expect(link.accessKey).toBe('k');
+                });
+            });
+        });
 
-    // access-key
-    it('Submenu: accessKey', () => {
-        element.accessKey = 'k';
+        describe('disabled', () => {
+            it('false', () => {
+                element.disabled = false;
 
-        return Promise.resolve().then(() => {
-            const link = element.shadowRoot.querySelector(
-                '[data-element-id="a"]'
-            );
-            expect(link.accessKey).toBe('k');
+                return Promise.resolve().then(() => {
+                    const link = element.shadowRoot.querySelector(
+                        '[data-element-id="a"]'
+                    );
+
+                    expect(link.ariaDisabled).toBe('false');
+                });
+            });
+
+            it('true', () => {
+                element.disabled = true;
+
+                return Promise.resolve().then(() => {
+                    const link = element.shadowRoot.querySelector(
+                        '[data-element-id="a"]'
+                    );
+
+                    expect(link.ariaDisabled).toBe('true');
+                });
+            });
+        });
+
+        // Depends on isDraft
+        describe('draftAlternativeText', () => {
+            it('Passed to the component', () => {
+                element.isDraft = true;
+                element.draftAlternativeText = 'A string help';
+
+                return Promise.resolve().then(() => {
+                    const abbr = element.shadowRoot.querySelector(
+                        '[data-element-id="abbr"]'
+                    );
+                    expect(abbr.title).toBe('A string help');
+                });
+            });
+        });
+
+        describe('iconName', () => {
+            it('Passed to the component', () => {
+                element.iconName = 'utility:apps';
+
+                return Promise.resolve().then(() => {
+                    const icon = element.shadowRoot.querySelector(
+                        '[data-element-id="lightning-icon"]'
+                    );
+                    expect(icon).toBeTruthy();
+                    expect(icon.iconName).toBe('utility:apps');
+                });
+            });
+        });
+
+        describe('isDraft', () => {
+            it('false', () => {
+                element.isDraft = false;
+
+                return Promise.resolve().then(() => {
+                    const abbr = element.shadowRoot.querySelector(
+                        '[data-element-id="abbr"]'
+                    );
+                    expect(abbr).toBeFalsy();
+                });
+            });
+
+            it('true', () => {
+                element.isDraft = true;
+
+                return Promise.resolve().then(() => {
+                    const abbr = element.shadowRoot.querySelector(
+                        '[data-element-id="abbr"]'
+                    );
+                    expect(abbr).toBeTruthy();
+                });
+            });
+        });
+
+        describe('label', () => {
+            it('Passed to the component', () => {
+                element.label = 'A string label';
+
+                return Promise.resolve().then(() => {
+                    const span = element.shadowRoot.querySelector(
+                        '[data-element-id="span-label"]'
+                    );
+                    expect(span.textContent).toBe('A string label');
+                    expect(span.title).toBe('A string label');
+                });
+            });
+        });
+
+        describe('prefixIconName', () => {
+            it('Passed to the component', () => {
+                element.prefixIconName = 'utility:apps';
+
+                return Promise.resolve().then(() => {
+                    const icon = element.shadowRoot.querySelector(
+                        '[data-element-id="lightning-icon-prefix"]'
+                    );
+                    expect(icon).toBeTruthy();
+                    expect(icon.iconName).toBe('utility:apps');
+                });
+            });
+        });
+
+        describe('tabIndex', () => {
+            it('Passed to the component', () => {
+                element.tabIndex = '-1';
+
+                return Promise.resolve().then(() => {
+                    const link = element.shadowRoot.querySelector(
+                        '[data-element-id="a"]'
+                    );
+                    expect(link.tabIndex).toBe(-1);
+                });
+            });
         });
     });
 
-    // disabled
-    it('Submenu: disabled = false', () => {
-        element.disabled = false;
-        const handler = jest.fn();
-        element.addEventListener('privateselect', handler);
+    describe('Methods', () => {
+        it('focus method', () => {
+            const handler = jest.fn();
+            element.addEventListener('focus', handler);
+            element.focus();
 
-        return Promise.resolve().then(() => {
-            const link = element.shadowRoot.querySelector(
-                '[data-element-id="a"]'
-            );
-            link.dispatchEvent(new CustomEvent('mouseenter'));
-
-            expect(link.ariaDisabled).toBe('false');
             expect(handler).toHaveBeenCalled();
         });
     });
 
-    it('Submenu: disabled = true', () => {
-        element.disabled = true;
-        const handler = jest.fn();
-        element.addEventListener('privateselect', handler);
+    describe('Events', () => {
+        describe('blur', () => {
+            it('blur', () => {
+                const handler = jest.fn();
+                element.addEventListener('blur', handler);
 
-        return Promise.resolve().then(() => {
-            const link = element.shadowRoot.querySelector(
-                '[data-element-id="a"]'
-            );
-            link.dispatchEvent(new CustomEvent('mouseenter'));
+                return Promise.resolve().then(() => {
+                    const a = element.shadowRoot.querySelector('a');
+                    a.dispatchEvent(new CustomEvent('blur'));
 
-            expect(link.ariaDisabled).toBe('true');
-            expect(handler).not.toHaveBeenCalled();
+                    expect(handler).toHaveBeenCalled();
+                    expect(handler.mock.calls[0][0].bubbles).toBeFalsy();
+                    expect(handler.mock.calls[0][0].composed).toBeFalsy();
+                    expect(handler.mock.calls[0][0].cancelable).toBeFalsy();
+                });
+            });
+
+            it('privateblur', () => {
+                const handler = jest.fn();
+                element.addEventListener('privateblur', handler);
+
+                return Promise.resolve().then(() => {
+                    const a = element.shadowRoot.querySelector('a');
+                    a.dispatchEvent(new CustomEvent('blur'));
+
+                    expect(handler).toHaveBeenCalled();
+                    expect(handler.mock.calls[0][0].bubbles).toBeTruthy();
+                    expect(handler.mock.calls[0][0].composed).toBeTruthy();
+                    expect(handler.mock.calls[0][0].cancelable).toBeTruthy();
+                });
+            });
         });
-    });
 
-    // draft-alternative-text
-    // Depends on isDraft
-    it('Submenu: draftAlternativeText', () => {
-        element.isDraft = true;
-        element.draftAlternativeText = 'A string help';
+        describe('focus', () => {
+            it('privatefocus', () => {
+                const handler = jest.fn();
+                element.addEventListener('privatefocus', handler);
 
-        return Promise.resolve().then(() => {
-            const abbr = element.shadowRoot.querySelector(
-                '[data-element-id="abbr"]'
-            );
-            expect(abbr.title).toBe('A string help');
-        });
-    });
+                return Promise.resolve().then(() => {
+                    const a = element.shadowRoot.querySelector('a');
+                    a.dispatchEvent(new CustomEvent('focus'));
 
-    // icon-name
-    it('Submenu: iconName', () => {
-        element.iconName = 'utility:apps';
-
-        return Promise.resolve().then(() => {
-            const icon = element.shadowRoot.querySelector(
-                '[data-element-id="lightning-icon"]'
-            );
-            expect(icon).toBeTruthy();
-            expect(icon.iconName).toBe('utility:apps');
-        });
-    });
-
-    // is-draft
-    it('Submenu: isDraft = false', () => {
-        element.isDraft = false;
-
-        return Promise.resolve().then(() => {
-            const abbr = element.shadowRoot.querySelector(
-                '[data-element-id="abbr"]'
-            );
-            expect(abbr).toBeFalsy();
-        });
-    });
-
-    it('Submenu: isDraft = true', () => {
-        element.isDraft = true;
-
-        return Promise.resolve().then(() => {
-            const abbr = element.shadowRoot.querySelector(
-                '[data-element-id="abbr"]'
-            );
-            expect(abbr).toBeTruthy();
-        });
-    });
-
-    it('Submenu: label', () => {
-        element.label = 'A string label';
-
-        return Promise.resolve().then(() => {
-            const span = element.shadowRoot.querySelector(
-                '[data-element-id="span-label"]'
-            );
-            expect(span.textContent).toBe('A string label');
-            expect(span.title).toBe('A string label');
-        });
-    });
-
-    // prefix-icon-name
-    it('Submenu: prefixIconName', () => {
-        element.prefixIconName = 'utility:apps';
-
-        return Promise.resolve().then(() => {
-            const icon = element.shadowRoot.querySelector(
-                '[data-element-id="lightning-icon-prefix"]'
-            );
-            expect(icon).toBeTruthy();
-            expect(icon.iconName).toBe('utility:apps');
-        });
-    });
-
-    // tab-index
-    it('Submenu: tabIndex', () => {
-        element.tabIndex = '-1';
-
-        return Promise.resolve().then(() => {
-            const link = element.shadowRoot.querySelector(
-                '[data-element-id="a"]'
-            );
-            expect(link.tabIndex).toBe(-1);
-        });
-    });
-
-    /* ----- METHODS ----- */
-
-    // focus
-    it('Submenu: focus method', () => {
-        const handler = jest.fn();
-        element.addEventListener('focus', handler);
-        element.focus();
-
-        expect(handler).toHaveBeenCalled();
-    });
-
-    /* ----- EVENTS ----- */
-    it('Submenu: blur', () => {
-        const handler = jest.fn();
-        element.addEventListener('blur', handler);
-
-        return Promise.resolve().then(() => {
-            const a = element.shadowRoot.querySelector('a');
-            a.dispatchEvent(new CustomEvent('blur'));
-
-            expect(handler).toHaveBeenCalled();
-            expect(handler.mock.calls[0][0].bubbles).toBeFalsy();
-            expect(handler.mock.calls[0][0].composed).toBeFalsy();
-            expect(handler.mock.calls[0][0].cancelable).toBeFalsy();
-        });
-    });
-
-    it('Submenu: privateblur', () => {
-        const handler = jest.fn();
-        element.addEventListener('privateblur', handler);
-
-        return Promise.resolve().then(() => {
-            const a = element.shadowRoot.querySelector('a');
-            a.dispatchEvent(new CustomEvent('blur'));
-
-            expect(handler).toHaveBeenCalled();
-            expect(handler.mock.calls[0][0].bubbles).toBeTruthy();
-            expect(handler.mock.calls[0][0].composed).toBeTruthy();
-            expect(handler.mock.calls[0][0].cancelable).toBeTruthy();
-        });
-    });
-
-    it('Submenu: privatefocus', () => {
-        const handler = jest.fn();
-        element.addEventListener('privatefocus', handler);
-
-        return Promise.resolve().then(() => {
-            const a = element.shadowRoot.querySelector('a');
-            a.dispatchEvent(new CustomEvent('focus'));
-
-            expect(handler).toHaveBeenCalled();
-            expect(handler.mock.calls[0][0].bubbles).toBeTruthy();
-            expect(handler.mock.calls[0][0].composed).toBeFalsy();
-            expect(handler.mock.calls[0][0].cancelable).toBeTruthy();
+                    expect(handler).toHaveBeenCalled();
+                    expect(handler.mock.calls[0][0].bubbles).toBeTruthy();
+                    expect(handler.mock.calls[0][0].composed).toBeFalsy();
+                    expect(handler.mock.calls[0][0].cancelable).toBeTruthy();
+                });
+            });
         });
     });
 });
