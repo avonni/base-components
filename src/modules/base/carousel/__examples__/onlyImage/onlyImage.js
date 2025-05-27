089d49c8f3a31d3994472c0110b6908db6dfe202
Céline Ly <celine.ly@polymtl.ca>
2025-05-27 16:49:12 -0400

    [story] Card and Carousel - Update media src in stories

--- src/modules/base/carousel/__examples__/onlyImage/onlyImage.js
+++ src/modules/base/carousel/__examples__/onlyImage/onlyImage.js
@@ -5,43 +5,43 @@ export default class AvonniOnlyImage extends LightningElement {
         {
             name: '1',
             imageAssistiveText: 'Appy',
-            src: 'https://react.lightningdesignsystem.com/assets/images/carousel/carousel-01.jpg',
+            src: 'https://v1.lightningdesignsystem.com/assets/images/carousel/carousel-01.jpg',
             href: 'https://www.salesforce.com'
         },
         {
             name: '2',
             imageAssistiveText: 'Appy',
-            src: 'https://react.lightningdesignsystem.com/assets/images/carousel/carousel-02.jpg',
+            src: 'https://v1.lightningdesignsystem.com/assets/images/carousel/carousel-02.jpg',
             href: 'https://www.salesforce.com'
         },
         {
             name: '3',
             imageAssistiveText: 'Salesforce Apps',
-            src: 'https://react.lightningdesignsystem.com/assets/images/carousel/carousel-03.jpg',
+            src: 'https://v1.lightningdesignsystem.com/assets/images/carousel/carousel-03.jpg',
             href: 'https://www.salesforce.com'
         },
         {
             name: '4',
             imageAssistiveText: 'Appy',
-            src: 'https://react.lightningdesignsystem.com/assets/images/carousel/carousel-02.jpg',
+            src: 'https://v1.lightningdesignsystem.com/assets/images/carousel/carousel-02.jpg',
             href: 'https://www.salesforce.com'
         },
         {
             name: '5',
             imageAssistiveText: 'Appy',
-            src: 'https://react.lightningdesignsystem.com/assets/images/carousel/carousel-01.jpg',
+            src: 'https://v1.lightningdesignsystem.com/assets/images/carousel/carousel-01.jpg',
             href: 'https://www.salesforce.com'
         },
         {
             name: '6',
             imageAssistiveText: 'Salesforce Apps',
-            src: 'https://react.lightningdesignsystem.com/assets/images/carousel/carousel-03.jpg',
+            src: 'https://v1.lightningdesignsystem.com/assets/images/carousel/carousel-03.jpg',
             href: 'https://www.salesforce.com'
         },
         {
             name: '7',
             imageAssistiveText: 'Appy',
-            src: 'https://react.lightningdesignsystem.com/assets/images/carousel/carousel-02.jpg',
+            src: 'https://v1.lightningdesignsystem.com/assets/images/carousel/carousel-02.jpg',
             href: 'https://www.salesforce.com'
         }
     ];
