2c096b3b2de8b43340c6ff3509f486f36f87a876
Renaud Archer-Fortin <71739112+renaudaf@users.noreply.github.com>
2025-07-04 14:39:43 -0400

    [refactor](ABC-1430): Tab Bar - Component Refactoring

--- src/modules/base/tabBar/__examples__/tabBar.js
+++ src/modules/base/tabBar/__examples__/tabBar.js
@@ -5,11 +5,11 @@ customElements.define(
     Component.CustomElementConstructor
 );
 
-export const TabBar = ({ items, labels, tabsHidden, defaultTab }) => {
+export const TabBar = ({ defaultTab, items, labels, tabsHidden }) => {
     const element = document.createElement('avonni-builder-tab-bar');
+    element.defaultTab = defaultTab;
     element.items = items;
     element.labels = labels;
     element.tabsHidden = tabsHidden;
-    element.defaultTab = defaultTab;
     return element;
 };
