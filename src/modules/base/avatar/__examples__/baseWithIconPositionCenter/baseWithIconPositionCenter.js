56664e33e2f84ddf98f105e46c572b9e3deb5bae
Gia-Sherwin Ly <ly.giasherwin@gmail.com>
2025-08-07 08:25:47 -0400

    Doc-ABC-1445-Avatar-Icon-Position

---
+++ src/modules/base/avatar/__examples__/baseWithIconPositionCenter/baseWithIconPositionCenter.js
@@ -0,0 +1,16 @@
+import { LightningElement } from 'lwc';
+
+export default class AvatarBaseWithIconPositionCenter extends LightningElement {
+    tags = [
+        { label: 'base', variant: 'base' },
+        { label: 'brand', variant: 'brand' },
+        { label: 'inverse', variant: 'inverse' },
+        { label: 'alt-inverse', variant: 'alt-inverse' },
+        { label: 'success', variant: 'success' },
+        { label: 'info', variant: 'info' },
+        { label: 'warning', variant: 'warning' },
+        { label: 'error', variant: 'error' },
+        { label: 'offline', variant: 'offline' },
+        { label: 'outline', variant: 'brand', outline: true }
+    ];
+}
