bf8b7a54dfe30a96c06b46eb9c658d32e67663b9
Olivier Bigonnesse <71099968+OliBigo@users.noreply.github.com>
2025-10-16 12:08:34 -0400

    [doc] (ABC-1506): Added story and example for type time

--- src/modules/base/outputData/__docs__/outputData.stories.js
+++ src/modules/base/outputData/__docs__/outputData.stories.js
@@ -25,8 +25,9 @@ export default {
                 'number',
                 'percent',
                 'phone',
-                'url',
-                'text'
+                'text',
+                'time',
+                'url'
             ],
             description: 'Data type',
             table: {
@@ -107,6 +108,13 @@ Percent.args = {
     typeAttributes: {
         maximumFractionDigits: 1
     }
+};
+
+export const Time = Template.bind({});
+Time.args = {
+    label: 'Time',
+    type: 'time',
+    value: 10800000
 };
 
 export const URL = Template.bind({});
