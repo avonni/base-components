6898bbf57986a938968d2361b0dbb617ff3cbce5
Céline Ly <46038766+Celyly@users.noreply.github.com>
2025-11-19 15:19:16 -0500

    [Feat](ABC-1530): Activity Timeline - Dates ignore locale when using SObject Collection Apex Action (#1129)
    * [fix](ABC-1530): Output Data - Add numeric to valid month values
    
    * [fix](ABC-1530): Activity Timeline - Fix formatting issue for date and datetime fields
    
    * [fix](ABC-1530): Activity Timeline - Show hour in numeric format by default

--- src/modules/base/dateTimeUtils/dateTimeUtils.js
+++ src/modules/base/dateTimeUtils/dateTimeUtils.js
@@ -1,4 +1,8 @@
-export { getFormattedDate } from './formatter';
+export {
+    DATE_FORMAT_PRESETS,
+    DEFAULT_DATE_FORMATS,
+    ISO_DATE_PATTERN
+} from './constants';
 export { DateTime } from './dateTime';
-export { DATE_FORMAT_PRESETS } from './constants';
+export { getFormattedDate, isISODateOnly } from './formatter';
 export { getStartOfWeek, setDate } from './utils';
