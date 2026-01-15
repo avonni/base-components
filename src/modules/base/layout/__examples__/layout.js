20b00db9266fe6dc5c4173cb80fa3bd23daa0877
Olivier Bigonnesse <71099968+OliBigo@users.noreply.github.com>
2026-01-15 15:33:21 -0500

    [doc] (ENG-108): Updated layout stories

--- src/modules/base/layout/__examples__/layout.js
+++ src/modules/base/layout/__examples__/layout.js
@@ -4,12 +4,14 @@ customElements.define('ac-layout', Component.CustomElementConstructor);
 
 export const Layout = ({
     direction,
+    equalHeights,
     horizontalAlign,
     multipleRows,
     verticalAlign
 }) => {
     const element = document.createElement('ac-layout');
     element.direction = direction;
+    element.equalHeights = equalHeights;
     element.horizontalAlign = horizontalAlign;
     element.multipleRows = multipleRows;
     element.verticalAlign = verticalAlign;
